4754d8dca560558182c45153a8eb395d647fb451
Robert Osfield <robert@openscenegraph.com>
2018-11-13 17:58:59 +0000

    Implemented an basic ObjectFactory and AsciiInput classes

--- examples_vsg/vsgio/vsgio.cpp
+++ examples_vsg/vsgio/vsgio.cpp
@@ -9,12 +9,15 @@
 
 #include <vsg/utils/CommandLine.h>
 
+#include <vsg/utils/FileSystem.h>
 #include <vsg/io/Input.h>
 #include <vsg/io/Output.h>
 
 #include <iostream>
+#include <fstream>
 #include <cstring>
 #include <unordered_map>
+#include <functional>
 
 
 vsg::ref_ptr<vsg::Node> createQuadTree(unsigned int numLevels, vsg::Node* sharedLeaf)
@@ -87,8 +90,7 @@ protected:
         std::ostream&   _output;
 
         using ObjectID = uint32_t;
-
-#if 1
+#if 0
         using ObjectIDMap = std::map<const vsg::Object*, ObjectID>;
 #else
         // 47% faster for overall write for large scene graph than std::map<>!
@@ -104,6 +106,193 @@ protected:
         const char*     _indentationString = "                        ";
 };
 
+class ObjectFactory : public vsg::Object
+{
+public:
+
+
+    using CreateFunction = std::function<vsg::ref_ptr<vsg::Object>()>;
+    using CreateMap = std::map<std::string, CreateFunction>;
+
+    CreateMap _createMap;
+
+
+    ObjectFactory()
+    {
+        _createMap["vsg::Group"] = [](){ return vsg::Group::create(); };
+        _createMap["vsg::Node"] = [](){ return vsg::Node::create(); };
+        _createMap["vsg::Object"] = [](){ return vsg::ref_ptr<vsg::Object>(new vsg::Object()); };
+    }
+
+    vsg::ref_ptr<vsg::Object> create(const std::string& className)
+    {
+        if (auto itr = _createMap.find(className); itr!=_createMap.end())
+        {
+            std::cout<<"Using _createMap for "<<className<<std::endl;
+            return (itr->second)();
+        }
+
+        return vsg::ref_ptr<vsg::Object>();
+    }
+};
+
+class AsciiInput : public vsg::Input
+{
+public:
+
+        using ObjectID = uint32_t;
+
+        AsciiInput(std::istream& input) :
+            _input(input)
+        {
+            _objectFactory = new ObjectFactory;
+
+            // write header
+            const char* match_token="#vsga";
+            char read_token[5];
+            _input.read(read_token, 5);
+            if (std::strncmp(match_token, read_token, 5)!=0)
+            {
+                std::cout<<"Header token not matched"<<std::endl;
+                throw std::string("Error: header not matched.");
+            }
+
+            char read_line[1024];
+            _input.getline(read_line, sizeof(read_line)-1);
+            std::cout<<"First line ["<<read_line<<"]"<<std::endl;
+        }
+
+        bool match(const char* propertyName)
+        {
+            if (propertyName)
+            {
+                _input >> _readPropertyName;
+                if (_readPropertyName!=propertyName)
+                {
+                    std::cout<<"Error: unable to match "<<propertyName<<std::endl;
+                    throw vsg::make_string("Error: unable to match ", propertyName);
+                }
+            }
+            return true;
+        }
+
+        std::optional<ObjectID> objectID()
+        {
+            std::string token;
+            _input >> token;
+            if (token.compare(0, 3, "id=")==0)
+            {
+                token.erase(0, 3);
+                std::stringstream str(token);
+                ObjectID id;
+                str>>id;
+                return std::optional<ObjectID>{id};
+            }
+            else
+            {
+                return std::nullopt;
+            }
+        }
+
+        template<typename T> void _read(const char* propertyName, T& value)
+        {
+            if (match(propertyName)) _input >> value;
+        }
+
+        // read single values
+        virtual void read(const char* propertyName, int8_t& value) { _read(propertyName, value); }
+        virtual void read(const char* propertyName, uint8_t& value) {_read(propertyName, value); }
+        virtual void read(const char* propertyName, int16_t& value) { _read(propertyName, value); }
+        virtual void read(const char* propertyName, uint16_t& value) { _read(propertyName, value); }
+        virtual void read(const char* propertyName, int32_t& value) { _read(propertyName, value); }
+        virtual void read(const char* propertyName, uint32_t& value) { _read(propertyName, value); }
+        virtual void read(const char* propertyName, int64_t& value) { _read(propertyName, value); }
+        virtual void read(const char* propertyName, uint64_t& value) { _read(propertyName, value); }
+        virtual void read(const char* propertyName, float& value) { _read(propertyName, value); }
+        virtual void read(const char* propertyName, double& value) { _read(propertyName, value); }
+
+#if 0
+        // read contiguous array of values
+        virtual void read(const char* propertyName, size_t num, int8_t* values) 0;
+        virtual void read(const char* propertyName, size_t num, uint8_t& value) 0;
+        virtual void read(const char* propertyName, size_t num, int16_t& value) 0;
+        virtual void read(const char* propertyName, size_t num, uint16_t& value) 0;
+        virtual void read(const char* propertyName, size_t num, int32_t& value) 0;
+        virtual void read(const char* propertyName, size_t num, uint32_t& value) 0;
+        virtual void read(const char* propertyName, size_t num, int64_t& value) 0;
+        virtual void read(const char* propertyName, size_t num, uint64_t& value) 0;
+        virtual void read(const char* propertyName, size_t num, float& value) 0;
+        virtual void read(const char* propertyName, size_t num, double& value) 0;
+#endif
+        // read object
+        virtual vsg::ref_ptr<vsg::Object> readObject(const char* propertyName)
+        {
+            if (match(propertyName))
+            {
+                std::cout<<"Matched "<<propertyName<<" need to read object"<<std::endl;
+
+                auto result = objectID();
+                if (result)
+                {
+                    ObjectID id = result.value();
+                    std::cout<<"   matched result="<<id<<std::endl;
+
+                    if (auto itr = _objectIDMap.find(id); itr != _objectIDMap.end())
+                    {
+                        std::cout<<"Returning existing object "<<itr->second.get()<<std::endl;
+                        return itr->second;
+                    }
+                    else
+                    {
+                        std::string className;
+                        _input >> className;
+
+                        std::cout<<"Loading new object "<<className<<std::endl;
+
+                        vsg::ref_ptr<vsg::Object> object = _objectFactory->create(className.c_str());
+
+                        if (object)
+                        {
+                            match("{");
+
+                            object->read(*this);
+
+                            std::cout<<"Loaded object, assigning to _objectIDMap."<<object.get()<<std::endl;
+
+                            match("}");
+
+                            _objectIDMap[id] = object;
+
+                            return object;
+                        }
+                        else
+                        {
+                            std::cout<<"Could not find means to create object"<<std::endl;
+                        }
+                    }
+
+                }
+
+            }
+            return vsg::ref_ptr<vsg::Object>();
+        }
+
+protected:
+
+        std::istream&   _input;
+
+#if 0
+        using ObjectIDMap = std::map<ObjectID, vsg::ref_ptr<vsg::Object>>;
+#else
+        // 47% faster for overall write for large scene graph than std::map<>!
+        using ObjectIDMap = std::unordered_map<ObjectID, vsg::ref_ptr<vsg::Object>>;
+#endif
+
+        std::string                 _readPropertyName;
+        ObjectIDMap                 _objectIDMap;
+        vsg::ref_ptr<ObjectFactory> _objectFactory;
+};
+
 int main(int argc, char** argv)
 {
     vsg::CommandLine arguments(&argc, argv);
@@ -117,19 +306,43 @@ int main(int argc, char** argv)
     std::cout<<"inputFilename "<<inputFilename<<std::endl;
     std::cout<<"outputFilename "<<outputFilename<<std::endl;
 
-
-    auto leaf = vsg::Node::create();
-    auto node = createQuadTree(numLevels, leaf);
-
-    if (node)
+    vsg::ref_ptr<vsg::Object> object;
+    if (inputFilename.empty())
     {
-        AsciiOutput output(std::cout);
-        output.write("Root", node);
+        auto leaf = vsg::Node::create();
+        object = createQuadTree(numLevels, leaf);
+    }
+    else
+    {
+        if (vsg::fileExists(inputFilename))
+        {
+            std::ifstream fin(inputFilename);
+            AsciiInput input(fin);
+            object = input.readObject("Root");
+        }
+        else
+        {
+            std::cout<<"Warning: could not find file : "<<inputFilename<<std::endl;
+            return 1;
+        }
     }
 
-    std::cout<<"type_name "<<vsg::type_name(*leaf)<<std::endl;
-    std::cout<<"type_name "<<vsg::type_name(*node)<<std::endl;
-    std::cout<<"type_name "<<vsg::type_name(vsg::vec4(1.0, 2.0, 3.0, 4.0))<<std::endl;
+    if (object)
+    {
+        if (outputFilename.empty())
+        {
+            // write graph to console
+            AsciiOutput output(std::cout);
+            output.write("Root", object);
+        }
+        else
+        {
+            // write to specified file
+            std::ofstream fout(outputFilename);
+            AsciiOutput output(fout);
+            output.write("Root", object);
+        }
+    }
 
     return 0;
 }
