da45271d62954db93c023863c4792e93562b1a50
Robert Osfield <robert@openscenegraph.com>
2019-01-10 17:54:47 +0000

    Moved createSceneData() into it's own dedicated header and source file

---
+++ Desktop/vsgcamera/CreateSceneData.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vsg/nodes/Node.h>
+
+#include <vsg/vk/GraphicsPipeline.h>
+#include <vsg/vk/PushConstants.h>
+#include <vsg/vk/CommandPool.h>
+
+struct SceneData
+{
+    vsg::ref_ptr<vsg::Node> commandGraph;
+    vsg::ref_ptr<vsg::PipelineLayout> pipelineLayout;
+    vsg::ref_ptr<vsg::GraphicsPipeline> pipeline;
+    vsg::ref_ptr<vsg::BindPipeline> bindPipeline;
+};
+
+extern SceneData createSceneData(vsg::ref_ptr<vsg::Device> device, vsg::ref_ptr<vsg::CommandPool> commandPool, vsg::ref_ptr<vsg::RenderPass> renderPass, VkQueue graphicsQueue, // viewer/window
+                                 vsg::ref_ptr<vsg::mat4Value> projMatrix, vsg::ref_ptr<vsg::mat4Value> viewMatrix, vsg::ref_ptr<vsg::ViewportState> viewport, // camera
+                                 vsg::ref_ptr<vsg::Shader> vertexShader, vsg::ref_ptr<vsg::Shader> fragmentShader, vsg::ref_ptr<vsg::Data> textureData); // scene graph
