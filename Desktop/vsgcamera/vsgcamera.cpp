132608ef3d8598807fae7f90800d2569fe3edefb
Robert Osfield <robert@openscenegraph.com>
2019-01-25 09:45:02 +0000

    Moved the createSceneData() into the main vsgcamera.cpp example

--- Desktop/vsgcamera/vsgcamera.cpp
+++ Desktop/vsgcamera/vsgcamera.cpp
@@ -9,9 +9,160 @@
 #include <chrono>
 
 #include "Trackball.h"
-#include "CreateSceneData.h"
 #include "GraphicsNodes.h"
 
+vsg::ref_ptr<vsg::Node> createSceneData(vsg::Paths& searchPaths)
+{
+    //
+    // load shaders
+    //
+    vsg::ref_ptr<vsg::Shader> vertexShader = vsg::Shader::read(VK_SHADER_STAGE_VERTEX_BIT, "main", vsg::findFile("shaders/vert_PushConstants.spv", searchPaths));
+    vsg::ref_ptr<vsg::Shader> fragmentShader = vsg::Shader::read(VK_SHADER_STAGE_FRAGMENT_BIT, "main", vsg::findFile("shaders/frag_PushConstants.spv", searchPaths));
+    if (!vertexShader || !fragmentShader)
+    {
+        std::cout<<"Could not create shaders."<<std::endl;
+        return vsg::ref_ptr<vsg::Node>();
+    }
+
+    std::string textureFile("textures/lz.vsgb");
+
+    //
+    // set up texture image
+    //
+    vsg::vsgReaderWriter vsgReader;
+    auto textureData = vsgReader.read<vsg::Data>(vsg::findFile(textureFile, searchPaths));
+    if (!textureData)
+    {
+        std::cout<<"Could not read texture file : "<<textureFile<<std::endl;
+        return vsg::ref_ptr<vsg::Node>();
+    }
+
+    //
+    // set up graphics pipeline
+    //
+    vsg::ref_ptr<vsg::GraphicsPipelineGroup> gp = vsg::GraphicsPipelineGroup::create();
+
+    gp->shaders = vsg::GraphicsPipelineGroup::Shaders{vertexShader, fragmentShader};
+    gp->maxSets = 1;
+    gp->descriptorPoolSizes = vsg::DescriptorPoolSizes
+    {
+        {VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1} // texture
+    };
+
+    gp->descriptorSetLayoutBindings = vsg::DescriptorSetLayoutBindings
+    {
+        {0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_FRAGMENT_BIT, nullptr} // { binding, descriptorTpe, descriptorCount, stageFlags, pImmutableSamplers}
+    };
+
+    gp->pushConstantRanges = vsg::PushConstantRanges
+    {
+        {VK_SHADER_STAGE_VERTEX_BIT, 0, 196} // projection view, and model matrices
+    };
+
+    gp->vertexBindingsDescriptions = vsg::VertexInputState::Bindings
+    {
+        VkVertexInputBindingDescription{0, sizeof(vsg::vec3), VK_VERTEX_INPUT_RATE_VERTEX}, // vertex data
+        VkVertexInputBindingDescription{1, sizeof(vsg::vec3), VK_VERTEX_INPUT_RATE_VERTEX}, // colour data
+        VkVertexInputBindingDescription{2, sizeof(vsg::vec2), VK_VERTEX_INPUT_RATE_VERTEX}  // tex coord data
+    };
+
+    gp->vertexAttributeDescriptions = vsg::VertexInputState::Attributes
+    {
+        VkVertexInputAttributeDescription{0, 0, VK_FORMAT_R32G32B32_SFLOAT, 0}, // vertex data
+        VkVertexInputAttributeDescription{1, 1, VK_FORMAT_R32G32B32_SFLOAT, 0}, // colour data
+        VkVertexInputAttributeDescription{2, 2, VK_FORMAT_R32G32_SFLOAT, 0},    // tex coord data
+    };
+
+    gp->pipelineStates = vsg::GraphicsPipelineStates
+    {
+        vsg::InputAssemblyState::create(), // device independent
+        vsg::RasterizationState::create(),// device independent
+        vsg::MultisampleState::create(),// device independent
+        vsg::ColorBlendState::create(),// device independent
+        vsg::DepthStencilState::create()// device independent
+    };
+
+
+    //
+    // set up model transformation node
+    //
+    auto transform = vsg::MatrixTransform::create(); // VK_SHADER_STAGE_VERTEX_BIT, 128
+
+    // add transform to graphics pipeline group
+    gp->addChild(transform);
+
+
+    //
+    // create texture node
+    //
+    vsg::ref_ptr<vsg::Texture> texture = vsg::Texture::create();
+    texture->_textureData = textureData;
+
+    // add texture node to transform node
+    transform->addChild(texture);
+
+
+    //
+    // set up vertex and index arrays
+    //
+    vsg::ref_ptr<vsg::vec3Array> vertices(new vsg::vec3Array
+    {
+        {-0.5f, -0.5f, 0.0f},
+        {0.5f,  -0.5f, 0.05f},
+        {0.5f , 0.5f, 0.0f},
+        {-0.5f, 0.5f, 0.0f},
+        {-0.5f, -0.5f, -0.5f},
+        {0.5f,  -0.5f, -0.5f},
+        {0.5f , 0.5f, -0.5},
+        {-0.5f, 0.5f, -0.5}
+    }); // VK_FORMAT_R32G32B32_SFLOAT, VK_VERTEX_INPUT_RATE_INSTANCE, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
+
+    vsg::ref_ptr<vsg::vec3Array> colors(new vsg::vec3Array
+    {
+        {1.0f, 0.0f, 0.0f},
+        {0.0f, 1.0f, 0.0f},
+        {0.0f, 0.0f, 1.0f},
+        {1.0f, 1.0f, 1.0f},
+        {1.0f, 0.0f, 0.0f},
+        {0.0f, 1.0f, 0.0f},
+        {0.0f, 0.0f, 1.0f},
+        {1.0f, 1.0f, 1.0f},
+    }); // VK_FORMAT_R32G32B32_SFLOAT, VK_VERTEX_INPUT_RATE_VERTEX, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
+
+    vsg::ref_ptr<vsg::vec2Array> texcoords(new vsg::vec2Array
+    {
+        {0.0f, 0.0f},
+        {1.0f, 0.0f},
+        {1.0f, 1.0f},
+        {0.0f, 1.0f},
+        {0.0f, 0.0f},
+        {1.0f, 0.0f},
+        {1.0f, 1.0f},
+        {0.0f, 1.0f}
+    }); // VK_FORMAT_R32G32_SFLOAT, VK_VERTEX_INPUT_RATE_VERTEX, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
+
+    vsg::ref_ptr<vsg::ushortArray> indices(new vsg::ushortArray
+    {
+        0, 1, 2,
+        2, 3, 0,
+        4, 5, 6,
+        6, 7, 4
+    }); // VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
+
+    auto geometry = vsg::Geometry::create();
+
+    // setup geometry
+    geometry->_arrays = vsg::DataList{vertices, colors, texcoords};
+    geometry->_indices = indices;
+
+    vsg::ref_ptr<vsg::DrawIndexed> drawIndexed = vsg::DrawIndexed::create(12, 1, 0, 0, 0); // device independent
+    geometry->_commands = vsg::Geometry::Commands{drawIndexed};
+
+    // add geometry to texture group
+    texture->addChild(geometry);
+
+    return gp;
+}
 
 
 int main(int argc, char** argv)
