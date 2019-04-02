b1ee0a72273ac67d9bbb6db18e11e7caef60cc33
tomhog <thomas.hogarth@gmail.com>
2019-04-02 17:06:15 +0100

    Added GraphicsPipelineBuilder, TextGraphicsPipelineBuiler, adjusted shaders to work with auto built pipeline. Added textgroup for multiple texts with unique instanced position

--- Desktop/vsginput/vsginput.cpp
+++ Desktop/vsginput/vsginput.cpp
@@ -1,14 +1,16 @@
 #include <vsg/all.h>
+
 #include <iostream>
+#include <iomanip>
 
 #include "Text.h"
 
 class KeyboardInput : public vsg::Inherit<vsg::Visitor, KeyboardInput>
 {
 public:
-    KeyboardInput(vsg::ref_ptr<vsg::Viewer> viewer, vsg::ref_ptr<vsg::TextGroup> textGroup, vsg::Paths searchPaths) :
+    KeyboardInput(vsg::ref_ptr<vsg::Viewer> viewer, vsg::ref_ptr<vsg::Group> root, vsg::Paths searchPaths) :
         _viewer(viewer),
-        _textGroup(textGroup),
+        _root(root),
         _shouldRecompile(false),
         _shouldReleaseViewport(false)
     {
@@ -17,33 +19,28 @@ public:
         auto width = window->extent2D().width;
         auto height = window->extent2D().height;
 
-        auto font = vsg::Font::create("roboto", searchPaths);
+        vsg::ref_ptr<vsg::StateGroup> stategroup = vsg::StateGroup::create();
+        root->addChild(stategroup);
 
-        _keyboardInputText = vsg::Text::create(font, _textGroup);
-        _textGroup->addChild(_keyboardInputText);
+        vsg::ref_ptr<vsg::TextGraphicsPipelineBuilder> textPipelineBuilder = vsg::TextGraphicsPipelineBuilder::create(searchPaths);
+        stategroup->add(vsg::BindGraphicsPipeline::create(textPipelineBuilder->getGraphicsPipeline()));
+        _graphicsPipeline = textPipelineBuilder->getGraphicsPipeline();
+
+        // any text attached below the font node will use it's atlas and lookup texture descriptor set (0)
+        _font = vsg::Font::create(textPipelineBuilder->getGraphicsPipeline(), "roboto", searchPaths);
+        stategroup->addChild(_font);
+
+        _keyboardInputText = vsg::Text::create(_font, textPipelineBuilder->getGraphicsPipeline());
+        _font->addChild(_keyboardInputText);
+
+        _textGroup = vsg::TextGroup::create(_font, textPipelineBuilder->getGraphicsPipeline());
+        _font->addChild(_textGroup);
         
-        // fill with test data
-        float fontheight = 50.0f;
-        std::string sampletext = "Cheese!";
+        //
+        _keyboardInputText->setFontHeight(50.0f);
+        _keyboardInputText->setText("VulkanSceneGraph!");
 
-        /*
-        uint32_t maxlines = 1000;
-        uint32_t charsPerLine = 1000;
-        for (uint32_t line = 0; line < maxlines; line++)
-        {
-            for (uint32_t c = 0; c < charsPerLine; c++)
-            {
-                sampletext += 32 + (rand() % 94);
-            }
-            sampletext += "\n";
-        }
-        std::cout << "total characters: " << sampletext.size() << std::endl;
-        */
-
-        _keyboardInputText->setFontHeight(fontheight);
-        _keyboardInputText->setText(sampletext);
-
-        _keyboardInputText->setPosition(vsg::vec3(-(width*0.5f), (height*0.5f) - fontheight, 0.0f));
+        _keyboardInputText->setPosition(vsg::vec3(-(width*0.5f), (height*0.5f) - _keyboardInputText->getFontHeight(), 0.0f));
     }
 
     void apply(vsg::ConfigureWindowEvent& configure)
@@ -55,14 +52,10 @@ public:
         vsg::ref_ptr<vsg::GraphicsStage> graphicsStage = vsg::ref_ptr<vsg::GraphicsStage>(dynamic_cast<vsg::GraphicsStage*>(window->stages()[0].get()));
         vsg::Camera* camera = graphicsStage->_camera;
 
-        vsg::ref_ptr<vsg::Orthographic> orthographic(new vsg::Orthographic(-(width*0.5f), (width*0.5f), -(height*0.5f), (height*0.5f), 0.1, 10.0));
+        vsg::ref_ptr<vsg::Orthographic> orthographic(new vsg::Orthographic(-(width*0.5f), (width*0.5f), -(height*0.5f), (height*0.5f), 0.1, 1000.0));
         
         camera->setProjectionMatrix(orthographic);
         _keyboardInputText->setPosition(vsg::vec3(-(width*0.5f), (height*0.5f) - _keyboardInputText->getFontHeight(), 0.0f));
-
-        //rand color
-        float randcolor = (rand() % 10) / 10.0f;
-        //window->clearColor().float32[0] = randcolor;
 
         _shouldRecompile = true;
         _shouldReleaseViewport = true;
@@ -75,23 +68,76 @@ public:
 
         std::string textstr = _keyboardInputText->getText();
 
-        if (keyPress.keyBase == vsg::KeySymbol::KEY_BackSpace)
+        if(keyPress.keyBase < 32 || keyPress.keyBase > 126)
         {
-            if(textstr.size() == 0) return;
-            _keyboardInputText->setText(textstr.substr(0, textstr.size() - 1));
-            _shouldRecompile = true;
-            return;
-        }
-
-        if (keyPress.keyBase == vsg::KeySymbol::KEY_Return)
-        {
-            _keyboardInputText->setText(textstr + '\n');
-            _shouldRecompile = true;
+            if (keyPress.keyBase == vsg::KeySymbol::KEY_BackSpace)
+            {
+                if(textstr.size() == 0) return;
+                _keyboardInputText->setText(textstr.substr(0, textstr.size() - 1));
+                _shouldRecompile = true;
+            }
+            else if (keyPress.keyBase == vsg::KeySymbol::KEY_Return)
+            {
+                _keyboardInputText->setText(textstr + '\n');
+                _shouldRecompile = true;
+            }
             return;
         }
 
         textstr += keyPress.keyModified;
         _keyboardInputText->setText(textstr);
+        _shouldRecompile = true;
+    }
+
+    void apply(vsg::KeyReleaseEvent& keyPress) override
+    {
+        if (keyPress.keyBase == vsg::KeySymbol::KEY_Up)
+        {
+            _textGroup->setFontHeight(_textGroup->getFontHeight() * 1.1f);
+        }
+        else if (keyPress.keyBase == vsg::KeySymbol::KEY_Down)
+        {
+            _textGroup->setFontHeight(_textGroup->getFontHeight() * 0.9f);
+        }
+        else if (keyPress.keyBase == vsg::KeySymbol::KEY_Left)
+        {
+            _density = _density - 10;
+            if (_density <= 0) _density = 10;
+            randomFillCube(vsg::vec3(-400.0f, -400.0f, -800.0f), vsg::vec3(800.0f, 800.0f, 800.0f), _density, _textGroup->getFontHeight());
+        }
+        else if (keyPress.keyBase == vsg::KeySymbol::KEY_Right)
+        {
+            _density = _density + 10;
+            if (_density > 100) _density = 100;
+            randomFillCube(vsg::vec3(-400.0f, -400.0f, -800.0f), vsg::vec3(800.0f, 800.0f, 800.0f), _density, _textGroup->getFontHeight());
+        }
+    }
+
+
+    void randomFillCube(const vsg::vec3& offset, const vsg::vec3& size, uint32_t density, float fontHeight)
+    {
+        _textGroup->clear();
+        _textGroup->setFontHeight(fontHeight);
+        
+        float invdensity = 1.0f / density;
+        vsg::vec3 cellsize = size * invdensity;
+
+        for (uint32_t z = 0; z < density; z++)
+        {
+            for (uint32_t y = 0; y < density; y++)
+            {
+                for (uint32_t x = 0; x < density; x++)
+                {
+                    vsg::vec3 point = offset + vsg::vec3(cellsize.x * x, cellsize.y * y, cellsize.z * z); // vsg::vec3(fmod((float)rand(), size.y), fmod((float)rand(), size.y), fmod((float)rand(), size.y));
+                    std::ostringstream ss;
+                    ss << "VSG";
+                    _textGroup->addText(ss.str(), point);
+                }
+            }
+        }
+
+        std::cout << "compiling unique texts objects: " << _textGroup->getNumTexts() << std::endl;
+        _textGroup->buildTextGraph();
         _shouldRecompile = true;
     }
 
@@ -103,19 +149,24 @@ public:
         // hack, release the graphics pipeline implementation to allow new viewport to recompile
         if(_shouldReleaseViewport)
         {
-            _textGroup->_bindGraphicsPipeline->getPipeline()->release();
+            _graphicsPipeline->release();
             _shouldReleaseViewport = false;
         }
     }
 
 protected:
     vsg::ref_ptr<vsg::Viewer> _viewer;
-    vsg::ref_ptr<vsg::TextGroup> _textGroup;
+    vsg::ref_ptr<vsg::Group> _root;
+    vsg::ref_ptr<vsg::Font> _font;
     vsg::ref_ptr<vsg::Text> _keyboardInputText;
+
+    vsg::ref_ptr<vsg::TextGroup> _textGroup;
+    uint32_t _density = 0;
 
     // flag used to force recompile
     bool _shouldRecompile;
     bool _shouldReleaseViewport;
+    vsg::ref_ptr<vsg::GraphicsPipeline> _graphicsPipeline;
 };
 
 int main(int argc, char** argv)
@@ -137,10 +188,6 @@ int main(int argc, char** argv)
     auto transform = vsg::MatrixTransform::create(); // there must be a transform, guessing to populate the push constant
     scenegraph->addChild(transform);
 
-    // create text group
-    auto textgroup = vsg::TextGroup::create(searchPaths);
-    transform->addChild(textgroup);
-
     // create the viewer and assign window(s) to it
     auto viewer = vsg::Viewer::create();
 
@@ -161,7 +208,7 @@ int main(int argc, char** argv)
     // camera related details
     auto viewport = vsg::ViewportState::create(VkExtent2D{width, height});
     //vsg::ref_ptr<vsg::Perspective> perspective(new vsg::Perspective(60.0, static_cast<double>(width) / static_cast<double>(height), 0.1, 10.0));
-    vsg::ref_ptr<vsg::Orthographic> orthographic(new vsg::Orthographic(-(width*0.5f), (width*0.5f), -(height*0.5f), (height*0.5f), 0.1, 10.0));
+    vsg::ref_ptr<vsg::Orthographic> orthographic(new vsg::Orthographic(-(width*0.5f), (width*0.5f), -(height*0.5f), (height*0.5f), 0.1, 1000.0));
     vsg::ref_ptr<vsg::LookAt> lookAt(new vsg::LookAt(vsg::dvec3(0.0, 0.0, 1.0), vsg::dvec3(0.0, 0.0, 0.0), vsg::dvec3(0.0, 1.0, 0.0)));
     vsg::ref_ptr<vsg::Camera> camera(new vsg::Camera(orthographic, lookAt, viewport));
 
@@ -169,7 +216,7 @@ int main(int argc, char** argv)
     window->addStage(vsg::GraphicsStage::create(scenegraph, camera));
 
     // keyboard input for demo
-    vsg::ref_ptr<KeyboardInput> keyboardInput = KeyboardInput::create(viewer, textgroup, searchPaths);
+    vsg::ref_ptr<KeyboardInput> keyboardInput = KeyboardInput::create(viewer, scenegraph, searchPaths);
 
     // assign a CloseHandler to the Viewer to respond to pressing Escape or press the window close button
     viewer->addEventHandlers({vsg::CloseHandler::create(viewer), keyboardInput});
--- data/shaders/vert_text.vert
+++ data/shaders/vert_text.vert
@@ -10,13 +10,14 @@ layout(push_constant) uniform PushConstants {
 layout(location = 0) in vec3 inPosition;
 
 // per instance
-layout(location = 1) in vec2 instOffset;
-layout(location = 2) in float instLookupOffset;
+layout(location = 1) in vec3 instPostion;
+layout(location = 2) in vec2 instOffset;
+layout(location = 3) in float instLookupOffset;
 
-layout(binding = 1) uniform sampler2D glyphUVs;
-layout(binding = 2) uniform sampler2D glyphSizes;
+layout(set = 0, binding = 0) uniform sampler2D glyphUVs;
+layout(set = 0, binding = 1) uniform sampler2D glyphSizes;
 
-layout(binding = 3) uniform TextMetrics
+layout(set = 1, binding = 3) uniform TextMetrics
 {
     float height;
     float lineHeight;
@@ -31,13 +32,15 @@ out gl_PerVertex {
 void main()
 {
     // lookup glyph data
-    vec4 glyphRect = texture(glyphUVs, vec2(instLookupOffset, 0.5));
+    vec4 glyphUvRect = texture(glyphUVs, vec2(instLookupOffset, 0.5));
     vec4 glyphSize = texture(glyphSizes, vec2(instLookupOffset, 0.5)) * textMetrics.height;
 
-    vec2 scaledOffset = (instOffset * textMetrics.height) + glyphSize.zw;
+    vec2 offset = (instOffset.xy * textMetrics.height) + glyphSize.xy;
+	vec2 size = inPosition.xy * glyphSize.zw;
 
-    vec2 vert = (inPosition.xy * glyphSize.xy) + scaledOffset;
-    gl_Position = (pc.projection * pc.view * pc.model) * vec4(vert, 0.0, 1.0);
+    vec2 vert = offset + size;
+	vec3 point = instPostion + vec3(vert, 0.0);
+    gl_Position = (pc.projection * pc.view * pc.model) * vec4(point, 1.0);
     
-    fragTexCoord = (inPosition.xy * glyphRect.zw) + glyphRect.xy;
+    fragTexCoord = (inPosition.xy * glyphUvRect.zw) + glyphUvRect.xy;
 }
