d416e495a5617a6d40f25fb8eae40079a5f31060
Robert Osfield <robert@openscenegraph.com>
2019-11-08 15:52:13 +0000

    Initial shells of Submission/Presentation/CommandGraph/RenderGraph classes.

--- Desktop/vsgexp/ExperimentalViewer.cpp
+++ Desktop/vsgexp/ExperimentalViewer.cpp
@@ -2,25 +2,86 @@
 
 using namespace vsg;
 
-// TODO
-// Replace Stage
-// Replace
-//
-// Viewer
-//        struct PerDeviceObjects
-//        {
-//            Windows windows;
-//            ref_ptr<Queue> graphicsQueue;
-//            ref_ptr<Queue> presentQueue;
-//            ref_ptr<Semaphore> renderFinishedSemaphore;
-//
-//            // cache data to be used each frame
-//            std::vector<uint32_t> imageIndices;
-//            std::vector<VkSemaphore> signalSemaphores;
-//            std::vector<VkCommandBuffer> commandBuffers;
-//            std::vector<VkSwapchainKHR> swapchains;
-//            std::vector<VkPipelineStageFlags> waitStages;
-//        };
+VkResult Submission::submit()
+{
+    std::vector<VkSemaphore> vk_waitSemaphores;
+    std::vector<VkPipelineStageFlags> vk_waitStages;
+    std::vector<VkCommandBuffer> vk_commandBuffers;
+    std::vector<VkSemaphore> vk_signalSemaphores;
+
+
+    for(auto& semaphore : waitSemaphores)
+    {
+        vk_waitSemaphores.emplace_back(*(semaphore));
+        vk_waitStages.emplace_back(semaphore->pipelineStageFlags());
+    }
+
+    for(auto& commandGraph : commandGraphs)
+    {
+        VkCommandBuffer vk_commandBuffer = *(commandGraph->commandBuffer);
+
+        // need to set up the command
+        VkCommandBufferBeginInfo beginInfo = {};
+        beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        beginInfo.flags = VK_COMMAND_BUFFER_USAGE_SIMULTANEOUS_USE_BIT;
+        // if we are nested within a CommandBuffer already then use VkCommandBufferInheritanceInfo
+
+        vkBeginCommandBuffer(vk_commandBuffer, &beginInfo);
+
+        // traverse the command graph
+        commandGraph->traverse(*(commandGraph->dispatchTraversal));
+
+        vkEndCommandBuffer(vk_commandBuffer);
+
+        vk_commandBuffers.emplace_back(vk_commandBuffer);
+    }
+
+    for(auto& semaphore : signalSemaphores)
+    {
+        vk_signalSemaphores.emplace_back(*(semaphore));
+    }
+
+
+    VkSubmitInfo submitInfo = {};
+    submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+
+    submitInfo.waitSemaphoreCount = static_cast<uint32_t>(vk_waitSemaphores.size());
+    submitInfo.pWaitSemaphores = vk_waitSemaphores.data();
+    submitInfo.pWaitDstStageMask = vk_waitStages.data();
+
+    submitInfo.commandBufferCount = static_cast<uint32_t>(vk_commandBuffers.size());
+    submitInfo.pCommandBuffers = vk_commandBuffers.data();
+
+    submitInfo.signalSemaphoreCount = static_cast<uint32_t>(vk_signalSemaphores.size());
+    submitInfo.pSignalSemaphores = vk_signalSemaphores.data();
+
+    return queue->submit(submitInfo, fence);
+}
+
+VkResult Presentation::present()
+{
+    std::vector<VkSemaphore> vk_semaphores;
+    for(auto& semaphore : waitSemaphores)
+    {
+        vk_semaphores.emplace_back(*(semaphore));
+    }
+
+    std::vector<VkSwapchainKHR> vk_swapchains;
+    for(auto& swapchain : swapchains)
+    {
+        vk_swapchains.emplace_back(*(swapchain));
+    }
+
+    VkPresentInfoKHR presentInfo = {};
+    presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+    presentInfo.waitSemaphoreCount = static_cast<uint32_t>(vk_semaphores.size());
+    presentInfo.pWaitSemaphores = vk_semaphores.data();
+    presentInfo.swapchainCount = static_cast<uint32_t>(vk_swapchains.size());
+    presentInfo.pSwapchains = vk_swapchains.data();
+    presentInfo.pImageIndices = indices.data();
+
+    return queue->present(presentInfo);
+}
 
 ExperimentalViewer::ExperimentalViewer()
 {
--- Desktop/vsgexp/ExperimentalViewer.h
+++ Desktop/vsgexp/ExperimentalViewer.h
@@ -2,9 +2,69 @@
 
 namespace vsg
 {
+    class CommandGraph : public Inherit<Group, CommandGraph>
+    {
+    public:
+        ref_ptr<CommandBuffer> commandBuffer;
+        ref_ptr<DispatchTraversal> dispatchTraversal;
+    };
+
+    class RenderGraph : public Inherit<Group, RenderGraph>
+    {
+    public:
+        using ClearValues = std::vector<VkClearValue>;
+
+        ref_ptr<RenderPass> renderPass;
+        ref_ptr<Framebuffer> framebuffer;
+        VkRect2D renderArea;
+        ClearValues clearValues;
+    };
+
+    class Submission : public Inherit<Object, Submission>
+    {
+    public:
+
+        VkResult submit();
+
+        using CommandGraphs = std::vector<ref_ptr<CommandGraph>>;
+
+        Semaphores waitSemaphores;
+        CommandGraphs commandGraphs;
+        Semaphores signalSemaphores;
+        ref_ptr<Fence> fence;
+
+        ref_ptr<Queue> queue;
+    };
+
+    class Presentation : public Inherit<Object, Presentation>
+    {
+    public:
+
+        VkResult present();
+
+        using Swapchains = std::vector<ref_ptr<Swapchain>>;
+        using Indices = std::vector<uint32_t>;
+
+        Semaphores waitSemaphores;
+        Swapchains swapchains;
+        Indices indices;
+
+        ref_ptr<Queue> queue;
+    };
+
+
     class ExperimentalViewer : public Inherit<Viewer, ExperimentalViewer>
     {
     public:
+
+        struct FrameObjects
+        {
+            ref_ptr<Submission> submission;
+            ref_ptr<Presentation> presentation;
+        };
+
+        std::vector<FrameObjects> frameObjects;
+
         ExperimentalViewer();
 
         ~ExperimentalViewer();
