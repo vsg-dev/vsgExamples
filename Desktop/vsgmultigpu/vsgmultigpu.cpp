36bde20b7baee20329ccc52689011ef973b81044
Robert Osfield <robert@openscenegraph.com>
2020-02-27 12:47:17 +0000

    Added fallback scene for when no command line specified model filename is provided. Added --shader / -s command line option to enable sharing of a scene graph between multiple devices

--- Desktop/vsgmultigpu/vsgmultigpu.cpp
+++ Desktop/vsgmultigpu/vsgmultigpu.cpp
@@ -6,15 +6,150 @@
 
 #include "AnimationPath.h"
 
-// TODO
-// 1. Each window to get it's own slave Camera -> need to sync the view/projection matrix + offset
-//    a. RelativeViewMatrix - multiple by an offset matrix DONE
-//    a. RelativeProjectionMatrix - multiple by an offset matrix  DONE
-// 2. Each logical Device to have it's own DeviceID : DONE
-//
-// 3. All Vulkan objects to be accessed via a DeviceID and to use compile time configured buffering
-// 4. Compile traversal to run for each Logical device/DeviceID - one Context per Logical Device?
-// 5. DatabasePager to handle multiple logical Device
+
+vsg::ref_ptr<vsg::Node> createScene(std::string filename)
+{
+    if (!filename.empty())
+    {
+        return vsg::read_cast<vsg::Node>(filename);
+    }
+
+    // set up search paths to SPIRV shaders and textures
+    vsg::Paths searchPaths = vsg::getEnvPaths("VSG_FILE_PATH");
+
+    // load shaders
+    vsg::ref_ptr<vsg::ShaderStage> vertexShader = vsg::ShaderStage::read(VK_SHADER_STAGE_VERTEX_BIT, "main", vsg::findFile("shaders/vert_PushConstants.spv", searchPaths));
+    vsg::ref_ptr<vsg::ShaderStage> fragmentShader = vsg::ShaderStage::read(VK_SHADER_STAGE_FRAGMENT_BIT, "main", vsg::findFile("shaders/frag_PushConstants.spv", searchPaths));
+    if (!vertexShader || !fragmentShader)
+    {
+        std::cout<<"Could not create shaders."<<std::endl;
+        return {};
+    }
+
+    // read texture image
+    vsg::Path textureFile("textures/lz.vsgb");
+    auto textureData = vsg::read_cast<vsg::Data>(vsg::findFile(textureFile, searchPaths));
+    if (!textureData)
+    {
+        std::cout<<"Could not read texture file : "<<textureFile<<std::endl;
+        return {};
+    }
+
+    // set up graphics pipeline
+    vsg::DescriptorSetLayoutBindings descriptorBindings
+    {
+        {0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_FRAGMENT_BIT, nullptr} // { binding, descriptorTpe, descriptorCount, stageFlags, pImmutableSamplers}
+    };
+
+    vsg::DescriptorSetLayouts descriptorSetLayouts{vsg::DescriptorSetLayout::create(descriptorBindings)};
+
+    vsg::PushConstantRanges pushConstantRanges
+    {
+        {VK_SHADER_STAGE_VERTEX_BIT, 0, 128} // projection view, and model matrices, actual push constant calls autoaatically provided by the VSG's DispatchTraversal
+    };
+
+    vsg::VertexInputState::Bindings vertexBindingsDescriptions
+    {
+        VkVertexInputBindingDescription{0, sizeof(vsg::vec3), VK_VERTEX_INPUT_RATE_VERTEX}, // vertex data
+        VkVertexInputBindingDescription{1, sizeof(vsg::vec3), VK_VERTEX_INPUT_RATE_VERTEX}, // colour data
+        VkVertexInputBindingDescription{2, sizeof(vsg::vec2), VK_VERTEX_INPUT_RATE_VERTEX}  // tex coord data
+    };
+
+    vsg::VertexInputState::Attributes vertexAttributeDescriptions
+    {
+        VkVertexInputAttributeDescription{0, 0, VK_FORMAT_R32G32B32_SFLOAT, 0}, // vertex data
+        VkVertexInputAttributeDescription{1, 1, VK_FORMAT_R32G32B32_SFLOAT, 0}, // colour data
+        VkVertexInputAttributeDescription{2, 2, VK_FORMAT_R32G32_SFLOAT, 0},    // tex coord data
+    };
+
+    vsg::GraphicsPipelineStates pipelineStates
+    {
+        vsg::VertexInputState::create( vertexBindingsDescriptions, vertexAttributeDescriptions ),
+        vsg::InputAssemblyState::create(),
+        vsg::RasterizationState::create(),
+        vsg::MultisampleState::create(),
+        vsg::ColorBlendState::create(),
+        vsg::DepthStencilState::create()
+    };
+
+    auto pipelineLayout = vsg::PipelineLayout::create(descriptorSetLayouts, pushConstantRanges);
+    auto graphicsPipeline = vsg::GraphicsPipeline::create(pipelineLayout, vsg::ShaderStages{vertexShader, fragmentShader}, pipelineStates);
+    auto bindGraphicsPipeline = vsg::BindGraphicsPipeline::create(graphicsPipeline);
+
+    // create texture image and associated DescriptorSets and binding
+    auto texture = vsg::DescriptorImage::create(vsg::Sampler::create(), textureData, 0, 0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
+
+    auto descriptorSet = vsg::DescriptorSet::create(descriptorSetLayouts, vsg::Descriptors{texture});
+    auto bindDescriptorSets = vsg::BindDescriptorSets::create(VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline->getPipelineLayout(), 0, vsg::DescriptorSets{descriptorSet});
+
+    // create StateGroup as the root of the scene/command graph to hold the GraphicsProgram, and binding of Descriptors to decorate the whole graph
+    auto scenegraph = vsg::StateGroup::create();
+    scenegraph->add(bindGraphicsPipeline);
+    scenegraph->add(bindDescriptorSets);
+
+    // set up model transformation node
+    auto transform = vsg::MatrixTransform::create(); // VK_SHADER_STAGE_VERTEX_BIT
+
+    // add transform to root of the scene graph
+    scenegraph->addChild(transform);
+
+    // set up vertex and index arrays
+    auto vertices = vsg::vec3Array::create(
+    {
+        {-0.5f, -0.5f, 0.0f},
+        {0.5f,  -0.5f, 0.05f},
+        {0.5f , 0.5f, 0.0f},
+        {-0.5f, 0.5f, 0.0f},
+        {-0.5f, -0.5f, -0.5f},
+        {0.5f,  -0.5f, -0.5f},
+        {0.5f , 0.5f, -0.5},
+        {-0.5f, 0.5f, -0.5}
+    }); // VK_FORMAT_R32G32B32_SFLOAT, VK_VERTEX_INPUT_RATE_INSTANCE, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
+
+    auto colors = vsg::vec3Array::create(
+    {
+        {1.0f, 0.0f, 0.0f},
+        {0.0f, 1.0f, 0.0f},
+        {0.0f, 0.0f, 1.0f},
+        {1.0f, 1.0f, 1.0f},
+        {1.0f, 0.0f, 0.0f},
+        {0.0f, 1.0f, 0.0f},
+        {0.0f, 0.0f, 1.0f},
+        {1.0f, 1.0f, 1.0f},
+    }); // VK_FORMAT_R32G32B32_SFLOAT, VK_VERTEX_INPUT_RATE_VERTEX, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
+
+    auto texcoords = vsg::vec2Array::create(
+    {
+        {0.0f, 0.0f},
+        {1.0f, 0.0f},
+        {1.0f, 1.0f},
+        {0.0f, 1.0f},
+        {0.0f, 0.0f},
+        {1.0f, 0.0f},
+        {1.0f, 1.0f},
+        {0.0f, 1.0f}
+    }); // VK_FORMAT_R32G32_SFLOAT, VK_VERTEX_INPUT_RATE_VERTEX, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
+
+    auto indices = vsg::ushortArray::create(
+    {
+        0, 1, 2,
+        2, 3, 0,
+        4, 5, 6,
+        6, 7, 4
+    }); // VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
+
+    // setup geometry
+    auto drawCommands = vsg::Commands::create();
+    drawCommands->addChild(vsg::BindVertexBuffers::create(0, vsg::DataList{vertices, colors, texcoords}));
+    drawCommands->addChild(vsg::BindIndexBuffer::create(indices));
+    drawCommands->addChild(vsg::DrawIndexed::create(12, 1, 0, 0, 0));
+
+    // add drawCommands to transform
+    transform->addChild(drawCommands);
+
+    return scenegraph;
+}
+
 
 int main(int argc, char** argv)
 {
@@ -46,25 +181,17 @@ int main(int argc, char** argv)
     auto useDatabasePager = arguments.read("--pager");
     auto maxPageLOD = arguments.value(-1, "--max-plod");
     auto powerWall = arguments.read({"--power-wall","--pw"});
+    auto sharedScene = arguments.read({"--shared","-s"});
 
     if (arguments.errors()) return arguments.writeErrorMessages(std::cerr);
 
-    if (argc<=1)
-    {
-        std::cout<<"Please specify a model to load on command line."<<std::endl;
-        return 1;
-    }
+    vsg::Path filename;
+    if (argc>1) filename = arguments[1];
 
-    // read shaders
-    vsg::Paths searchPaths = vsg::getEnvPaths("VSG_FILE_PATH");
-
-    vsg::Path filename = arguments[1];
-    vsg::Path path = vsg::filePath(filename);
-
-    auto vsg_scene = vsg::read_cast<vsg::Node>(filename);
+    auto vsg_scene = createScene(filename);
     if (!vsg_scene)
     {
-        std::cout<<"Unable to load model "<<filename<<std::endl;
+        std::cout<<"Unable to load model."<<std::endl;
         return 1;
     }
 
@@ -141,7 +268,6 @@ int main(int argc, char** argv)
             return 1;
         }
 
-        auto local_scene = vsg::read_cast<vsg::Node>(filename);
         vsg::ref_ptr<vsg::Camera> camera;
         if (powerWall)
         {
@@ -158,6 +284,8 @@ int main(int argc, char** argv)
             auto relative_view = vsg::RelativeView::create(lookAt, vsg::rotate(angle, 0.0, 1.0, 0.0));
             camera = vsg::Camera::create(perspective, relative_view, vsg::ViewportState::create(window->extent2D()));
         }
+
+        auto local_scene = sharedScene ? vsg_scene : createScene(filename);
 
         viewer->assignRecordAndSubmitTaskAndPresentation({vsg::createCommandGraphForView(window, camera, local_scene)}, databasePager);
         viewer->addWindow(window);
