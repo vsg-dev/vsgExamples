ffb3f5e31306a6fb3bf4dfa6daf27ebe53dad420
Robert Osfield <robert@openscenegraph.com>
2020-04-25 18:02:50 +0100

    Enabled option of using float or double maths in intersection calculation

--- Desktop/vsgintersection/LineSegmentIntersector.cpp
+++ Desktop/vsgintersection/LineSegmentIntersector.cpp
@@ -16,11 +16,24 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 using namespace vsg;
 
+template<typename V>
 struct TriangleIntersector
 {
-    using value_type = float;
+    using value_type = V;
+    using vec_type = t_vec3<value_type>;
 
-    TriangleIntersector(const vec3& s, const vec3& e)
+    dvec3 start;
+    dvec3 end;
+
+    vec_type _d;
+    value_type _length;
+    value_type _inverse_length;
+
+    vec_type _d_invX;
+    vec_type _d_invY;
+    vec_type _d_invZ;
+
+    TriangleIntersector(const dvec3& s, const dvec3& e)
     {
         start = s;
         end = e;
@@ -30,98 +43,82 @@ struct TriangleIntersector
         _inverse_length = (_length!=0.0) ? 1.0/_length : 0.0;
         _d *= _inverse_length;
 
-        _d_invX = _d.x!=0.0 ? _d/_d.x : vec3(0.0, 0.0, 0.0);
-        _d_invY = _d.y!=0.0 ? _d/_d.y : vec3(0.0, 0.0, 0.0);
-        _d_invZ = _d.z!=0.0 ? _d/_d.z : vec3(0.0, 0.0, 0.0);
+        _d_invX = _d.x!=0.0 ? _d/_d.x : vec_type(0.0, 0.0, 0.0);
+        _d_invY = _d.y!=0.0 ? _d/_d.y : vec_type(0.0, 0.0, 0.0);
+        _d_invZ = _d.z!=0.0 ? _d/_d.z : vec_type(0.0, 0.0, 0.0);
     }
 
     /// intersect with a single triangle
-    bool intersect(const vec3& v0, const vec3& v1, const vec3& v2);
+    bool intersect(const vec3& v0, const vec3& v1, const vec3& v2)
+    {
+        vec_type T = vec_type(start) - vec_type(v0);
+        vec_type E2 = vec_type(v2) - vec_type(v0);
+        vec_type E1 = vec_type(v1) - vec_type(v0);
 
-    vec3 start;
-    vec3 end;
+        vec_type P =  cross(_d , E2);
 
-    vec3        _d;
-    value_type  _length;
-    value_type  _inverse_length;
+        value_type det = dot(P, E1);
 
-    vec3        _d_invX;
-    vec3        _d_invY;
-    vec3        _d_invZ;
+        value_type r, r0, r1, r2;
+
+        const value_type epsilon = 1e-10;
+        if (det>epsilon)
+        {
+            value_type u = dot(P, T);
+            if (u<0.0 || u>det) return false;
+
+            vec_type Q = cross(T , E1);
+            value_type v = dot(Q, _d);
+            if (v<0.0 || v>det) return false;
+
+            if ((u+v)> det) return false;
+
+            value_type inv_det = 1.0/det;
+            value_type t = dot(Q, E2)*inv_det;
+            if (t<0.0 || t>_length) return false;
+
+            u *= inv_det;
+            v *= inv_det;
+
+            r0 = 1.0-u-v;
+            r1 = u;
+            r2 = v;
+            r = t * _inverse_length;
+        }
+        else if (det<-epsilon)
+        {
+            value_type u = dot(P, T);
+            if (u>0.0 || u<det) return false;
+
+            vec_type Q = cross(T, E1);
+            value_type v = dot(Q, _d);
+            if (v>0.0 || v<det) return false;
+
+            if ((u+v) < det) return false;
+
+            value_type inv_det = 1.0/det;
+            value_type t = dot(Q, E2)*inv_det;
+            if (t<0.0 || t>_length) return false;
+
+            u *= inv_det;
+            v *= inv_det;
+
+            r0 = 1.0-u-v;
+            r1 = u;
+            r2 = v;
+            r = t * _inverse_length;
+        }
+        else
+        {
+            return false;
+        }
+
+        // TODO : handle hit
+
+        std::cout<<"We have a hit r="<<r<<", r0 = "<<r0<<", r1 = "<<r1<<", r2 = "<<r2<<" total = "<<(r0+r1+r2)<<std::endl;
+        return true;
+    }
 };
-
-bool TriangleIntersector::intersect(const vec3& v0, const vec3& v1, const vec3& v2)
-{
-    // const vec3& ls = startend.first;
-    // const vec3& le = startend.second;
-
-    vec3 T = start - v0;
-    vec3 E2 = v2 - v0;
-    vec3 E1 = v1 - v0;
-
-    vec3 P =  cross(_d , E2);
-
-    value_type det = dot(P, E1);
-
-    value_type r, r0, r1, r2;
-
-    const value_type epsilon = 1e-10;
-    if (det>epsilon)
-    {
-        value_type u = dot(P, T);
-        if (u<0.0 || u>det) return false;
-
-        vec3 Q = cross(T , E1);
-        value_type v = dot(Q, _d);
-        if (v<0.0 || v>det) return false;
-
-        if ((u+v)> det) return false;
-
-        value_type inv_det = 1.0/det;
-        value_type t = dot(Q, E2)*inv_det;
-        if (t<0.0 || t>_length) return false;
-
-        u *= inv_det;
-        v *= inv_det;
-
-        r0 = 1.0-u-v;
-        r1 = u;
-        r2 = v;
-        r = t * _inverse_length;
-    }
-    else if (det<-epsilon)
-    {
-        value_type u = dot(P, T);
-        if (u>0.0 || u<det) return false;
-
-        vec3 Q = cross(T, E1);
-        value_type v = dot(Q, _d);
-        if (v>0.0 || v<det) return false;
-
-        if ((u+v) < det) return false;
-
-        value_type inv_det = 1.0/det;
-        value_type t = dot(Q, E2)*inv_det;
-        if (t<0.0 || t>_length) return false;
-
-        u *= inv_det;
-        v *= inv_det;
-
-        r0 = 1.0-u-v;
-        r1 = u;
-        r2 = v;
-        r = t * _inverse_length;
-    }
-    else
-    {
-        return false;
-    }
-
-    // TODO : handle hit
-
-    std::cout<<"We have a hit"<<std::endl;
-    return true;
-}
 
 LineSegmentIntersector::LineSegmentIntersector(const dvec3& s, const dvec3& e) :
     start(s),
@@ -189,9 +186,7 @@ bool LineSegmentIntersector::intersect(VkPrimitiveTopology topology, const vsg::
     {
         case(VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST) :
         {
-            vec3 f_start(start);
-            vec3 f_end(end);
-            TriangleIntersector triIntsector(f_start, f_end);
+            TriangleIntersector<double> triIntsector(start, end);
             for(uint32_t i = firstIndex; i<endIndex; i+=3)
             {
                 triIntsector.intersect( vertices->at(us_indices->at(i)), vertices->at(us_indices->at(i+1)), vertices->at(us_indices->at(i+2)));
