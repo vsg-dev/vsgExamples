961e41bc7b05f7b71f1d6b75d89dd9d7b37b3287
Robert Osfield <robert@openscenegraph.com>
2020-05-16 13:59:02 +0100

    Adopted more conservative nearfar ratio

--- Desktop/vsgrendertotexture/vsgrendertotexture.cpp
+++ Desktop/vsgrendertotexture/vsgrendertotexture.cpp
@@ -379,7 +379,7 @@ vsg::ref_ptr<vsg::Camera> createCameraForScene(vsg::Node* scenegraph, const VkEx
     scenegraph->accept(computeBounds);
     vsg::dvec3 centre = (computeBounds.bounds.min+computeBounds.bounds.max)*0.5;
     double radius = vsg::length(computeBounds.bounds.max-computeBounds.bounds.min)*0.6;
-    double nearFarRatio = 0.0001;
+    double nearFarRatio = 0.001;
 
     // set up the camera
     auto lookAt = vsg::LookAt::create(centre+vsg::dvec3(0.0, -radius*3.5, 0.0),
