ef46ea7bc9af613bcd3bbc162562c8151413e57a
Robert Osfield <robert@openscenegraph.com>
2020-05-28 07:49:16 +0100

    Added vsgorto example based on Tim Moore's original modifications of vsgviewer

---
+++ Desktop/vsgortho/vsgortho.cpp
@@ -0,0 +1,106 @@
+#include <vsg/all.h>
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <algorithm>
+
+#include "AnimationPath.h"
+
+int main(int argc, char** argv)
+{
+    // set up defaults and read command line arguments to override them
+    auto windowTraits = vsg::WindowTraits::create();
+    windowTraits->windowTitle = "vsgviewer";
+
+    // set up defaults and read command line arguments to override them
+    vsg::CommandLine arguments(&argc, argv);
+    windowTraits->debugLayer = arguments.read({"--debug","-d"});
+    windowTraits->apiDumpLayer = arguments.read({"--api","-a"});
+    if (arguments.read({"--fullscreen", "--fs"})) windowTraits->fullscreen = true;
+    if (arguments.read({"--window", "-w"}, windowTraits->width, windowTraits->height)) { windowTraits->fullscreen = false; }
+
+    if (arguments.errors()) return arguments.writeErrorMessages(std::cerr);
+
+    if (arguments.argc() <= 1)
+    {
+        std::cout<<"Please specify model on cmmmand line."<<std::endl;
+        return 1;
+    }
+
+    vsg::Path filename = arguments[1];
+
+    auto vsg_scene = vsg::read_cast<vsg::Node>(filename);
+    if (!vsg_scene)
+    {
+        std::cout<<"Please specify a 3d model file on the command line."<<std::endl;
+        return 1;
+    }
+
+    // create the viewer and assign window(s) to it
+    auto viewer = vsg::Viewer::create();
+
+    vsg::ref_ptr<vsg::Window> window(vsg::Window::create(windowTraits));
+    if (!window)
+    {
+        std::cout<<"Could not create windows."<<std::endl;
+        return 1;
+    }
+
+    viewer->addWindow(window);
+
+    // compute the bounds of the scene graph to help position camera
+    vsg::ComputeBounds computeBounds;
+    vsg_scene->accept(computeBounds);
+    vsg::dvec3 centre = (computeBounds.bounds.min+computeBounds.bounds.max)*0.5;
+    double radius = vsg::length(computeBounds.bounds.max-computeBounds.bounds.min)*0.6;
+    double nearFarRatio = 0.001;
+
+
+    // set up the camera
+    auto lookAt = vsg::LookAt::create(centre+vsg::dvec3(0.0, -radius*3.5, 0.0), centre, vsg::dvec3(0.0, 0.0, 1.0));
+
+    double aspectRatio = static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height);
+    double halfDim = radius * 1.1;
+    double halfHeight, halfWidth;
+    if (window->extent2D().width > window->extent2D().height)
+    {
+        halfHeight = halfDim;
+        halfWidth = halfDim * aspectRatio;
+    }
+    else
+    {
+        halfWidth = halfDim;
+        halfHeight = halfDim / aspectRatio;
+    }
+    auto projection = vsg::Orthographic::create(-halfWidth, halfWidth, -halfHeight, halfHeight,
+                                                nearFarRatio*radius, radius * 4.5);
+
+    auto camera = vsg::Camera::create(projection, lookAt, vsg::ViewportState::create(window->extent2D()));
+
+
+    // add close handler to respond the close window button and pressing escape
+    viewer->addEventHandler(vsg::CloseHandler::create(viewer));
+    viewer->addEventHandler(vsg::Trackball::create(camera));
+
+    auto commandGraph = vsg::createCommandGraphForView(window, camera, vsg_scene);
+    viewer->assignRecordAndSubmitTaskAndPresentation({commandGraph});
+
+    viewer->compile();
+
+    // rendering main loop
+    while (viewer->advanceToNextFrame())
+    {
+        // pass any events into EventHandlers assigned to the Viewer
+        viewer->handleEvents();
+
+        viewer->update();
+
+        viewer->recordAndSubmit();
+
+        viewer->present();
+    }
+
+    // clean up done automatically thanks to ref_ptr<>
+    return 0;
+}
