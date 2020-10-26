857a7d1b041ca210769a99e80b7546850fa20192
Robert Osfield <robert@openscenegraph.com>
2020-10-26 16:57:58 +0000

    Added initial cut of vsgmultiviews example

---
+++ Desktop/vsgmultiviews/vsgmultiviews.cpp
@@ -0,0 +1,133 @@
+#include <vsg/all.h>
+
+#include <iostream>
+#include <chrono>
+
+#ifdef USE_VSGXCHANGE
+#include <vsgXchange/ReaderWriter_all.h>
+#endif
+
+vsg::ref_ptr<vsg::Camera> createCameraForScene(vsg::Node* scenegraph, int32_t x, int32_t y, uint32_t width, uint32_t height)
+{
+    // compute the bounds of the scene graph to help position camera
+    vsg::ComputeBounds computeBounds;
+    scenegraph->accept(computeBounds);
+    vsg::dvec3 centre = (computeBounds.bounds.min+computeBounds.bounds.max)*0.5;
+    double radius = vsg::length(computeBounds.bounds.max-computeBounds.bounds.min)*0.6;
+    double nearFarRatio = 0.001;
+
+    // set up the camera
+    auto lookAt = vsg::LookAt::create(centre+vsg::dvec3(0.0, -radius*3.5, 0.0),
+                                      centre, vsg::dvec3(0.0, 0.0, 1.0));
+
+    auto perspective = vsg::Perspective::create(30.0, static_cast<double>(width) / static_cast<double>(height),
+                                                nearFarRatio*radius, radius * 4.5);
+
+    VkViewport viewport{static_cast<float>(x), static_cast<float>(y), static_cast<float>(width), static_cast<float>(height), 0.0f, 1.0f};
+    VkRect2D scissor{VkOffset2D{x, y}, VkExtent2D{width, height}};
+
+    auto viewportstate = vsg::ViewportState::create();
+    viewportstate->viewports.push_back(viewport);
+    viewportstate->scissors.push_back(scissor);
+
+    return vsg::Camera::create(perspective, lookAt, viewportstate);
+}
+
+
+int main(int argc, char** argv)
+{
+    // set up defaults and read command line arguments to override them
+    vsg::CommandLine arguments(&argc, argv);
+
+    auto windowTraits = vsg::WindowTraits::create();
+    windowTraits->windowTitle = "rendertotexture";
+    windowTraits->debugLayer = arguments.read({"--debug","-d"});
+    windowTraits->apiDumpLayer = arguments.read({"--api","-a"});
+    if (arguments.read({"--window", "-w"}, windowTraits->width, windowTraits->height)) { windowTraits->fullscreen = false; }
+
+    bool separateCommandGraph = arguments.read("-s");
+    bool multiThreading = arguments.read("--mt");
+
+    if (arguments.errors()) return arguments.writeErrorMessages(std::cerr);
+
+    auto options = vsg::Options::create();
+#ifdef USE_VSGXCHANGE
+    // add use of vsgXchange's support for reading and writing 3rd party file formats
+    options->readerWriter = vsgXchange::ReaderWriter_all::create();
+#endif
+
+    vsg::ref_ptr<vsg::Node> scenegraph;
+    vsg::ref_ptr<vsg::Node> scenegraph2;
+    if (argc>1)
+    {
+        vsg::Path filename = arguments[1];
+        scenegraph = vsg::read_cast<vsg::Node>(filename, options);
+        scenegraph2 = vsg::read_cast<vsg::Node>(filename, options);
+    }
+
+    if (argc>2)
+    {
+        vsg::Path filename = arguments[2];
+        scenegraph2 = vsg::read_cast<vsg::Node>(filename, options);
+    }
+
+    if (!scenegraph)
+    {
+        std::cout<<"Please specify a valid model on command line"<<std::endl;
+        return 1;
+    }
+
+    // create the viewer and assign window(s) to it
+    auto viewer = vsg::Viewer::create();
+
+    auto window = vsg::Window::create(windowTraits);
+    if (!window)
+    {
+        std::cout<<"Could not create windows."<<std::endl;
+        return 1;
+    }
+
+    viewer->addWindow(window);
+
+    uint32_t width = window->extent2D().width;
+    uint32_t height = window->extent2D().height;
+
+    auto main_camera = createCameraForScene(scenegraph, 0, 0, width, height);
+    auto main_RenderGraph = vsg::createRenderGraphForView(window, main_camera, scenegraph);
+
+    auto map_camera = createCameraForScene(scenegraph2, (width*3)/4, 0, width/4, height/4);
+    auto map_RenderGraph = vsg::createRenderGraphForView(window, map_camera, scenegraph2);
+    map_RenderGraph->renderArea = map_camera->getRenderArea();
+    map_RenderGraph->clearValues[0].color = {0.2f, 0.2f, 0.2f, 1.0f};
+
+    // add close handler to respond the close window button and pressing escape
+    viewer->addEventHandler(vsg::CloseHandler::create(viewer));
+
+    viewer->addEventHandler(vsg::Trackball::create(main_camera));
+
+
+    auto commandGraph = vsg::CommandGraph::create(window);
+    commandGraph->addChild(main_RenderGraph);
+    commandGraph->addChild(map_RenderGraph);
+
+    viewer->assignRecordAndSubmitTaskAndPresentation({commandGraph});
+
+    viewer->compile();
+
+
+    // rendering main loop
+    while (viewer->advanceToNextFrame())
+    {
+        // pass any events into EventHandlers assigned to the Viewer
+        viewer->handleEvents();
+
+        viewer->update();
+
+        viewer->recordAndSubmit();
+
+        viewer->present();
+    }
+
+    // clean up done automatically thanks to ref_ptr<>
+    return 0;
+}
