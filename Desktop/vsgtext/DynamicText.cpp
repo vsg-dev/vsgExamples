5e8b4eb82544602f6b7836bd2d0b7ac254d349a8
Robert Osfield <robert@openscenegraph.com>
2020-11-25 16:36:45 +0000

    Moved the rendering scene graph setup into GpuLayoutTechnique.

--- Desktop/vsgtext/DynamicText.cpp
+++ Desktop/vsgtext/DynamicText.cpp
@@ -11,43 +11,18 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 </editor-fold> */
 
 #include <vsg/core/Array2D.h>
-#include <vsg/commands/BindVertexBuffers.h>
-#include <vsg/commands/Commands.h>
-#include <vsg/commands/Draw.h>
 #include <vsg/io/read.h>
 #include <vsg/io/write.h>
 #include <vsg/state/DescriptorImage.h>
 
 #include "DynamicText.h"
+#include "GpuLayoutTechnique.h"
 
 #include <iostream>
 
-//#include "shaders/text_frag.cpp"
-//#include "shaders/text_vert.cpp"
-
 using namespace vsg;
 
-void DynamicText::read(Input& input)
-{
-    Node::read(input);
-
-    input.readObject("font", font);
-    input.readObject("layout", layout);
-    input.readObject("text", text);
-
-    setup();
-}
-
-void DynamicText::write(Output& output) const
-{
-    Node::write(output);
-
-    output.writeObject("font", font);
-    output.writeObject("layout", layout);
-    output.writeObject("text", text);
-}
-
-DynamicText::FontState::FontState(Font* font)
+FontState::FontState(Font* font)
 {
     {
         auto sampler = Sampler::create();
@@ -77,282 +52,32 @@ DynamicText::FontState::FontState(Font* font)
     }
 }
 
-DynamicText::GpuLayoutState::GpuLayoutState(Font* font)
+void DynamicText::read(Input& input)
 {
-    // load shaders
-    auto vertexShader = read_cast<ShaderStage>("shaders/dynamic_text.vert", font->options);
-    //if (!vertexShader) vertexShader = text_vert(); // fallback to shaders/text_vert.cppp
+    Node::read(input);
 
-    auto fragmentShader = read_cast<ShaderStage>("shaders/dynamic_text.frag", font->options);
-    //if (!fragmentShader) fragmentShader = text_frag(); // fallback to shaders/text_frag.cppp
+    input.readObject("font", font);
+    input.readObject("technique", technique);
+    input.readObject("layout", layout);
+    input.readObject("text", text);
 
-    // compile section
-    ShaderStages stagesToCompile;
-    if (vertexShader && vertexShader->module && vertexShader->module->code.empty()) stagesToCompile.emplace_back(vertexShader);
-    if (fragmentShader && fragmentShader->module && fragmentShader->module->code.empty()) stagesToCompile.emplace_back(fragmentShader);
-
-    uint32_t numTextIndices = 256;
-    vertexShader->specializationConstants = vsg::ShaderStage::SpecializationConstants{
-        {0, vsg::uintValue::create(numTextIndices)} // numTextIndices
-    };
-
-    // Glyph
-    DescriptorSetLayoutBindings descriptorBindings{
-        {0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_FRAGMENT_BIT, nullptr}, // texture atlas
-        {1, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_VERTEX_BIT, nullptr } // glyph matrices
-    };
-
-    auto descriptorSetLayout = DescriptorSetLayout::create(descriptorBindings);
-
-    // set up graphics pipeline
-    DescriptorSetLayoutBindings textArrayDescriptorBindings{
-        {0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1, VK_SHADER_STAGE_VERTEX_BIT, nullptr }, // Layout uniform
-        {1, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1, VK_SHADER_STAGE_VERTEX_BIT, nullptr } // Text uniform
-    };
-
-    textArrayDescriptorSetLayout = DescriptorSetLayout::create(textArrayDescriptorBindings);
-
-    PushConstantRanges pushConstantRanges{
-        {VK_SHADER_STAGE_VERTEX_BIT, 0, 128} // projection view, and model matrices, actual push constant calls autoaatically provided by the VSG's DispatchTraversal
-    };
-
-    VertexInputState::Bindings vertexBindingsDescriptions{
-        VkVertexInputBindingDescription{0, sizeof(vec3), VK_VERTEX_INPUT_RATE_VERTEX},  // vertex data
-    };
-
-    VertexInputState::Attributes vertexAttributeDescriptions{
-        VkVertexInputAttributeDescription{0, 0, VK_FORMAT_R32G32B32_SFLOAT, 0},    // vertex data
-    };
-
-    // alpha blending
-    VkPipelineColorBlendAttachmentState colorBlendAttachment = {};
-    colorBlendAttachment.blendEnable = VK_TRUE;
-    colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT |
-                                          VK_COLOR_COMPONENT_G_BIT |
-                                          VK_COLOR_COMPONENT_B_BIT |
-                                          VK_COLOR_COMPONENT_A_BIT;
-
-    colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;
-    colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
-    colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;
-    colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
-    colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
-    colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;
-
-    auto blending = ColorBlendState::create(ColorBlendState::ColorBlendAttachments{colorBlendAttachment});
-
-    // switch off back face culling
-    auto rasterization = RasterizationState::create();
-    rasterization->cullMode = VK_CULL_MODE_NONE;
-
-    GraphicsPipelineStates pipelineStates{
-        VertexInputState::create(vertexBindingsDescriptions, vertexAttributeDescriptions),
-        InputAssemblyState::create(VK_PRIMITIVE_TOPOLOGY_TRIANGLE_STRIP),
-        MultisampleState::create(),
-        blending,
-        rasterization,
-        DepthStencilState::create()};
-
-    pipelineLayout = PipelineLayout::create(DescriptorSetLayouts{descriptorSetLayout, textArrayDescriptorSetLayout}, pushConstantRanges);
-
-    auto graphicsPipeline = GraphicsPipeline::create(pipelineLayout, ShaderStages{vertexShader, fragmentShader}, pipelineStates);
-    bindGraphicsPipeline = BindGraphicsPipeline::create(graphicsPipeline);
-
-
-    // create texture image and associated DescriptorSets and binding
-    auto fontState = font->getShared<FontState>();
-    auto descriptorSet = DescriptorSet::create(descriptorSetLayout, Descriptors{fontState->textureAtlas, fontState->glyphMetrics});
-
-    bindDescriptorSet = BindDescriptorSet::create(VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 0, descriptorSet);
+    setup();
 }
 
-template<typename T> void assignValue(T& dest, const T& src, bool& updated)
+void DynamicText::write(Output& output) const
 {
-    if (dest==src) return;
-    dest = src;
-    updated = true;
+    Node::write(output);
+
+    output.writeObject("font", font);
+    output.writeObject("technique", technique);
+    output.writeObject("layout", layout);
+    output.writeObject("text", text);
 }
 
 void DynamicText::setup(uint32_t minimumAllocation)
 {
     if (!layout) layout = LeftAlignment::create();
+    if (!technique) technique = GpuLayoutTechnique::create();
 
-    bool textLayoutUpdated = false;
-    bool textArrayUpdated = false;
-
-    struct ConvertString : public ConstVisitor
-    {
-        Font& font;
-        ref_ptr<uintArray>& textArray;
-        bool& updated;
-        size_t minimumSize = 0;
-        size_t size = 0;
-
-        ConvertString(Font& in_font, ref_ptr<uintArray>& in_textArray, bool& in_updated, size_t in_minimumSize) :
-            font(in_font),
-            textArray(in_textArray),
-            updated(in_updated),
-            minimumSize(in_minimumSize) {}
-
-        void allocate(size_t allocationSize)
-        {
-            size = allocationSize;
-
-            if (allocationSize < minimumSize) allocationSize = minimumSize;
-            if (!textArray || allocationSize > textArray->valueCount())
-            {
-                updated = true;
-                textArray = uintArray::create(allocationSize);
-            }
-        }
-
-        void apply(const stringValue& text) override
-        {
-            allocate(text.value().size());
-
-            auto itr = textArray->begin();
-            for (auto& c : text.value())
-            {
-                assignValue(*(itr++), font.glyphIndexForCharcode(uint8_t(c)), updated);
-            }
-        }
-        void apply(const ubyteArray& text) override
-        {
-            allocate(text.valueCount());
-
-            auto itr = textArray->begin();
-            for (auto& c : text)
-            {
-                assignValue(*(itr++), font.glyphIndexForCharcode(c), updated);
-            }
-        }
-        void apply(const ushortArray& text) override
-        {
-            allocate(text.valueCount());
-
-            auto itr = textArray->begin();
-            for (auto& c : text)
-            {
-                assignValue(*(itr++), font.glyphIndexForCharcode(c), updated);
-            }
-        }
-        void apply(const uintArray& text) override
-        {
-            allocate(text.valueCount());
-
-            auto itr = textArray->begin();
-            for (auto& c : text)
-            {
-                assignValue(*(itr++), font.glyphIndexForCharcode(c), updated);
-            }
-        }
-    };
-
-#if 1
-    TextQuads quads;
-    layout->layout(text, *font, quads);
-#endif
-
-    if (!renderingBackend)
-    {
-#if 1
-        if (quads.empty()) return;
-#endif
-        renderingBackend = GpuLayoutBackend::create();
-    }
-
-    ConvertString convert(*font, renderingBackend->textArray, textArrayUpdated, minimumAllocation);
-    text->accept(convert);
-
-    auto textArray = convert.textArray;
-    size_t num_quads = convert.size;
-
-    // TODO need to reallocate DescriptorBuffer if textArray changes size?
-    auto& textDescriptor = renderingBackend->textDescriptor;
-    if (!textDescriptor) textDescriptor = DescriptorBuffer::create(textArray, 1, 0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER);
-
-
-    // set up the layout data in a form digestable by the GPU.
-    auto& layoutValue = renderingBackend->layoutValue;
-    if (!layoutValue) layoutValue = TextLayoutValue::create();
-
-    auto& layoutStruct = layoutValue->value();
-    if (auto leftAlignment = layout.cast<LeftAlignment>(); leftAlignment)
-    {
-        assignValue(layoutStruct.position, leftAlignment->position, textLayoutUpdated);
-        assignValue(layoutStruct.horizontal, leftAlignment->horizontal, textLayoutUpdated);
-        assignValue(layoutStruct.vertical, leftAlignment->vertical, textLayoutUpdated);
-        assignValue(layoutStruct.color, leftAlignment->color, textLayoutUpdated);
-        assignValue(layoutStruct.outlineColor, leftAlignment->outlineColor, textLayoutUpdated);
-        assignValue(layoutStruct.outlineWidth, leftAlignment->outlineWidth, textLayoutUpdated);
-    }
-
-    auto& layoutDescriptor = renderingBackend->layoutDescriptor;
-    if (!layoutDescriptor) layoutDescriptor = DescriptorBuffer::create(layoutValue, 0, 0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER);
-
-    auto& vertices = renderingBackend->vertices;
-    auto& draw = renderingBackend->draw;
-
-    size_t num_vertices = 4;
-
-    if (!vertices || num_vertices > vertices->size())
-    {
-        vertices = vec3Array::create(num_vertices);
-
-        float leadingEdgeGradient = 0.1f;
-
-        vertices->set(0, vec3(0.0f, 1.0f, 2.0*leadingEdgeGradient));
-        vertices->set(1, vec3(0.0f, 0.0f, leadingEdgeGradient));
-        vertices->set(2, vec3(1.0f, 1.0f, leadingEdgeGradient));
-        vertices->set(3, vec3(1.0f, 0.0f, 0.0f));
-    }
-
-    if (!draw)
-        draw = Draw::create(4, num_quads, 0, 0);
-    else
-        draw->instanceCount = num_quads;
-
-    // std::cout<<"drawIndexed->instanceCount = "<<drawIndexed->instanceCount<<std::endl;
-
-    // create StateGroup as the root of the scene/command graph to hold the GraphicsProgram, and binding of Descriptors to decorate the whole graph
-    auto& scenegraph = renderingBackend->stategroup;
-    if (!scenegraph)
-    {
-        scenegraph = StateGroup::create();
-
-        // set up state related objects if they haven't lready been assigned
-        auto& sharedGpuLayoutState = renderingBackend->sharedRenderingState;
-        if (!sharedGpuLayoutState) renderingBackend->sharedRenderingState = font->getShared<GpuLayoutState>();
-
-        if (sharedGpuLayoutState->bindGraphicsPipeline) scenegraph->add(sharedGpuLayoutState->bindGraphicsPipeline);
-        if (sharedGpuLayoutState->bindDescriptorSet) scenegraph->add(sharedGpuLayoutState->bindDescriptorSet);
-
-        // set up graphics pipeline
-        auto textDescriptorSet = DescriptorSet::create(sharedGpuLayoutState->textArrayDescriptorSetLayout, Descriptors{layoutDescriptor, textDescriptor});
-        renderingBackend->bindTextDescriptorSet = vsg::BindDescriptorSet::create(VK_PIPELINE_BIND_POINT_GRAPHICS, sharedGpuLayoutState->pipelineLayout, 1, textDescriptorSet);
-        scenegraph->add(renderingBackend->bindTextDescriptorSet);
-
-        auto& bindVertexBuffers = renderingBackend->bindVertexBuffers;
-        bindVertexBuffers = BindVertexBuffers::create(0, DataList{vertices});
-
-        // setup geometry
-        auto drawCommands = Commands::create();
-        drawCommands->addChild(bindVertexBuffers);
-        drawCommands->addChild(draw);
-
-        scenegraph->addChild(drawCommands);
-
-        //vsg::write(scenegraph, "scenegraph.vsgt");
-    }
-    else
-    {
-        if (textArrayUpdated)
-        {
-            textDescriptor->copyDataListToBuffers();
-        }
-        if (textLayoutUpdated)
-        {
-            layoutDescriptor->copyDataListToBuffers();
-        }
-    }
+    technique->setup(this, minimumAllocation);
 }
--- Desktop/vsgtext/GpuLayoutTechnique.h
+++ Desktop/vsgtext/GpuLayoutTechnique.h
@@ -13,70 +13,60 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 </editor-fold> */
 
 #include <vsg/nodes/Node.h>
+#include <vsg/state/StateGroup.h>
+#include <vsg/state/DescriptorBuffer.h>
+#include <vsg/commands/Draw.h>
+#include <vsg/commands/BindVertexBuffers.h>
 #include <vsg/text/Font.h>
 #include <vsg/text/TextLayout.h>
 
+#include "DynamicText.h"
+
 namespace vsg
 {
-
-    struct LayoutStruct
-    {
-        vec3 position = vec3(0.0f, 0.0f, 0.0f); float pad0;
-        vec3 horizontal = vec3(1.0f, 0.0f, 0.0f); float pad1;
-        vec3 vertical = vec3(0.0f, 1.0f, 0.0f); float pad2;
-        vec4 color = vec4(1.0f, 1.0f, 1.0f, 1.0f);
-        vec4 outlineColor = vec4(0.0f, 0.0f, 0.0f, 1.0f);
-        float outlineWidth = 0.0f;
-    };
-
-    VSG_value(TextLayoutValue, LayoutStruct);
-
-    class DynamicText;
-
-    /// Wraooer for Font::textureAtlas data.
-    struct VSG_DECLSPEC FontState : public Inherit<Object, FontState>
-    {
-        FontState(Font* font);
-        bool match() const { return true; }
-
-        ref_ptr<DescriptorImage> textureAtlas;
-        ref_ptr<DescriptorImage> glyphMetrics;
-    };
-
-    class TextTechnique : public Inherit<Object, TextTechnique>
-    {
-    public:
-        virtual void setup(DynamicText* text, uint32_t minimumAllocation = 0) = 0;
-    };
-
-    class VSG_DECLSPEC DynamicText : public Inherit<Node, DynamicText>
+    class VSG_DECLSPEC GpuLayoutTechnique : public Inherit<TextTechnique, GpuLayoutTechnique>
     {
     public:
         template<class N, class V>
         static void t_traverse(N& node, V& visitor)
         {
-            if (node.technique) node.technique->accept(visitor);
+            node.scenegraph->accept(visitor);
         }
 
         void traverse(Visitor& visitor) override { t_traverse(*this, visitor); }
         void traverse(ConstVisitor& visitor) const override { t_traverse(*this, visitor); }
         void traverse(RecordTraversal& visitor) const override { t_traverse(*this, visitor); }
 
-        void read(Input& input) override;
-        void write(Output& output) const override;
+        void setup(DynamicText* text, uint32_t minimumAllocation = 0) override;
 
-        /// settings
-        ref_ptr<Font> font;
-        ref_ptr<TextTechnique> technique;
-        ref_ptr<TextLayout> layout;
-        ref_ptr<Data> text;
+        /// rendering state used to set up grahics pipeline and descriptor sets, assigned to Font to allow it be be shared
+        struct VSG_DECLSPEC GpuLayoutState : public Inherit<Object, GpuLayoutState>
+        {
+            GpuLayoutState(Font* font);
 
-        /// create the rendering backend.
-        /// minimumAllocation provides a hint for the minimum number of glyphs to allocate space for.
-        virtual void setup(uint32_t minimumAllocation = 0);
+            bool match() const { return true; }
 
-    protected:
+            ref_ptr<PipelineLayout> pipelineLayout;
+            ref_ptr<DescriptorSetLayout> textArrayDescriptorSetLayout;
+            ref_ptr<BindGraphicsPipeline> bindGraphicsPipeline;
+            ref_ptr<BindDescriptorSet> bindDescriptorSet;
+        };
+
+        // implementation data structure
+        ref_ptr<StateGroup> scenegraph;
+
+        ref_ptr<vec3Array> vertices;
+        ref_ptr<Draw> draw;
+
+        ref_ptr<uintArray> textArray;
+        ref_ptr<TextLayoutValue> layoutValue;
+        ref_ptr<DescriptorBuffer> textDescriptor;
+        ref_ptr<DescriptorBuffer> layoutDescriptor;
+        ref_ptr<BindDescriptorSet> bindTextDescriptorSet;
+
+        ref_ptr<BindVertexBuffers> bindVertexBuffers;
+        ref_ptr<GpuLayoutState> sharedRenderingState;
     };
-    VSG_type_name(vsg::DynamicText);
+    VSG_type_name(vsg::GpuLayoutTechnique);
 
 } // namespace vsg
