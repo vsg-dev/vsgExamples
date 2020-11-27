5dde0c9040b9b86de5943903798e322201426e92
Robert Osfield <robert@openscenegraph.com>
2020-11-27 16:38:11 +0000

    Implemented fisrt cut Input event capture

--- Desktop/vsginput/vsginput.cpp
+++ Desktop/vsginput/vsginput.cpp
@@ -1,6 +1,43 @@
 #include <vsg/all.h>
 #include <iostream>
 
+class InputHandler : public vsg::Inherit<vsg::Visitor, InputHandler>
+{
+public:
+
+    InputHandler()
+    {
+    }
+
+    void apply(vsg::KeyPressEvent& keyPress) override
+    {
+        std::cout<<"vsg::KeyPressEvent keyBase = "<<keyPress.keyBase<<", keyModified = "<<keyPress.keyModified<<std::endl;
+    }
+
+    void apply(vsg::KeyReleaseEvent& keyRelease) override
+    {
+        std::cout<<"vsg::KeyReleaseEvent keyBase = "<<keyRelease.keyBase<<", keyModified = "<<keyRelease.keyModified<<std::endl;
+    }
+
+    void apply(vsg::MoveEvent& moveEvent) override
+    {
+        std::cout<<"vsg::MoveEvent x= "<<moveEvent.x<<", y= "<<moveEvent.y<<", mask = "<<moveEvent.mask<<std::endl;
+    }
+
+    void apply(vsg::ButtonPressEvent& buttonPressEvent) override
+    {
+        std::cout<<"vsg::ButtonPressEvent x= "<<buttonPressEvent.x<<", y= "<<buttonPressEvent.y<<", mask = "<<buttonPressEvent.mask<<", button = "<<buttonPressEvent.button<<std::endl;
+    }
+
+    void apply(vsg::ButtonReleaseEvent& buttonReleaseEvent) override
+    {
+        std::cout<<"vsg::ButtonPressEvent x= "<<buttonReleaseEvent.x<<", y= "<<buttonReleaseEvent.y<<", mask = "<<buttonReleaseEvent.mask<<", button = "<<buttonReleaseEvent.button<<std::endl;
+    }
+
+protected:
+
+};
+
 int main(int argc, char** argv)
 {
     // set up defaults and read command line arguments to override them
@@ -10,141 +47,40 @@ int main(int argc, char** argv)
     windowTraits->debugLayer = arguments.read({"--debug","-d"});
     windowTraits->apiDumpLayer = arguments.read({"--api","-a"});
     arguments.read({"--window", "-w"}, windowTraits->width, windowTraits->height);
+    auto font_filename = arguments.value(std::string("fonts/times.vsgb"), "--font");
 
     if (arguments.errors()) return arguments.writeErrorMessages(std::cerr);
 
     // set up search paths to SPIRV shaders and textures
     vsg::Paths searchPaths = vsg::getEnvPaths("VSG_FILE_PATH");
 
-    // load shaders
-    vsg::ref_ptr<vsg::ShaderStage> vertexShader = vsg::ShaderStage::read(VK_SHADER_STAGE_VERTEX_BIT, "main", vsg::findFile("shaders/vert_PushConstants.spv", searchPaths));
-    vsg::ref_ptr<vsg::ShaderStage> fragmentShader = vsg::ShaderStage::read(VK_SHADER_STAGE_FRAGMENT_BIT, "main", vsg::findFile("shaders/frag_PushConstants.spv", searchPaths));
-    if (!vertexShader || !fragmentShader)
+    auto options = vsg::Options::create();
+    options->paths = searchPaths;
+
+
+    auto scenegraph = vsg::Group::create();
+
+    auto font = vsg::read_cast<vsg::Font>(font_filename, options);
+
+    auto dynamic_text_label = vsg::stringValue::create("Please press keys and move/click mouse buttons.\n\nWatch the console output to see the event\ninformation displayed.");
+    auto dynamic_text_layout = vsg::LeftAlignment::create();
+    auto dynamic_text = vsg::Text::create();
     {
-        std::cout<<"Could not create shaders."<<std::endl;
-        return 1;
+//        dynamic_text->technique = vsg::GpuLayoutTechnique::create();
+
+        dynamic_text_layout->position = vsg::vec3(0.0, 0.0, 3.0);
+        dynamic_text_layout->horizontal = vsg::vec3(1.0, 0.0, 0.0);
+        dynamic_text_layout->vertical = vsg::vec3(0.0, 0.0, 1.0);
+        dynamic_text_layout->color = vsg::vec4(1.0, 0.9, 1.0, 1.0);
+        dynamic_text_layout->outlineWidth = 0.1;
+
+        dynamic_text->text = dynamic_text_label;
+        dynamic_text->font = font;
+        dynamic_text->font->options = options;
+        dynamic_text->layout = dynamic_text_layout;
+        dynamic_text->setup();
+        scenegraph->addChild(dynamic_text);
     }
-
-    // read texture image
-    vsg::Path textureFile("textures/lz.vsgb");
-    auto textureData = vsg::read_cast<vsg::Data>(vsg::findFile(textureFile, searchPaths));
-    if (!textureData)
-    {
-        std::cout<<"Could not read texture file : "<<textureFile<<std::endl;
-        return 1;
-    }
-
-    // set up graphics pipeline
-    vsg::DescriptorSetLayoutBindings descriptorBindings
-    {
-        {0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_FRAGMENT_BIT, nullptr} // { binding, descriptorTpe, descriptorCount, stageFlags, pImmutableSamplers}
-    };
-
-    auto descriptorSetLayout = vsg::DescriptorSetLayout::create(descriptorBindings);
-
-    vsg::PushConstantRanges pushConstantRanges
-    {
-        {VK_SHADER_STAGE_VERTEX_BIT, 0, 128} // projection view, and model matrices, actual push constant calls autoaatically provided by the VSG's DispatchTraversal
-    };
-
-    vsg::VertexInputState::Bindings vertexBindingsDescriptions
-    {
-        VkVertexInputBindingDescription{0, sizeof(vsg::vec3), VK_VERTEX_INPUT_RATE_VERTEX}, // vertex data
-        VkVertexInputBindingDescription{1, sizeof(vsg::vec3), VK_VERTEX_INPUT_RATE_VERTEX}, // colour data
-        VkVertexInputBindingDescription{2, sizeof(vsg::vec2), VK_VERTEX_INPUT_RATE_VERTEX}  // tex coord data
-    };
-
-    vsg::VertexInputState::Attributes vertexAttributeDescriptions
-    {
-        VkVertexInputAttributeDescription{0, 0, VK_FORMAT_R32G32B32_SFLOAT, 0}, // vertex data
-        VkVertexInputAttributeDescription{1, 1, VK_FORMAT_R32G32B32_SFLOAT, 0}, // colour data
-        VkVertexInputAttributeDescription{2, 2, VK_FORMAT_R32G32_SFLOAT, 0},    // tex coord data
-    };
-
-    vsg::GraphicsPipelineStates pipelineStates
-    {
-        vsg::VertexInputState::create( vertexBindingsDescriptions, vertexAttributeDescriptions ),
-        vsg::InputAssemblyState::create(),
-        vsg::RasterizationState::create(),
-        vsg::MultisampleState::create(),
-        vsg::ColorBlendState::create(),
-        vsg::DepthStencilState::create()
-    };
-
-    auto pipelineLayout = vsg::PipelineLayout::create(vsg::DescriptorSetLayouts{descriptorSetLayout}, pushConstantRanges);
-    auto graphicsPipeline = vsg::GraphicsPipeline::create(pipelineLayout, vsg::ShaderStages{vertexShader, fragmentShader}, pipelineStates);
-    auto bindGraphicsPipeline = vsg::BindGraphicsPipeline::create(graphicsPipeline);
-
-    // create texture image and associated DescriptorSets and binding
-    auto texture = vsg::DescriptorImage::create(vsg::Sampler::create(), textureData, 0, 0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
-
-    auto descriptorSet = vsg::DescriptorSet::create(descriptorSetLayout, vsg::Descriptors{texture});
-    auto bindDescriptorSet = vsg::BindDescriptorSet::create(VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 0, descriptorSet);
-
-    // create StateGroup as the root of the scene/command graph to hold the GraphicsProgram, and binding of Descriptors to decorate the whole graph
-    auto scenegraph = vsg::StateGroup::create();
-    scenegraph->add(bindGraphicsPipeline);
-    scenegraph->add(bindDescriptorSet);
-
-    // set up model transformation node
-    auto transform = vsg::MatrixTransform::create(); // VK_SHADER_STAGE_VERTEX_BIT
-
-    // add transform to root of the scene graph
-    scenegraph->addChild(transform);
-
-    // set up vertex and index arrays
-    auto vertices = vsg::vec3Array::create(
-    {
-        {-0.5f, -0.5f, 0.0f},
-        {0.5f,  -0.5f, 0.0f},
-        {0.5f , 0.5f, 0.0f},
-        {-0.5f, 0.5f, 0.0f},
-        {-0.5f, -0.5f, -0.5f},
-        {0.5f,  -0.5f, -0.5f},
-        {0.5f , 0.5f, -0.5f},
-        {-0.5f, 0.5f, -0.5f}
-    }); // VK_FORMAT_R32G32B32_SFLOAT, VK_VERTEX_INPUT_RATE_INSTANCE, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
-
-    auto colors = vsg::vec3Array::create(
-    {
-        {1.0f, 0.0f, 0.0f},
-        {0.0f, 1.0f, 0.0f},
-        {0.0f, 0.0f, 1.0f},
-        {1.0f, 1.0f, 1.0f},
-        {1.0f, 0.0f, 0.0f},
-        {0.0f, 1.0f, 0.0f},
-        {0.0f, 0.0f, 1.0f},
-        {1.0f, 1.0f, 1.0f},
-    }); // VK_FORMAT_R32G32B32_SFLOAT, VK_VERTEX_INPUT_RATE_VERTEX, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
-
-    auto texcoords = vsg::vec2Array::create(
-    {
-        {0.0f, 0.0f},
-        {1.0f, 0.0f},
-        {1.0f, 1.0f},
-        {0.0f, 1.0f},
-        {0.0f, 0.0f},
-        {1.0f, 0.0f},
-        {1.0f, 1.0f},
-        {0.0f, 1.0f}
-    }); // VK_FORMAT_R32G32_SFLOAT, VK_VERTEX_INPUT_RATE_VERTEX, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
-
-    auto indices = vsg::ushortArray::create(
-    {
-        0, 1, 2,
-        2, 3, 0,
-        4, 5, 6,
-        6, 7, 4
-    }); // VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
-
-    // setup geometry
-    auto drawCommands = vsg::Commands::create();
-    drawCommands->addChild(vsg::BindVertexBuffers::create(0, vsg::DataList{vertices, colors, texcoords}));
-    drawCommands->addChild(vsg::BindIndexBuffer::create(indices));
-    drawCommands->addChild(vsg::DrawIndexed::create(12, 1, 0, 0, 0));
-
-    // add drawCommands to transform
-    transform->addChild(drawCommands);
 
     // create the viewer and assign window(s) to it
     auto viewer = vsg::Viewer::create();
@@ -159,9 +95,17 @@ int main(int argc, char** argv)
     viewer->addWindow(window);
 
     // camera related details
-    auto viewport = vsg::ViewportState::create(0, 0, window->extent2D().width, window->extent2D().height);
-    auto perspective = vsg::Perspective::create(60.0, static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height), 0.1, 10.0);
-    auto lookAt = vsg::LookAt::create(vsg::dvec3(1.0, 1.0, 1.0), vsg::dvec3(0.0, 0.0, 0.0), vsg::dvec3(0.0, 0.0, 1.0));
+    // compute the bounds of the scene graph to help position camera
+    vsg::ComputeBounds computeBounds;
+    scenegraph->accept(computeBounds);
+    vsg::dvec3 centre = (computeBounds.bounds.min+computeBounds.bounds.max)*0.5;
+    double radius = vsg::length(computeBounds.bounds.max-computeBounds.bounds.min)*0.6;
+    double nearFarRatio = 0.001;
+
+    // set up the camera
+    auto viewport = vsg::ViewportState::create(window->extent2D());
+    auto perspective = vsg::Perspective::create(30.0, static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height), nearFarRatio*radius, radius * 1000.0);
+    auto lookAt = vsg::LookAt::create(centre+vsg::dvec3(0.0, -radius*3.5, 0.0), centre, vsg::dvec3(0.0, 0.0, 1.0));
     auto camera = vsg::Camera::create(perspective, lookAt, viewport);
 
     auto commandGraph = vsg::createCommandGraphForView(window, camera, scenegraph);
@@ -171,17 +115,25 @@ int main(int argc, char** argv)
     viewer->compile();
 
     // assign a CloseHandler to the Viewer to respond to pressing Escape or press the window close button
-    viewer->addEventHandlers({vsg::CloseHandler::create(viewer)});
+    viewer->addEventHandler(vsg::CloseHandler::create(viewer));
+
+    // assign Trackball
+    viewer->addEventHandler(vsg::Trackball::create(camera));
+
+    // assign Input handler
+    viewer->addEventHandler(InputHandler::create());
 
     // main frame loop
     while (viewer->advanceToNextFrame())
     {
+#if 0
+        // update the dynamic_text label string and position
+        dynamic_text_label->value() = vsg::make_string(viewer->getFrameStamp()->frameCount);
+        dynamic_text_layout->position.y += 0.01;
+        dynamic_text->setup();
+#endif
         // pass any events into EventHandlers assigned to the Viewer
         viewer->handleEvents();
-
-        // animate the transform
-        float time = std::chrono::duration<float, std::chrono::seconds::period>(viewer->getFrameStamp()->time - viewer->start_point()).count();
-        transform->setMatrix(vsg::rotate(time * vsg::radians(90.0f), vsg::vec3(0.0f, 0.0, 1.0f)));
 
         viewer->update();
 
