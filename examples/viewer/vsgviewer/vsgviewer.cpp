6a4d2602800fed73322a8e76bda88c1310a595fe
Robert Osfield <robert@openscenegraph.com>
2021-02-12 17:11:08 +0000

    Added try/catch block to enable better reporting of exceptions

--- examples/viewer/vsgviewer/vsgviewer.cpp
+++ examples/viewer/vsgviewer/vsgviewer.cpp
@@ -169,190 +169,203 @@ vsg::ref_ptr<vsg::Node> createTextureQuad(vsg::ref_ptr<vsg::Data> sourceData)
 
 int main(int argc, char** argv)
 {
-    // set up defaults and read command line arguments to override them
-    vsg::CommandLine arguments(&argc, argv);
-
-    // set up vsg::Options to pass in filepaths and ReaderWriter's and other IO realted options to use when reading and writing files.
-    auto options = vsg::Options::create();
-    options->paths = vsg::getEnvPaths("VSG_FILE_PATH");
-
-#ifdef USE_VSGGIS
-    options->add(vsgGIS::ReaderWriter_GDAL::create()); // add the optional ReaderWriter_GDAL fron vsgGIS to read geospatial imagery
-#endif
-
-#ifdef USE_VSGXCHANGE
-    options->add(vsgXchange::ReaderWriter_all::create()); // add the optional ReaderWriter_all fron vsgXchange to read 3d models and imagery
-#endif
-
-    arguments.read(options);
-
-    auto windowTraits = vsg::WindowTraits::create();
-    windowTraits->windowTitle = "vsgviewer";
-    windowTraits->debugLayer = arguments.read({"--debug", "-d"});
-    windowTraits->apiDumpLayer = arguments.read({"--api", "-a"});
-    if (arguments.read("--double-buffer")) windowTraits->swapchainPreferences.imageCount = 2;
-    if (arguments.read("--triple-buffer")) windowTraits->swapchainPreferences.imageCount = 3; // default
-    if (arguments.read("--IMMEDIATE")) windowTraits->swapchainPreferences.presentMode = VK_PRESENT_MODE_IMMEDIATE_KHR;
-    if (arguments.read("--FIFO")) windowTraits->swapchainPreferences.presentMode = VK_PRESENT_MODE_FIFO_KHR;
-    if (arguments.read("--FIFO_RELAXED")) windowTraits->swapchainPreferences.presentMode = VK_PRESENT_MODE_FIFO_RELAXED_KHR;
-    if (arguments.read("--MAILBOX")) windowTraits->swapchainPreferences.presentMode = VK_PRESENT_MODE_MAILBOX_KHR;
-    if (arguments.read({"-t", "--test"}))
+    try
     {
-        windowTraits->swapchainPreferences.presentMode = VK_PRESENT_MODE_IMMEDIATE_KHR;
-        windowTraits->fullscreen = true;
-    }
-    if (arguments.read({"--st", "--small-test"}))
-    {
-        windowTraits->swapchainPreferences.presentMode = VK_PRESENT_MODE_IMMEDIATE_KHR;
-        windowTraits->width = 192, windowTraits->height = 108;
-        windowTraits->decoration = false;
-    }
-    if (arguments.read({"--fullscreen", "--fs"})) windowTraits->fullscreen = true;
-    if (arguments.read({"--window", "-w"}, windowTraits->width, windowTraits->height)) { windowTraits->fullscreen = false; }
-    if (arguments.read({"--no-frame", "--nf"})) windowTraits->decoration = false;
-    if (arguments.read("--or")) windowTraits->overrideRedirect = true;
-    if (arguments.read("--d32")) windowTraits->depthFormat = VK_FORMAT_D32_SFLOAT;
-    arguments.read("--screen", windowTraits->screenNum);
-    arguments.read("--display", windowTraits->display);
-    arguments.read("--samples", windowTraits->samples);
-    auto numFrames = arguments.value(-1, "-f");
-    auto pathFilename = arguments.value(std::string(), "-p");
-    auto loadLevels = arguments.value(0, "--load-levels");
-    auto horizonMountainHeight = arguments.value(0.0, "--hmh");
+        // set up defaults and read command line arguments to override them
+        vsg::CommandLine arguments(&argc, argv);
 
-    if (arguments.errors()) return arguments.writeErrorMessages(std::cerr);
+        // set up vsg::Options to pass in filepaths and ReaderWriter's and other IO realted options to use when reading and writing files.
+        auto options = vsg::Options::create();
+        options->paths = vsg::getEnvPaths("VSG_FILE_PATH");
 
+    #ifdef USE_VSGGIS
+        options->add(vsgGIS::ReaderWriter_GDAL::create()); // add the optional ReaderWriter_GDAL fron vsgGIS to read geospatial imagery
+    #endif
 
-    auto group = vsg::Group::create();
+    #ifdef USE_VSGXCHANGE
+        options->add(vsgXchange::ReaderWriter_all::create()); // add the optional ReaderWriter_all fron vsgXchange to read 3d models and imagery
+    #endif
 
-    vsg::Path path;
+        arguments.read(options);
 
-    // read any vsg files
-    for (int i = 1; i < argc; ++i)
-    {
-        vsg::Path filename = arguments[i];
-        path = vsg::filePath(filename);
-
-        auto object = vsg::read(filename, options);
-        if (auto node = object.cast<vsg::Node>(); node)
+        auto windowTraits = vsg::WindowTraits::create();
+        windowTraits->windowTitle = "vsgviewer";
+        windowTraits->debugLayer = arguments.read({"--debug", "-d"});
+        windowTraits->apiDumpLayer = arguments.read({"--api", "-a"});
+        if (arguments.read("--double-buffer")) windowTraits->swapchainPreferences.imageCount = 2;
+        if (arguments.read("--triple-buffer")) windowTraits->swapchainPreferences.imageCount = 3; // default
+        if (arguments.read("--IMMEDIATE")) windowTraits->swapchainPreferences.presentMode = VK_PRESENT_MODE_IMMEDIATE_KHR;
+        if (arguments.read("--FIFO")) windowTraits->swapchainPreferences.presentMode = VK_PRESENT_MODE_FIFO_KHR;
+        if (arguments.read("--FIFO_RELAXED")) windowTraits->swapchainPreferences.presentMode = VK_PRESENT_MODE_FIFO_RELAXED_KHR;
+        if (arguments.read("--MAILBOX")) windowTraits->swapchainPreferences.presentMode = VK_PRESENT_MODE_MAILBOX_KHR;
+        if (arguments.read({"-t", "--test"}))
         {
-            group->addChild(node);
+            windowTraits->swapchainPreferences.presentMode = VK_PRESENT_MODE_IMMEDIATE_KHR;
+            windowTraits->fullscreen = true;
         }
-        else if (auto data = object.cast<vsg::Data>(); data)
+        if (arguments.read({"--st", "--small-test"}))
         {
-            if (auto node = createTextureQuad(data); node)
-            {
-                group->addChild(node);
-            }
+            windowTraits->swapchainPreferences.presentMode = VK_PRESENT_MODE_IMMEDIATE_KHR;
+            windowTraits->width = 192, windowTraits->height = 108;
+            windowTraits->decoration = false;
         }
-        else if (object)
+        if (arguments.read({"--fullscreen", "--fs"})) windowTraits->fullscreen = true;
+        if (arguments.read({"--window", "-w"}, windowTraits->width, windowTraits->height)) { windowTraits->fullscreen = false; }
+        if (arguments.read({"--no-frame", "--nf"})) windowTraits->decoration = false;
+        if (arguments.read("--or")) windowTraits->overrideRedirect = true;
+        if (arguments.read("--d32")) windowTraits->depthFormat = VK_FORMAT_D32_SFLOAT;
+        arguments.read("--screen", windowTraits->screenNum);
+        arguments.read("--display", windowTraits->display);
+        arguments.read("--samples", windowTraits->samples);
+        auto numFrames = arguments.value(-1, "-f");
+        auto pathFilename = arguments.value(std::string(), "-p");
+        auto loadLevels = arguments.value(0, "--load-levels");
+        auto horizonMountainHeight = arguments.value(0.0, "--hmh");
+
+        if (arguments.errors()) return arguments.writeErrorMessages(std::cerr);
+
+        if (argc<=1)
         {
-            std::cout << "Unable to view object of type " << object->className() << std::endl;
-        }
-        else
-        {
-            std::cout << "Unable to load model from file " << filename << std::endl;
-        }
-    }
-
-    if (group->getNumChildren() == 0)
-    {
-        std::cout << "Please specify a 3d model file on the command line." << std::endl;
-        return 1;
-    }
-
-    vsg::ref_ptr<vsg::Node> vsg_scene;
-    if (group->getChildren().size() == 1)
-        vsg_scene = group->getChild(0);
-    else
-        vsg_scene = group;
-
-    // create the viewer and assign window(s) to it
-    auto viewer = vsg::Viewer::create();
-
-    vsg::ref_ptr<vsg::Window> window(vsg::Window::create(windowTraits));
-    if (!window)
-    {
-        std::cout << "Could not create windows." << std::endl;
-        return 1;
-    }
-
-    viewer->addWindow(window);
-
-    // compute the bounds of the scene graph to help position camera
-    vsg::ComputeBounds computeBounds;
-    vsg_scene->accept(computeBounds);
-    vsg::dvec3 centre = (computeBounds.bounds.min + computeBounds.bounds.max) * 0.5;
-    double radius = vsg::length(computeBounds.bounds.max - computeBounds.bounds.min) * 0.6;
-    double nearFarRatio = 0.001;
-
-    // set up the camera
-    auto lookAt = vsg::LookAt::create(centre + vsg::dvec3(0.0, -radius * 3.5, 0.0), centre, vsg::dvec3(0.0, 0.0, 1.0));
-
-    vsg::ref_ptr<vsg::ProjectionMatrix> perspective;
-    if (vsg::ref_ptr<vsg::EllipsoidModel> ellipsoidModel(vsg_scene->getObject<vsg::EllipsoidModel>("EllipsoidModel")); ellipsoidModel)
-    {
-        perspective = vsg::EllipsoidPerspective::create(lookAt, ellipsoidModel, 30.0, static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height), nearFarRatio, horizonMountainHeight);
-    }
-    else
-    {
-        perspective = vsg::Perspective::create(30.0, static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height), nearFarRatio * radius, radius * 4.5);
-    }
-
-    auto camera = vsg::Camera::create(perspective, lookAt, vsg::ViewportState::create(window->extent2D()));
-
-    // add close handler to respond the close window button and pressing escape
-    viewer->addEventHandler(vsg::CloseHandler::create(viewer));
-
-    if (pathFilename.empty())
-    {
-        viewer->addEventHandler(vsg::Trackball::create(camera));
-    }
-    else
-    {
-        std::ifstream in(pathFilename);
-        if (!in)
-        {
-            std::cout << "AnimationPat: Could not open animation path file \"" << pathFilename << "\".\n";
+            std::cout << "Please specify a 3d model or image file on the command line." << std::endl;
             return 1;
         }
 
-        vsg::ref_ptr<vsg::AnimationPath> animationPath(new vsg::AnimationPath);
-        animationPath->read(in);
+        auto group = vsg::Group::create();
 
-        viewer->addEventHandler(vsg::AnimationPathHandler::create(camera, animationPath, viewer->start_point()));
+        vsg::Path path;
+
+        // read any vsg files
+        for (int i = 1; i < argc; ++i)
+        {
+            vsg::Path filename = arguments[i];
+            path = vsg::filePath(filename);
+
+            auto object = vsg::read(filename, options);
+            if (auto node = object.cast<vsg::Node>(); node)
+            {
+                group->addChild(node);
+            }
+            else if (auto data = object.cast<vsg::Data>(); data)
+            {
+                if (auto node = createTextureQuad(data); node)
+                {
+                    group->addChild(node);
+                }
+            }
+            else if (object)
+            {
+                std::cout << "Unable to view object of type " << object->className() << std::endl;
+            }
+            else
+            {
+                std::cout << "Unable to load model from file " << filename << std::endl;
+            }
+        }
+
+        if (group->getNumChildren() == 0)
+        {
+            return 1;
+        }
+
+        vsg::ref_ptr<vsg::Node> vsg_scene;
+        if (group->getChildren().size() == 1)
+            vsg_scene = group->getChild(0);
+        else
+            vsg_scene = group;
+
+        // create the viewer and assign window(s) to it
+        auto viewer = vsg::Viewer::create();
+
+        vsg::ref_ptr<vsg::Window> window(vsg::Window::create(windowTraits));
+        if (!window)
+        {
+            std::cout << "Could not create windows." << std::endl;
+            return 1;
+        }
+
+        viewer->addWindow(window);
+
+        // compute the bounds of the scene graph to help position camera
+        vsg::ComputeBounds computeBounds;
+        vsg_scene->accept(computeBounds);
+        vsg::dvec3 centre = (computeBounds.bounds.min + computeBounds.bounds.max) * 0.5;
+        double radius = vsg::length(computeBounds.bounds.max - computeBounds.bounds.min) * 0.6;
+        double nearFarRatio = 0.001;
+
+        // set up the camera
+        auto lookAt = vsg::LookAt::create(centre + vsg::dvec3(0.0, -radius * 3.5, 0.0), centre, vsg::dvec3(0.0, 0.0, 1.0));
+
+        vsg::ref_ptr<vsg::ProjectionMatrix> perspective;
+        if (vsg::ref_ptr<vsg::EllipsoidModel> ellipsoidModel(vsg_scene->getObject<vsg::EllipsoidModel>("EllipsoidModel")); ellipsoidModel)
+        {
+            perspective = vsg::EllipsoidPerspective::create(lookAt, ellipsoidModel, 30.0, static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height), nearFarRatio, horizonMountainHeight);
+        }
+        else
+        {
+            perspective = vsg::Perspective::create(30.0, static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height), nearFarRatio * radius, radius * 4.5);
+        }
+
+        auto camera = vsg::Camera::create(perspective, lookAt, vsg::ViewportState::create(window->extent2D()));
+
+        // add close handler to respond the close window button and pressing escape
+        viewer->addEventHandler(vsg::CloseHandler::create(viewer));
+
+        if (pathFilename.empty())
+        {
+            viewer->addEventHandler(vsg::Trackball::create(camera));
+        }
+        else
+        {
+            std::ifstream in(pathFilename);
+            if (!in)
+            {
+                std::cout << "AnimationPat: Could not open animation path file \"" << pathFilename << "\".\n";
+                return 1;
+            }
+
+            vsg::ref_ptr<vsg::AnimationPath> animationPath(new vsg::AnimationPath);
+            animationPath->read(in);
+
+            viewer->addEventHandler(vsg::AnimationPathHandler::create(camera, animationPath, viewer->start_point()));
+        }
+
+        // if required pre load specific number of PagedLOD levels.
+        if (loadLevels > 0)
+        {
+            vsg::LoadPagedLOD loadPagedLOD(camera, loadLevels);
+
+            auto startTime = std::chrono::steady_clock::now();
+
+            vsg_scene->accept(loadPagedLOD);
+
+            auto time = std::chrono::duration<float, std::chrono::milliseconds::period>(std::chrono::steady_clock::now() - startTime).count();
+            std::cout << "No. of tiles loaed " << loadPagedLOD.numTiles << " in " << time << "ms." << std::endl;
+        }
+
+        auto commandGraph = vsg::createCommandGraphForView(window, camera, vsg_scene);
+        viewer->assignRecordAndSubmitTaskAndPresentation({commandGraph});
+
+        viewer->compile();
+
+        // rendering main loop
+        while (viewer->advanceToNextFrame() && (numFrames < 0 || (numFrames--) > 0))
+        {
+            // pass any events into EventHandlers assigned to the Viewer
+            viewer->handleEvents();
+
+            viewer->update();
+
+            viewer->recordAndSubmit();
+
+            viewer->present();
+        }
     }
-
-    // if required pre load specific number of PagedLOD levels.
-    if (loadLevels > 0)
+    catch (const vsg::Exception& ve)
     {
-        vsg::LoadPagedLOD loadPagedLOD(camera, loadLevels);
-
-        auto startTime = std::chrono::steady_clock::now();
-
-        vsg_scene->accept(loadPagedLOD);
-
-        auto time = std::chrono::duration<float, std::chrono::milliseconds::period>(std::chrono::steady_clock::now() - startTime).count();
-        std::cout << "No. of tiles loaed " << loadPagedLOD.numTiles << " in " << time << "ms." << std::endl;
+        std::cerr << "[Exception] - " << ve.message << " result = "<<ve.result<<std::endl;
+        return 1;
     }
 
-    auto commandGraph = vsg::createCommandGraphForView(window, camera, vsg_scene);
-    viewer->assignRecordAndSubmitTaskAndPresentation({commandGraph});
-
-    viewer->compile();
-
-    // rendering main loop
-    while (viewer->advanceToNextFrame() && (numFrames < 0 || (numFrames--) > 0))
-    {
-        // pass any events into EventHandlers assigned to the Viewer
-        viewer->handleEvents();
-
-        viewer->update();
-
-        viewer->recordAndSubmit();
-
-        viewer->present();
-    }
 
     // clean up done automatically thanks to ref_ptr<>
     return 0;
