ec6b80c1d258a2b2e0204f752866c890548803ec
Robert Osfield <robert@openscenegraph.com>
2021-03-11 15:07:23 +0000

    Added VSG_FILE_CACHE setting to vsg::Options setup

--- examples/viewer/vsgviewer/vsgviewer.cpp
+++ examples/viewer/vsgviewer/vsgviewer.cpp
@@ -174,6 +174,7 @@ int main(int argc, char** argv)
 
         // set up vsg::Options to pass in filepaths and ReaderWriter's and other IO realted options to use when reading and writing files.
         auto options = vsg::Options::create();
+        options->fileCache = vsg::getEnv("VSG_FILE_CACHE");
         options->paths = vsg::getEnvPaths("VSG_FILE_PATH");
 
 #ifdef vsgXchange_all
