ff3e157e98df99231acbd535aaaaeea913e9e06d
Karol Nowak <knw@spyro-soft.com>
2021-04-27 20:20:50 +0200

    vsgmultiviews + imgui for tests purposes

--- examples/viewer/vsgmultiviews/vsgmultiviews.cpp
+++ examples/viewer/vsgmultiviews/vsgmultiviews.cpp
@@ -1,11 +1,87 @@
 #include <vsg/all.h>
 
+#include <vsgImGui/RenderImGui.h>
+#include <vsgImGui/SendEventsToImGui.h>
+#include <vsgImGui/imgui.h>
+
 #include <chrono>
 #include <iostream>
 
 #ifdef vsgXchange_FOUND
 #    include <vsgXchange/all.h>
 #endif
+
+struct Params : public vsg::Inherit<vsg::Object, Params>
+{
+    bool showGui = true; // you can toggle this with your own EventHandler and key
+    bool showDemoWindow = false;
+    bool showSecondWindow = false;
+    float clearColor[3]{0.2f, 0.2f, 0.4f}; // Unfortunately, this doesn't change dynamically in vsg
+    uint32_t counter = 0;
+    float dist = 0.f;
+};
+
+class MyGuiComponent
+{
+public:
+    MyGuiComponent(vsg::ref_ptr<Params> params) :
+        _params(params)
+    {
+    }
+
+    // Example here taken from the Dear imgui comments (mostly)
+    bool operator()()
+    {
+        bool visibleComponents = false;
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        if (_params->showGui)
+        {
+            ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("Some useful message here.");                 // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &_params->showDemoWindow); // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &_params->showSecondWindow);
+            ImGui::SliderFloat("float", &_params->dist, 0.0f, 1.0f);        // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&_params->clearColor); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
+                _params->counter++;
+
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", _params->counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+
+            visibleComponents = true;
+        }
+
+        // 3. Show another simple window.
+        if (_params->showSecondWindow)
+        {
+            ImGui::Begin("Another Window", &_params->showSecondWindow); // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                _params->showSecondWindow = false;
+            ImGui::End();
+
+            visibleComponents = true;
+        }
+
+        if (_params->showDemoWindow)
+        {
+            ImGui::ShowDemoWindow(&_params->showDemoWindow);
+
+            visibleComponents = true;
+        }
+
+        return visibleComponents;
+    }
+
+private:
+    vsg::ref_ptr<Params> _params;
+};
 
 vsg::ref_ptr<vsg::Camera> createCameraForScene(vsg::Node* scenegraph, int32_t x, int32_t y, uint32_t width, uint32_t height)
 {
@@ -37,6 +113,8 @@ int main(int argc, char** argv)
     windowTraits->windowTitle = "Multiple Views";
     windowTraits->debugLayer = arguments.read({"--debug", "-d"});
     windowTraits->apiDumpLayer = arguments.read({"--api", "-a"});
+    windowTraits->height = 600;
+    windowTraits->width = 800;
     if (arguments.read({"--window", "-w"}, windowTraits->width, windowTraits->height)) { windowTraits->fullscreen = false; }
 
     bool separateRenderGraph = arguments.read("-s");
@@ -92,23 +170,29 @@ int main(int argc, char** argv)
     auto secondary_camera = createCameraForScene(scenegraph2, (width * 3) / 4, 0, width / 4, height / 4);
     auto secondary_view = vsg::View::create(secondary_camera, scenegraph2);
 
-    // add close handler to respond the close window button and pressing escape
-    viewer->addEventHandler(vsg::CloseHandler::create(viewer));
-
-    // add event handlers, in the order we wish event to be handled.
-    viewer->addEventHandler(vsg::Trackball::create(secondary_camera));
-    viewer->addEventHandler(vsg::Trackball::create(main_camera));
+    auto sw = vsg::Switch::create();
+    auto swImGUI = vsg::Switch::create();
 
     if (separateRenderGraph)
     {
         std::cout << "Using a RenderGraph per View" << std::endl;
         auto main_RenderGraph = vsg::RenderGraph::create(window, main_view);
         auto secondary_RenderGraph = vsg::RenderGraph::create(window, secondary_view);
+        auto imgui_RenderGraph = vsg::RenderGraph::create(window);
         secondary_RenderGraph->clearValues[0].color = {0.2f, 0.2f, 0.2f, 1.0f};
+        imgui_RenderGraph->clearValues[0].color = {0.3f, 0.3f, 0.3f, 1.0f};
 
         auto commandGraph = vsg::CommandGraph::create(window);
         commandGraph->addChild(main_RenderGraph);
-        commandGraph->addChild(secondary_RenderGraph);
+        commandGraph->addChild(sw);
+        commandGraph->addChild(imgui_RenderGraph);
+
+        sw->addChild(true, secondary_RenderGraph);
+
+        auto params = Params::create();
+        swImGUI->addChild(true, vsgImGui::RenderImGui::create(window, MyGuiComponent(params)));
+
+        imgui_RenderGraph->addChild(swImGUI);
 
         viewer->assignRecordAndSubmitTaskAndPresentation({commandGraph});
     }
@@ -117,6 +201,9 @@ int main(int argc, char** argv)
         std::cout << "Using a single RenderGraph, with both Views separated by a ClearAttachemnts" << std::endl;
         auto renderGraph = vsg::RenderGraph::create(window);
 
+        auto commandGraph = vsg::CommandGraph::create(window);
+        commandGraph->addChild(renderGraph);
+
         renderGraph->addChild(main_view);
 
         // clear the depth buffer before view2 gets rendered
@@ -124,22 +211,49 @@ int main(int argc, char** argv)
         VkClearAttachment depth_attachment{VK_IMAGE_ASPECT_DEPTH_BIT, 1, VkClearValue{1.0f, 0.0f}};
         VkClearRect rect{secondary_camera->getRenderArea(), 0, 1};
         auto clearAttachments = vsg::ClearAttachments::create(vsg::ClearAttachments::Attachments{color_attachment, depth_attachment}, vsg::ClearAttachments::Rects{rect, rect});
-        renderGraph->addChild(clearAttachments);
 
-        renderGraph->addChild(secondary_view);
+        sw->addChild(true, clearAttachments);
+        sw->addChild(true, secondary_view);
 
-        auto commandGraph = vsg::CommandGraph::create(window);
-        commandGraph->addChild(renderGraph);
+        auto params = Params::create();
+        swImGUI->addChild(true, vsgImGui::RenderImGui::create(window, MyGuiComponent(params)));
+
+        renderGraph->addChild(sw);
+        renderGraph->addChild(swImGUI);
+
         viewer->assignRecordAndSubmitTaskAndPresentation({commandGraph});
     }
 
+    // add close handler to respond the close window button and pressing escape
+    viewer->addEventHandler(vsg::CloseHandler::create(viewer));
+
+    // Add the ImGui event handler first to handle events early
+    viewer->addEventHandler(vsgImGui::SendEventsToImGui::create());
+
+    // add event handlers, in the order we wish event to be handled.
+    viewer->addEventHandler(vsg::Trackball::create(secondary_camera));
+    viewer->addEventHandler(vsg::Trackball::create(main_camera));
+
     viewer->compile();
+
+    int count = 0;
+    int countIMGUI = 0;
 
     // rendering main loop
     while (viewer->advanceToNextFrame())
     {
         // pass any events into EventHandlers assigned to the Viewer
         viewer->handleEvents();
+
+        // toggle the Switch decorating the secondary view
+        sw->setAllChildren(((count / 60) % 2) == 0);
+
+        // toggle the Switch decorating the secondary view
+        swImGUI->setAllChildren(((countIMGUI / 120) % 2) == 0);
+
+        //++countIMGUI;
+
+        ++count;
 
         viewer->update();
 
