a64932286d3d5561eff08616d6cc73d897a815a4
Robert Osfield <robert@openscenegraph.com>
2021-07-23 18:25:59 +0100

    Implemented capsule geometry

--- examples/traversals/vsgintersection/Builder.cpp
+++ examples/traversals/vsgintersection/Builder.cpp
@@ -235,8 +235,224 @@ vsg::ref_ptr<vsg::Node> Builder::createBox(const GeometryInfo& info)
 
 vsg::ref_ptr<vsg::Node> Builder::createCapsule(const GeometryInfo& info)
 {
-    std::cout<<"createCapsule()"<<std::endl;
-    return createBox(info);
+    auto& subgraph = _capsules[info];
+    if (subgraph)
+    {
+        std::cout<<"reused createCapsule()"<<std::endl;
+        return subgraph;
+    }
+
+    std::cout<<"new createCapsule()"<<std::endl;
+
+    auto [t_origin, t_scale, t_top] = y_texcoord(info).value;
+
+    // create StateGroup as the root of the scene/command graph to hold the GraphicsProgram, and binding of Descriptors to decorate the whole graph
+    auto scenegraph = vsg::StateGroup::create();
+    scenegraph->add(_createGraphicsPipeline());
+    scenegraph->add(_createTexture(info));
+
+    auto dx = info.dx * 0.5f;
+    auto dy = info.dy * 0.5f;
+    auto dz = info.dz * 0.5f;
+
+    auto bottom = info.position - dz;
+    auto top = info.position + dz;
+
+    bool withEnds = true;
+
+    unsigned int num_columns = 20;
+    unsigned int num_rows = 6;
+
+    unsigned int num_vertices = num_columns * 2;
+    unsigned int num_indices = (num_columns-1) * 6;
+
+    if (withEnds)
+    {
+        num_vertices += num_columns * num_rows * 2;
+        num_indices += (num_columns-1) * (num_rows-1) * 6 * 2;
+    }
+
+    auto vertices = vsg::vec3Array::create(num_vertices);
+    auto normals = vsg::vec3Array::create(num_vertices);
+    auto texcoords = vsg::vec2Array::create(num_vertices);
+    auto colors = vsg::vec3Array::create(vertices->size(), vsg::vec3(1.0f, 1.0f, 1.0f));
+    auto indices = vsg::ushortArray::create(num_indices);
+
+
+    vsg::vec3 v = dy;
+    vsg::vec3 n = normalize(dy);
+    vertices->set(0, bottom + v);
+    normals->set(0, n);
+    texcoords->set(0, vsg::vec2(0.0, t_origin));
+    vertices->set(num_columns*2-2, bottom+v);
+    normals->set(num_columns*2-2, n);
+    texcoords->set(num_columns*2-2, vsg::vec2(1.0, t_origin));
+
+    vertices->set(1, top + v);
+    normals->set(1, n);
+    texcoords->set(1, vsg::vec2(0.0, t_top));
+    vertices->set(num_columns*2-1, top+v);
+    normals->set(num_columns*2-1, n);
+    texcoords->set(num_columns*2-1, vsg::vec2(1.0, t_top));
+
+    for(unsigned int c = 1; c < num_columns-1; ++c)
+    {
+        unsigned int vi = c*2;
+        float r = float(c)/float(num_columns-1);
+        float alpha = (r) * 2.0 * vsg::PI;
+        v = dx * (-sinf(alpha)) + dy * (cosf(alpha));
+        n = normalize(v);
+
+        vertices->set(vi, bottom + v);
+        normals->set(vi, n);
+        texcoords->set(vi, vsg::vec2(r, t_origin));
+
+        vertices->set(vi+1, top + v);
+        normals->set(vi+1, n);
+        texcoords->set(vi+1, vsg::vec2(r, t_top));
+    }
+
+    unsigned int i = 0;
+    for(unsigned int c = 0; c < num_columns-1; ++c)
+    {
+        unsigned lower = c*2;
+        unsigned upper = lower + 1;
+
+        indices->set(i++, lower);
+        indices->set(i++, lower + 2);
+        indices->set(i++, upper);
+
+        indices->set(i++, upper);
+        indices->set(i++, lower + 2);
+        indices->set(i++, upper + 2);
+    }
+
+    if (withEnds)
+    {
+        unsigned int base_vi = num_columns * 2;
+
+        // bottom
+        {
+            for(unsigned int r = 0; r < num_rows; ++r)
+            {
+                float beta = ((float(r)/float(num_rows-1))-1.0f) * vsg::PI * 0.5f;
+                float ty = t_origin + t_scale * float(r)/float(num_rows-1);
+                float cos_beta = cosf(beta);
+                vsg::vec3 dz_sin_beta = dz * sinf(beta);
+
+                vsg::vec3 v = dy * cos_beta + dz_sin_beta;
+                vsg::vec3 n = normalize(v);
+
+                unsigned int left_i = base_vi + r * num_columns;
+                vertices->set(left_i, bottom + v);
+                normals->set(left_i, n);
+                texcoords->set(left_i, vsg::vec2(0.0f, ty));
+
+                unsigned int right_i = left_i + num_columns-1;
+                vertices->set(right_i, bottom + v);
+                normals->set(right_i, n);
+                texcoords->set(right_i, vsg::vec2(1.0f, ty));
+
+                for(unsigned int c = 1; c < num_columns-1; ++c)
+                {
+                    unsigned int vi = left_i + c;
+                    float alpha = (float(c)/float(num_columns-1)) * 2.0 * vsg::PI;
+                    v = dx * (-sinf(alpha) * cos_beta) + dy * (cosf(alpha) * cos_beta) + dz_sin_beta;
+                    n = normalize(v);
+                    vertices->set(vi, bottom + v);
+                    normals->set(vi, n);
+                    texcoords->set(vi, vsg::vec2(float(c)/float(num_columns-1), ty));
+                }
+            }
+
+            for(unsigned int r = 0; r < num_rows-1; ++r)
+            {
+                for(unsigned int c = 0; c < num_columns-1; ++c)
+                {
+                    unsigned lower = base_vi + num_columns * r + c;
+                    unsigned upper = lower + num_columns;
+
+                    indices->set(i++, lower);
+                    indices->set(i++, lower + 1);
+                    indices->set(i++, upper);
+
+                    indices->set(i++, upper);
+                    indices->set(i++, lower + 1);
+                    indices->set(i++, upper + 1);
+                }
+            }
+
+            base_vi += num_columns * num_rows;
+        }
+
+        // top
+        {
+            for(unsigned int r = 0; r < num_rows; ++r)
+            {
+                float beta = ((float(r)/float(num_rows-1))) * vsg::PI * 0.5f;
+                float ty = t_origin + t_scale * float(r)/float(num_rows-1);
+                float cos_beta = cosf(beta);
+                vsg::vec3 dz_sin_beta = dz * sinf(beta);
+
+                vsg::vec3 v = dy * cos_beta + dz_sin_beta;
+                vsg::vec3 n = normalize(v);
+
+                unsigned int left_i = base_vi + r * num_columns;
+                vertices->set(left_i, top + v);
+                normals->set(left_i, n);
+                texcoords->set(left_i, vsg::vec2(0.0f, ty));
+
+                unsigned int right_i = left_i + num_columns-1;
+                vertices->set(right_i, top + v);
+                normals->set(right_i, n);
+                texcoords->set(right_i, vsg::vec2(1.0f, ty));
+
+                for(unsigned int c = 1; c < num_columns-1; ++c)
+                {
+                    unsigned int vi = left_i + c;
+                    float alpha = (float(c)/float(num_columns-1)) * 2.0 * vsg::PI;
+                    v = dx * (-sinf(alpha) * cos_beta) + dy * (cosf(alpha) * cos_beta) + dz_sin_beta;
+                    n = normalize(v);
+                    vertices->set(vi, top + v);
+                    normals->set(vi, n);
+                    texcoords->set(vi, vsg::vec2(float(c)/float(num_columns-1), ty));
+                }
+            }
+
+            for(unsigned int r = 0; r < num_rows-1; ++r)
+            {
+                for(unsigned int c = 0; c < num_columns-1; ++c)
+                {
+                    unsigned lower = base_vi + num_columns * r + c;
+                    unsigned upper = lower + num_columns;
+
+                    indices->set(i++, lower);
+                    indices->set(i++, lower + 1);
+                    indices->set(i++, upper);
+
+                    indices->set(i++, upper);
+                    indices->set(i++, lower + 1);
+                    indices->set(i++, upper + 1);
+                }
+            }
+
+            base_vi += num_columns * num_rows;
+        }
+    }
+
+    // setup geometry
+    auto vid = vsg::VertexIndexDraw::create();
+    vid->arrays = vsg::DataList{vertices, colors, texcoords};
+    vid->indices = indices;
+    vid->indexCount = indices->size();
+    vid->instanceCount = 1;
+
+    scenegraph->addChild(vid);
+
+    compile(scenegraph);
+
+    subgraph = scenegraph;
+    return subgraph;
 }
 
 vsg::ref_ptr<vsg::Node> Builder::createCone(const GeometryInfo& info)
