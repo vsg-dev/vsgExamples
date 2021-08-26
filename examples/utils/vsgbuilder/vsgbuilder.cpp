2925337b58b779d8cd91f123f010f48285cef586
Robert Osfield <robert@openscenegraph.com>
2021-08-26 14:27:05 +0100

    Added --two-sided command line option.

--- examples/utils/vsgbuilder/vsgbuilder.cpp
+++ examples/utils/vsgbuilder/vsgbuilder.cpp
@@ -51,6 +51,7 @@ int main(int argc, char** argv)
     bool floatColors = !arguments.read("--ubvec4-colors");
     stateInfo.wireframe = arguments.read("--wireframe");
     stateInfo.lighting = !arguments.read("--flat");
+    stateInfo.doubleSided = arguments.read("--two-sided");
 
     arguments.read("--dx", geomInfo.dx);
     arguments.read("--dy", geomInfo.dy);
