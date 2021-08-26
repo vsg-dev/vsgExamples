2c4fb9ed939ec494d0cb7f6ff87e27bb82aa6432
Robert Osfield <robert@openscenegraph.com>
2021-08-26 15:36:35 +0100

    Refactored vsgdynamictexture example to use the vsg::Builder with an opton to use heightfield with a dynamic displacment map or a textured box with dynamic texture

--- examples/state/vsgdynamictexture/vsgdynamictexture.cpp
+++ examples/state/vsgdynamictexture/vsgdynamictexture.cpp
@@ -70,6 +70,8 @@ int main(int argc, char** argv)
     auto numFrames = arguments.value(-1, "-f");
     auto workgroupSize = arguments.value(32, "-w");
     bool useRGBA = arguments.read("--rgba");
+    bool heightfield = arguments.read("--hf");
+    auto image_size = arguments.value<uint32_t>(256, "-s");
 
     if (arguments.errors()) return arguments.writeErrorMessages(std::cerr);
 
@@ -112,7 +114,7 @@ int main(int argc, char** argv)
     if (useRGBA)
     {
         // R, RG and RGBA data can be copied to vkImage without any conversion so is efficient, while RGB requires conversion, see below explanation
-        textureData = vsg::vec4Array2D::create(256, 256);
+        textureData = vsg::vec4Array2D::create(image_size, image_size);
         textureData->getLayout().format = VK_FORMAT_R32G32B32A32_SFLOAT;
     }
     else
@@ -121,7 +123,7 @@ int main(int argc, char** argv)
         // the VSG will do this automatically do the RGB to RGBA conversion for you each time the data is copied
         // this makes RGB substantially slower than using RGBA data.
         // one approach, illustrated in the vsgdynamictexture_cs example, for avoiding this conversion overhead is to use a compute shader to map the RGB data to RGBA.
-        textureData = vsg::vec3Array2D::create(256, 256);
+        textureData = vsg::vec3Array2D::create(image_size, image_size);
         textureData->getLayout().format = VK_FORMAT_R32G32B32_SFLOAT;
     }
 
@@ -129,105 +131,25 @@ int main(int argc, char** argv)
     UpdateImage updateImage;
     updateImage(textureData, 0.0);
 
-    vsg::ref_ptr<vsg::Image> image = vsg::Image::create(textureData);
-    image->usage |= (VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT);
-
-    auto imageView = vsg::ImageView::create(image, VK_IMAGE_ASPECT_COLOR_BIT);
-    auto sampler = vsg::Sampler::create();
-
-    vsg::ImageInfo imageInfo(sampler, imageView, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
-
-    // create texture image and associated DescriptorSets and binding
-    auto texture = vsg::DescriptorImage::create(imageInfo, 0, 0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
-
     // setup graphics rendering subgraph
     auto scenegraph = vsg::StateGroup::create();
+
+    vsg::Builder builder;
+    vsg::GeometryInfo geomInfo;
+    vsg::StateInfo stateInfo;
+    stateInfo.doubleSided = true;
+
+    if (heightfield)
     {
-        // set up graphics pipeline
-        vsg::DescriptorSetLayoutBindings descriptorBindings{
-            {0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_FRAGMENT_BIT, nullptr} // { binding, descriptorTpe, descriptorCount, stageFlags, pImmutableSamplers}
-        };
-
-        auto descriptorSetLayout = vsg::DescriptorSetLayout::create(descriptorBindings);
-
-        vsg::PushConstantRanges pushConstantRanges{
-            {VK_SHADER_STAGE_VERTEX_BIT, 0, 128} // projection view, and model matrices, actual push constant calls autoaatically provided by the VSG's DispatchTraversal
-        };
-
-        vsg::VertexInputState::Bindings vertexBindingsDescriptions{
-            VkVertexInputBindingDescription{0, sizeof(vsg::vec3), VK_VERTEX_INPUT_RATE_VERTEX}, // vertex data
-            VkVertexInputBindingDescription{1, sizeof(vsg::vec3), VK_VERTEX_INPUT_RATE_VERTEX}, // colour data
-            VkVertexInputBindingDescription{2, sizeof(vsg::vec2), VK_VERTEX_INPUT_RATE_VERTEX}  // tex coord data
-        };
-
-        vsg::VertexInputState::Attributes vertexAttributeDescriptions{
-            VkVertexInputAttributeDescription{0, 0, VK_FORMAT_R32G32B32_SFLOAT, 0}, // vertex data
-            VkVertexInputAttributeDescription{1, 1, VK_FORMAT_R32G32B32_SFLOAT, 0}, // colour data
-            VkVertexInputAttributeDescription{2, 2, VK_FORMAT_R32G32_SFLOAT, 0},    // tex coord data
-        };
-
-        vsg::GraphicsPipelineStates pipelineStates{
-            vsg::VertexInputState::create(vertexBindingsDescriptions, vertexAttributeDescriptions),
-            vsg::InputAssemblyState::create(),
-            vsg::RasterizationState::create(),
-            vsg::MultisampleState::create(),
-            vsg::ColorBlendState::create(),
-            vsg::DepthStencilState::create()};
-
-        auto pipelineLayout = vsg::PipelineLayout::create(vsg::DescriptorSetLayouts{descriptorSetLayout}, pushConstantRanges);
-        auto graphicsPipeline = vsg::GraphicsPipeline::create(pipelineLayout, vsg::ShaderStages{vertexShader, fragmentShader}, pipelineStates);
-        auto bindGraphicsPipeline = vsg::BindGraphicsPipeline::create(graphicsPipeline);
-
-        auto descriptorSet = vsg::DescriptorSet::create(descriptorSetLayout, vsg::Descriptors{texture});
-        auto bindDescriptorSet = vsg::BindDescriptorSet::create(VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline->layout, 0, descriptorSet);
-
-        // create StateGroup as the root of the scene/command graph to hold the GraphicsProgram, and binding of Descriptors to decorate the whole graph
-        scenegraph->add(bindGraphicsPipeline);
-        scenegraph->add(bindDescriptorSet);
-
-        // set up model transformation node
-        auto transform = vsg::MatrixTransform::create(); // VK_SHADER_STAGE_VERTEX_BIT
-
-        // add transform to root of the scene graph
-        scenegraph->addChild(transform);
-
-        // set up vertex and index arrays
-        auto vertices = vsg::vec3Array::create(
-            {{-0.5f, -0.5f, 0.0f},
-            {0.5f, -0.5f, 0.0f},
-            {0.5f, 0.5f, 0.0f},
-            {-0.5f, 0.5f, 0.0f},
-            {-0.5f, -0.5f, -0.5f},
-            {0.5f, -0.5f, -0.5f},
-            {0.5f, 0.5f, -0.5f},
-            {-0.5f, 0.5f, -0.5f}}); // VK_FORMAT_R32G32B32_SFLOAT, VK_VERTEX_INPUT_RATE_INSTANCE, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
-
-        auto colors = vsg::vec3Array::create(vertices->size(), vsg::vec3(1.0f, 1.0f, 1.0f));
-
-        auto texcoords = vsg::vec2Array::create(
-            {{0.0f, 0.0f},
-            {1.0f, 0.0f},
-            {1.0f, 1.0f},
-            {0.0f, 1.0f},
-            {0.0f, 0.0f},
-            {1.0f, 0.0f},
-            {1.0f, 1.0f},
-            {0.0f, 1.0f}}); // VK_FORMAT_R32G32_SFLOAT, VK_VERTEX_INPUT_RATE_VERTEX, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
-
-        auto indices = vsg::ushortArray::create(
-            {0, 1, 2,
-            2, 3, 0,
-            4, 5, 6,
-            6, 7, 4}); // VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
-
-        // setup geometry
-        auto drawCommands = vsg::Commands::create();
-        drawCommands->addChild(vsg::BindVertexBuffers::create(0, vsg::DataList{vertices, colors, texcoords}));
-        drawCommands->addChild(vsg::BindIndexBuffer::create(indices));
-        drawCommands->addChild(vsg::DrawIndexed::create(12, 1, 0, 0, 0));
-
-        // add drawCommands to transform
-        transform->addChild(drawCommands);
+        // create height field use the texture data as a displacementMap
+        stateInfo.displacementMap = textureData;
+        scenegraph->addChild(builder.createHeightField(geomInfo, stateInfo));
+    }
+    else
+    {
+        // create textured box
+        stateInfo.image = textureData;
+        scenegraph->addChild(builder.createBox(geomInfo, stateInfo));
     }
 
 
@@ -248,7 +170,34 @@ int main(int argc, char** argv)
 
     // texture has been filled in so it's now safe to get the ImageInfo that holds the handles to the texture's
     vsg::ImageInfo textureImageInfo;
-    if (!texture->imageInfoList.empty()) textureImageInfo = texture->imageInfoList[0]; // contextID=0, and only one imageData
+    struct FindDescriptorImage : public vsg::Visitor
+    {
+        vsg::ImageInfo& imageInfo;
+
+        FindDescriptorImage(vsg::ImageInfo& im) : imageInfo(im) {}
+
+        void apply(vsg::Object& object) override
+        {
+            object.traverse(*this);
+        }
+        void apply(vsg::StateGroup& sg) override
+        {
+            for(auto& sc : sg.stateCommands) { sc->accept(*this); }
+            sg.traverse(*this);
+        }
+        void apply(vsg::DescriptorImage& di) override
+        {
+            if (!di.imageInfoList.empty()) imageInfo = di.imageInfoList[0]; // contextID=0, and only one imageData
+        }
+
+    } fdi(textureImageInfo);
+    scenegraph->accept(fdi);
+
+    if (!textureImageInfo.imageView)
+    {
+        std::cout<<"Can not locate imageInfo to update."<<std::endl;
+        return 1;
+    }
 
     auto startTime = vsg::clock::now();
     double numFramesCompleted = 0.0;
