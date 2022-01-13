1149c4fd96f0b4a2ce1ada2cc4254be3e818d791
Robert Osfield <robert@openscenegraph.com>
2022-01-13 10:57:59 +0000

    Added second approach for implementing custom visitors and types.

---
+++ examples/core/vsgvisitorcustomtype/AlternateVisitorCustomType.h
@@ -0,0 +1,70 @@
+#include <vsg/core/Visitor.h>
+#include <vsg/nodes/Group.h>
+
+class AlternateCustomGroupNode;
+class AlternateCustomLODNode;
+
+class AlternateCustomVisitorBase : public vsg::Inherit<vsg::Visitor, AlternateCustomVisitorBase>
+{
+public:
+
+    virtual void apply(AlternateCustomGroupNode& /*node*/) {}
+    virtual void apply(AlternateCustomLODNode& /*node*/) {}
+};
+
+class AlternateCustomGroupNode : public vsg::Inherit<vsg::Group, AlternateCustomGroupNode>
+{
+public:
+
+    void accept(vsg::Visitor& visitor) override
+    {
+        if (auto acvb = visitor.cast<AlternateCustomVisitorBase>()) acvb->apply(*this);
+        else visitor.apply(*this);
+    }
+
+    std::string name = "alternate car";
+
+protected:
+
+    ~AlternateCustomGroupNode() = default;
+};
+
+class AlternateCustomLODNode : public vsg::Inherit<vsg::Group, AlternateCustomLODNode>
+{
+public:
+
+    void accept(vsg::Visitor& visitor) override
+    {
+        if (auto acvb = visitor.cast<AlternateCustomVisitorBase>()) acvb->apply(*this);
+        else visitor.apply(*this);
+    }
+
+    double maxDistance = 2.0;
+
+protected:
+
+    ~AlternateCustomLODNode() = default;
+};
+
+class AlternateVisitCustomTypes : public AlternateCustomVisitorBase
+{
+    public:
+
+        void apply(vsg::Group& group) override
+        {
+            std::cout << "apply(Group& node)"<<std::endl;
+            group.traverse(*this);
+        }
+
+        void apply(AlternateCustomGroupNode& node) override
+        {
+            std::cout << "apply(AlternateCustomGroupNode& node) name = "<<node.name<<std::endl;
+            node.traverse(*this);
+        }
+
+        void apply(AlternateCustomLODNode& node) override
+        {
+            std::cout << "apply(AlternateCustomLODNode& node) maxDistance = "<<node.maxDistance<<std::endl;
+            node.traverse(*this);
+        }
+};
--- examples/core/vsgvisitorcustomtype/vsgvisitorcustomtype.cpp
+++ examples/core/vsgvisitorcustomtype/vsgvisitorcustomtype.cpp
@@ -1,89 +1,47 @@
-#include <vsg/all.h>
-
 /*
  * shows how to setup visitors that can visit custom types
  * https://groups.google.com/g/vsg-users/c/YpSZwrKlcKI
  * https://groups.google.com/g/vsg-users/c/dpazkzRLO_8/m/TsqQWnN6AQAJ
  */
 
-class CustomGroupNode;
-class CustomLODNode;
+#include <iostream>
 
-class CustomVisitorBase : public vsg::Inherit<vsg::Visitor, CustomVisitorBase>
+#include "VisitorCustomType.h"
+#include "AlternateVisitorCustomType.h"
+
+int main(int, char**)
 {
-public:
+    // Approach 1
+    {
+        std::cout<<"First approach to implementing custom types and custom visitors that support these custom types."<<std::endl;
 
-    virtual void apply(CustomGroupNode& node) {}
-    virtual void apply(CustomLODNode& node) {}
-};
+        auto group = vsg::Group::create();
 
-class CustomGroupNode : public vsg::Inherit<vsg::Group, CustomGroupNode>
-{
-    public:
+        auto child1 = CustomGroupNode::create();
+        auto child2 = CustomLODNode::create();
 
-        void accept(vsg::Visitor& visitor) override
-        {
-            if (auto v = visitor.cast<CustomVisitorBase>(); v != nullptr)
-            {
-                v->apply(*this);
-            }
-            else
-            {
-                visitor.apply(*this);
-            }
-        }
+        group->addChild(child1);
+        group->addChild(child2);
 
-    protected:
+        VisitCustomTypes v;
+        group->accept(v);
+    }
 
-        ~CustomGroupNode() = default;
-};
+    // Approach 2
+    {
+        std::cout<<"\nSecond/Alternate approach to implementing custom types and custom visitors that support these custom types."<<std::endl;
 
-class CustomLODNode : public vsg::Inherit<vsg::Group, CustomLODNode>
-{
-    public:
+        auto group = vsg::Group::create();
 
-        void accept(vsg::Visitor& visitor) override
-        {
-            if (auto v = visitor.cast<CustomVisitorBase>(); v != nullptr)
-            {
-                v->apply(*this);
-            }
-            else
-            {
-                visitor.apply(*this);
-            }
-        }
+        auto child1 = AlternateCustomGroupNode::create();
+        auto child2 = AlternateCustomLODNode::create();
 
-    protected:
+        group->addChild(child1);
+        group->addChild(child2);
 
-        ~CustomLODNode() = default;
-};
+        AlternateVisitCustomTypes v;
+        group->accept(v);
+    }
 
-class VisitCustomTypes : public CustomVisitorBase
-{
-    public:
-
-        void apply(CustomGroupNode& node) override
-        {
-            std::cout << "apply(CustomGroupNode& node)" << std::endl;
-        }
-
-        void apply(CustomLODNode& node) override
-        {
-            std::cout << "apply(CustomLODNode& node)" << std::endl;
-        }
-};
-
-int main(int argc, char* argv[])
-{
-    auto group = vsg::Group::create();
-
-    auto child1 = CustomGroupNode::create();
-    auto child2 = CustomLODNode::create();
-
-    group->addChild(child1);
-    group->addChild(child2);
-
-    VisitCustomTypes v;
-    group->traverse(v);
+    return 0;
 }
