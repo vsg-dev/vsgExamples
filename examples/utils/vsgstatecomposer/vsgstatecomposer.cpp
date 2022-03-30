3b830c0539520c45f40baf95bb6bc42921b08ec0
Robert Osfield <robert@openscenegraph.com>
2022-03-30 11:33:40 +0100

    Refactored to stress test vsg::SharedObjects.

--- examples/utils/vsgstatecomposer/vsgstatecomposer.cpp
+++ examples/utils/vsgstatecomposer/vsgstatecomposer.cpp
@@ -61,6 +61,8 @@ int main(int argc, char** argv)
     auto shaderSetFile = arguments.value<vsg::Path>("", "-s");
     auto outputFile = arguments.value<vsg::Path>("", "-o");
     auto outputShaderSetFile = arguments.value<vsg::Path>("", "--os");
+    auto share = arguments.read("--share");
+    auto numInstances = arguments.value<size_t>(1, "-n");
 
     if (arguments.errors()) return arguments.writeErrorMessages(std::cerr);
 
@@ -80,113 +82,177 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    auto graphicsPipelineConfig = vsg::GraphicsPipelineConfig::create(shaderSet);
+    auto sharedObjects = vsg::SharedObjects::create_if(share);
 
-    // set up graphics pipeline
-    vsg::Descriptors descriptors;
+    vsg::dvec3 position{0.0, 0.0, 0.0};
+    vsg::dvec3 delta_column{2.0, 0.0, 0.0};
+    vsg::dvec3 delta_row{0.0, 2.0, 0.0};
 
-    // read texture image
-    if (!textureFile.empty())
+    auto scenegraph = vsg::Group::create();
+
+    size_t numColumns = std::max(size_t(1), static_cast<size_t>(sqrt(static_cast<double>(numInstances))));
+    size_t numRows = std::max(size_t(1), numInstances / numColumns);
+
+    for(size_t r=0; (r < numRows) && (scenegraph->children.size() < numInstances); ++r)
     {
-        auto textureData = vsg::read_cast<vsg::Data>(textureFile, options);
-        if (!textureData)
+        for(size_t c=0; (c < numColumns) && (scenegraph->children.size() < numInstances); ++c)
         {
-            std::cout << "Could not read texture file : " << textureFile << std::endl;
-            return 1;
-        }
+            auto graphicsPipelineConfig = vsg::GraphicsPipelineConfig::create(shaderSet);
 
-        // enable texturing
-        graphicsPipelineConfig->assignTexture(descriptors, "diffuseMap", textureData);
+            // set up graphics pipeline
+            vsg::Descriptors descriptors;
+
+            // read texture image
+            if (!textureFile.empty())
+            {
+                auto textureData = vsg::read_cast<vsg::Data>(textureFile, options);
+                if (!textureData)
+                {
+                    std::cout << "Could not read texture file : " << textureFile << std::endl;
+                    return 1;
+                }
+
+                // enable texturing
+                graphicsPipelineConfig->assignTexture(descriptors, "diffuseMap", textureData);
+            }
+
+            // set up pass of material
+            auto mat = vsg::PhongMaterialValue::create();
+            mat->value().diffuse.set(1.0f, 1.0f, 0.0f, 1.0f);
+            mat->value().specular.set(1.0f, 0.0f, 0.0f, 1.0f);
+
+            graphicsPipelineConfig->assignUniform(descriptors, "material", mat);
+
+            if (sharedObjects) sharedObjects->share(descriptors);
+
+            // set up vertex and index arrays
+            auto vertices = vsg::vec3Array::create(
+                {{-0.5f, -0.5f, 0.0f},
+                {0.5f, -0.5f, 0.0f},
+                {0.5f, 0.5f, 0.0f},
+                {-0.5f, 0.5f, 0.0f},
+                {-0.5f, -0.5f, -0.5f},
+                {0.5f, -0.5f, -0.5f},
+                {0.5f, 0.5f, -0.5f},
+                {-0.5f, 0.5f, -0.5f}});
+
+            auto normals = vsg::vec3Array::create(
+                {{0.0f, 0.0f, 1.0f},
+                {0.0f, 0.0f, 1.0f},
+                {0.0f, 0.0f, 1.0f},
+                {0.0f, 0.0f, 1.0f},
+                {0.0f, 0.0f, 1.0f},
+                {0.0f, 0.0f, 1.0f},
+                {0.0f, 0.0f, 1.0f},
+                {0.0f, 0.0f, 1.0f}});
+
+            auto texcoords = vsg::vec2Array::create(
+                {{0.0f, 0.0f},
+                {1.0f, 0.0f},
+                {1.0f, 1.0f},
+                {0.0f, 1.0f},
+                {0.0f, 0.0f},
+                {1.0f, 0.0f},
+                {1.0f, 1.0f},
+                {0.0f, 1.0f}});
+
+            auto colors = vsg::vec4Array::create(
+                {{1.0f, 1.0f, 1.0f, 1.0f},
+                {1.0f, 1.0f, 1.0f, 1.0f},
+                {1.0f, 1.0f, 1.0f, 1.0f},
+                {1.0f, 1.0f, 1.0f, 1.0f},
+                {1.0f, 1.0f, 1.0f, 1.0f},
+                {1.0f, 1.0f, 1.0f, 1.0f},
+                {1.0f, 1.0f, 1.0f, 1.0f},
+                {1.0f, 1.0f, 1.0f, 1.0f},
+                });
+
+            auto indices = vsg::ushortArray::create(
+                {0, 1, 2,
+                2, 3, 0,
+                4, 5, 6,
+                6, 7, 4});
+
+            vsg::DataList vertexArrays;
+
+            graphicsPipelineConfig->assignArray(vertexArrays, "vsg_Vertex", VK_VERTEX_INPUT_RATE_VERTEX, vertices);
+            graphicsPipelineConfig->assignArray(vertexArrays, "vsg_Normal", VK_VERTEX_INPUT_RATE_VERTEX, normals);
+            graphicsPipelineConfig->assignArray(vertexArrays, "vsg_TexCoord0", VK_VERTEX_INPUT_RATE_VERTEX, texcoords);
+            graphicsPipelineConfig->assignArray(vertexArrays, "vsg_Color", VK_VERTEX_INPUT_RATE_VERTEX, colors);
+
+            if (sharedObjects) sharedObjects->share(vertexArrays);
+            if (sharedObjects) indices = sharedObjects->share(indices);
+
+            // setup geometry
+            auto drawCommands = vsg::Commands::create();
+            drawCommands->addChild(vsg::BindVertexBuffers::create(graphicsPipelineConfig->baseAttributeBinding, vertexArrays));
+            drawCommands->addChild(vsg::BindIndexBuffer::create(indices));
+            drawCommands->addChild(vsg::DrawIndexed::create(12, 1, 0, 0, 0));
+
+            if (sharedObjects)
+            {
+                sharedObjects->share(drawCommands->children);
+                drawCommands = sharedObjects->share(drawCommands);
+            }
+
+            // share the pipeline config and initilaize if it's unique
+            if (sharedObjects) graphicsPipelineConfig = sharedObjects->share(graphicsPipelineConfig, [](auto gpc) { gpc->init(); });
+            else graphicsPipelineConfig->init();
+
+            auto descriptorSet = vsg::DescriptorSet::create(graphicsPipelineConfig->descriptorSetLayout, descriptors);
+            if (sharedObjects) descriptorSet = sharedObjects->share(descriptorSet);
+
+            auto bindDescriptorSet = vsg::BindDescriptorSet::create(VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipelineConfig->layout, 0, descriptorSet);
+            if (sharedObjects) bindDescriptorSet = sharedObjects->share(bindDescriptorSet);
+
+            // create StateGroup as the root of the scene/command graph to hold the GraphicsProgram, and binding of Descriptors to decorate the whole graph
+            auto stateGroup = vsg::StateGroup::create();
+            stateGroup->add(graphicsPipelineConfig->bindGraphicsPipeline);
+            stateGroup->add(bindDescriptorSet);
+
+            // set up model transformation node
+            auto transform = vsg::MatrixTransform::create(vsg::translate(position + delta_row * static_cast<double>(r) + delta_column * static_cast<double>(c)));
+            transform->subgraphRequiresLocalFrustum = false;
+            //auto transform = vsg::MatrixTransform::create(vsg::translate(position));
+
+#if 0
+            // add drawCommands to transform
+            transform->addChild(drawCommands);
+
+            if (sharedObjects)
+            {
+                transform = sharedObjects->share(transform);
+            }
+
+            // add transform to root of the scene graph
+            stateGroup->addChild(transform);
+            if (sharedObjects)
+            {
+                stateGroup = sharedObjects->share(stateGroup);
+            }
+
+            scenegraph->addChild(stateGroup);
+#else
+            // add drawCommands to StateGroup
+            stateGroup->addChild(drawCommands);
+            if (sharedObjects)
+            {
+                stateGroup = sharedObjects->share(stateGroup);
+            }
+
+            transform->addChild(stateGroup);
+
+            if (sharedObjects)
+            {
+                transform = sharedObjects->share(transform);
+            }
+
+            scenegraph->addChild(transform);
+#endif
+        }
     }
 
-    // set up pass of material
-    auto mat = vsg::PhongMaterialValue::create();
-    mat->value().diffuse.set(1.0f, 1.0f, 0.0f, 1.0f);
-    mat->value().specular.set(1.0f, 0.0f, 0.0f, 1.0f);
-
-    graphicsPipelineConfig->assignUniform(descriptors, "material", mat);
-
-    // set up vertex and index arrays
-    auto vertices = vsg::vec3Array::create(
-        {{-0.5f, -0.5f, 0.0f},
-         {0.5f, -0.5f, 0.0f},
-         {0.5f, 0.5f, 0.0f},
-         {-0.5f, 0.5f, 0.0f},
-         {-0.5f, -0.5f, -0.5f},
-         {0.5f, -0.5f, -0.5f},
-         {0.5f, 0.5f, -0.5f},
-         {-0.5f, 0.5f, -0.5f}});
-
-    auto normals = vsg::vec3Array::create(
-        {{0.0f, 0.0f, 1.0f},
-         {0.0f, 0.0f, 1.0f},
-         {0.0f, 0.0f, 1.0f},
-         {0.0f, 0.0f, 1.0f},
-         {0.0f, 0.0f, 1.0f},
-         {0.0f, 0.0f, 1.0f},
-         {0.0f, 0.0f, 1.0f},
-         {0.0f, 0.0f, 1.0f}});
-
-    auto texcoords = vsg::vec2Array::create(
-        {{0.0f, 0.0f},
-         {1.0f, 0.0f},
-         {1.0f, 1.0f},
-         {0.0f, 1.0f},
-         {0.0f, 0.0f},
-         {1.0f, 0.0f},
-         {1.0f, 1.0f},
-         {0.0f, 1.0f}});
-
-    auto colors = vsg::vec4Array::create(
-        {{1.0f, 1.0f, 1.0f, 1.0f},
-         {1.0f, 1.0f, 1.0f, 1.0f},
-         {1.0f, 1.0f, 1.0f, 1.0f},
-         {1.0f, 1.0f, 1.0f, 1.0f},
-         {1.0f, 1.0f, 1.0f, 1.0f},
-         {1.0f, 1.0f, 1.0f, 1.0f},
-         {1.0f, 1.0f, 1.0f, 1.0f},
-         {1.0f, 1.0f, 1.0f, 1.0f},
-        });
-
-    auto indices = vsg::ushortArray::create(
-        {0, 1, 2,
-         2, 3, 0,
-         4, 5, 6,
-         6, 7, 4});
-
-    vsg::DataList vertexArrays;
-    graphicsPipelineConfig->assignArray(vertexArrays, "vsg_Vertex", VK_VERTEX_INPUT_RATE_VERTEX, vertices);
-    graphicsPipelineConfig->assignArray(vertexArrays, "vsg_Normal", VK_VERTEX_INPUT_RATE_VERTEX, normals);
-    graphicsPipelineConfig->assignArray(vertexArrays, "vsg_TexCoord0", VK_VERTEX_INPUT_RATE_VERTEX, texcoords);
-    graphicsPipelineConfig->assignArray(vertexArrays, "vsg_Color", VK_VERTEX_INPUT_RATE_VERTEX, colors);
-
-    // setup geometry
-    auto drawCommands = vsg::Commands::create();
-    drawCommands->addChild(vsg::BindVertexBuffers::create(graphicsPipelineConfig->baseAttributeBinding, vertexArrays));
-    drawCommands->addChild(vsg::BindIndexBuffer::create(indices));
-    drawCommands->addChild(vsg::DrawIndexed::create(12, 1, 0, 0, 0));
-
-    // set up the DescriptorSetLayout, PipelineLayout and GraphicsPipeline
-    graphicsPipelineConfig->init();
-
-    auto descriptorSet = vsg::DescriptorSet::create(graphicsPipelineConfig->descriptorSetLayout, descriptors);
-    auto bindDescriptorSet = vsg::BindDescriptorSet::create(VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipelineConfig->layout, 0, descriptorSet);
-
-    // create StateGroup as the root of the scene/command graph to hold the GraphicsProgram, and binding of Descriptors to decorate the whole graph
-    auto scenegraph = vsg::StateGroup::create();
-    scenegraph->add(graphicsPipelineConfig->bindGraphicsPipeline);
-    scenegraph->add(bindDescriptorSet);
-
-    // set up model transformation node
-    auto transform = vsg::MatrixTransform::create();
-
-    // add transform to root of the scene graph
-    scenegraph->addChild(transform);
-
-
-    // add drawCommands to transform
-    transform->addChild(drawCommands);
-
+    if (sharedObjects) sharedObjects->report(std::cout);
 
     // create the viewer and assign window(s) to it
     auto viewer = vsg::Viewer::create();
@@ -200,10 +266,19 @@ int main(int argc, char** argv)
 
     viewer->addWindow(window);
 
+    vsg::ComputeBounds computeBounds;
+    scenegraph->accept(computeBounds);
+    vsg::dvec3 centre = (computeBounds.bounds.min + computeBounds.bounds.max) * 0.5;
+    double radius = vsg::length(computeBounds.bounds.max - computeBounds.bounds.min) * 0.6 * 3.0;
+
+    std::cout << "centre = " << centre << std::endl;
+    std::cout << "radius = " << radius << std::endl;
+
     // camera related details
+    double nearFarRatio = 0.001;
     auto viewport = vsg::ViewportState::create(0, 0, window->extent2D().width, window->extent2D().height);
-    auto perspective = vsg::Perspective::create(60.0, static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height), 0.1, 10.0);
-    auto lookAt = vsg::LookAt::create(vsg::dvec3(1.0, 1.0, 1.0), vsg::dvec3(0.0, 0.0, 0.0), vsg::dvec3(0.0, 0.0, 1.0));
+    auto perspective = vsg::Perspective::create(60.0, static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height), nearFarRatio * radius, radius * 10.0);
+    auto lookAt = vsg::LookAt::create(centre + vsg::dvec3(0.0, -radius * 3.5, 0.0), centre, vsg::dvec3(0.0, 0.0, 1.0));
     auto camera = vsg::Camera::create(perspective, lookAt, viewport);
 
     auto commandGraph = vsg::createCommandGraphForView(window, camera, scenegraph);
@@ -236,8 +311,8 @@ int main(int argc, char** argv)
         viewer->handleEvents();
 
         // animate the transform
-        float time = std::chrono::duration<float, std::chrono::seconds::period>(viewer->getFrameStamp()->time - viewer->start_point()).count();
-        transform->matrix = vsg::rotate(time * vsg::radians(90.0f), vsg::vec3(0.0f, 0.0, 1.0f));
+        //float time = std::chrono::duration<float, std::chrono::seconds::period>(viewer->getFrameStamp()->time - viewer->start_point()).count();
+        //transform->matrix = vsg::rotate(time * vsg::radians(90.0f), vsg::vec3(0.0f, 0.0, 1.0f));
 
         viewer->update();
 
