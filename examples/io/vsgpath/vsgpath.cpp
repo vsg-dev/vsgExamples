99e828be12c1ca1c5b9081e82f8506371fb3c19d
Robert Osfield <robert@openscenegraph.com>
2022-05-06 11:56:02 +0100

    Added tests for file operations.

--- examples/io/vsgpath/vsgpath.cpp
+++ examples/io/vsgpath/vsgpath.cpp
@@ -4,54 +4,37 @@
 #include <iostream>
 
 
-class MyPath
-{
-public:
-
-    MyPath(const std::wstring& str):
-        _wstring(str)
-    {
-        std::cout<<"MyPath(wstring) "<<str.size()<<std::endl;
-    }
-    MyPath(const std::string& str):
-        _string(str)
-    {
-        std::cout<<"MyPath(string) "<<str<<std::endl;
-    }
-
-    std::string _string;
-    std::wstring _wstring;
-
-//    operator const char* () { return _string.c_str(); }
-    operator const wchar_t* () { return _wstring.c_str(); }
-};
-
-
-void print(const char* str)
-{
-    std::cout<<"print(char_t) "<<str<<std::endl;
-}
-void print(const wchar_t* wstr)
-{
-    std::cout<<"print(wchar_t) "<<wstr<<std::endl;
-}
-
 
 int main(int argc, char** argv)
 {
     vsg::CommandLine arguments(&argc, argv);
 
-    std::cout<<"vsgpath "<<std::endl;
+    std::vector<vsg::Path> paths;
+    paths.emplace_back("one");
+    paths.emplace_back("one.dot");
+    paths.emplace_back("one.dot.two");
+    paths.emplace_back("three/");
+    paths.emplace_back("four/.");
+    paths.emplace_back("five/..");
+    paths.emplace_back("six\\.");
+    paths.emplace_back("seven\\..");
+    paths.emplace_back(L"eight.dot");
+    paths.emplace_back("nine/ten/eleven.dot.twelve");
+    paths.emplace_back("therteen.fourteen/fifeteen");
+    paths.emplace_back("");
+    paths.emplace_back(".");
+    paths.emplace_back("..");
 
-    auto c_string = "c_string";
-    auto w_string = L"w_string";
+    for(auto& path : paths)
+    {
+        std::cout<<"path = "<<path<<std::endl;
+        std::cout<<"    vsg::filePath(\""<<path<<"\") = "<<vsg::filePath(path)<<std::endl;
+        std::cout<<"    vsg::fileExtension(\""<<path<<"\") = "<<vsg::fileExtension(path)<<std::endl;
+        std::cout<<"    vsg::simpleFilename(\""<<path<<"\") = "<<vsg::simpleFilename(path)<<std::endl;
+        std::cout<<std::endl;
+    }
 
-    std::cout<<"c_string "<<typeid(*c_string).name()<<std::endl;
-    std::cout<<"w_string "<<typeid(*w_string).name()<<std::endl;
 
-    MyPath path1("string 1");
-
-    print(path1);
 
     return 0;
 }
