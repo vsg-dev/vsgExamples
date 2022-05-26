c242ce64fd308d88c95bfeb6bc98f598f6c3774e
Robert Osfield <robert@openscenegraph.com>
2022-05-26 16:07:20 +0100

    Created beginnings of a new vsgcustomtext example that will illustrate how to override the standard "text" vsg::ShadrSet.

---
+++ examples/text/vsgcustomtext/vsgcustomtext.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <vsg/all.h>
+
+#ifdef vsgXchange_FOUND
+#    include <vsgXchange/all.h>
+#endif
+
+
+int main(int argc, char** argv)
+{
+    // set up defaults and read command line arguments to override them
+    vsg::CommandLine arguments(&argc, argv);
+
+    auto windowTraits = vsg::WindowTraits::create("vsg::Text example");
+    windowTraits->debugLayer = arguments.read({"--debug", "-d"});
+    windowTraits->apiDumpLayer = arguments.read({"--api", "-a"});
+    arguments.read({"--window", "-w"}, windowTraits->width, windowTraits->height);
+    auto font_filename = arguments.value(std::string("fonts/times.vsgb"), "-f");
+    auto output_filename = arguments.value(std::string(), "-o");
+    auto numFrames = arguments.value(-1, "--nf");
+    auto clearColor = arguments.value(vsg::vec4(0.2f, 0.2f, 0.4f, 1.0f), "--clear");
+
+    if (arguments.errors()) return arguments.writeErrorMessages(std::cerr);
+
+    // set up search paths to SPIRV shaders and textures
+    auto options = vsg::Options::create();
+    options->paths = vsg::getEnvPaths("VSG_FILE_PATH");
+#ifdef vsgXchange_all
+    // add vsgXchange's support for reading and writing 3rd party file formats
+    options->add(vsgXchange::all::create());
+#endif
+
+    auto font = vsg::read_cast<vsg::Font>(font_filename, options);
+
+    if (!font)
+    {
+        std::cout << "Failing to read font : " << font_filename << std::endl;
+        return 1;
+    }
+
+    font->options = options;
+
+    // set up model transformation node
+    auto scenegraph = vsg::Group::create();
+
+    {
+        auto layout = vsg::StandardLayout::create();
+        layout->horizontalAlignment = vsg::StandardLayout::CENTER_ALIGNMENT;
+        layout->position = vsg::vec3(6.0, 0.0, 0.0);
+        layout->horizontal = vsg::vec3(1.0, 0.0, 0.0);
+        layout->vertical = vsg::vec3(0.0, 0.0, 1.0);
+        layout->color = vsg::vec4(1.0, 1.0, 1.0, 1.0);
+        layout->outlineWidth = 0.1;
+
+        auto text = vsg::Text::create();
+        text->text = vsg::stringValue::create("Standard vsg::Text shaders.");
+        text->font = font;
+        text->font->options = options;
+        text->layout = layout;
+        text->setup();
+        scenegraph->addChild(text);
+    }
+
+    {
+        auto layout = vsg::StandardLayout::create();
+        layout->horizontalAlignment = vsg::StandardLayout::CENTER_ALIGNMENT;
+        layout->position = vsg::vec3(6.0, 0.0, -2.0);
+        layout->horizontal = vsg::vec3(1.0, 0.0, 0.0);
+        layout->vertical = vsg::vec3(0.0, 0.0, 1.0);
+        layout->color = vsg::vec4(1.0, 1.0, 1.0, 1.0);
+        layout->outlineWidth = 0.1;
+
+        auto text = vsg::Text::create();
+        text->text = vsg::stringValue::create("Custom vsg::Text shaders.");
+        text->font = font;
+        text->font->options = options;
+        text->layout = layout;
+        text->setup();
+        scenegraph->addChild(text);
+    }
+
+    if (!output_filename.empty())
+    {
+        vsg::write(scenegraph, output_filename);
+        return 1;
+    }
+
+    // create the viewer and assign window(s) to it
+    auto viewer = vsg::Viewer::create();
+
+    auto window = vsg::Window::create(windowTraits);
+    if (!window)
+    {
+        std::cout << "Could not create window." << std::endl;
+        return 1;
+    }
+
+    window->clearColor() = VkClearColorValue{{clearColor.r, clearColor.g, clearColor.b, clearColor.a}};
+
+    viewer->addWindow(window);
+
+    // camera related details
+    // compute the bounds of the scene graph to help position camera
+    vsg::ComputeBounds computeBounds;
+    scenegraph->accept(computeBounds);
+    vsg::dvec3 centre = (computeBounds.bounds.min + computeBounds.bounds.max) * 0.5;
+    double radius = vsg::length(computeBounds.bounds.max - computeBounds.bounds.min) * 0.6;
+    double nearFarRatio = 0.001;
+
+    // set up the camera
+    auto viewport = vsg::ViewportState::create(window->extent2D());
+    auto perspective = vsg::Perspective::create(30.0, static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height), nearFarRatio * radius, radius * 1000.0);
+    auto lookAt = vsg::LookAt::create(centre + vsg::dvec3(0.0, -radius * 3.5, 0.0), centre, vsg::dvec3(0.0, 0.0, 1.0));
+    auto camera = vsg::Camera::create(perspective, lookAt, viewport);
+
+    auto commandGraph = vsg::createCommandGraphForView(window, camera, scenegraph);
+    viewer->assignRecordAndSubmitTaskAndPresentation({commandGraph});
+
+    // compile the Vulkan objects
+    viewer->compile();
+
+    // assign Trackball
+    viewer->addEventHandler(vsg::Trackball::create(camera));
+
+    // assign a CloseHandler to the Viewer to respond to pressing Escape or press the window close button
+    viewer->addEventHandlers({vsg::CloseHandler::create(viewer)});
+
+    // main frame loop
+    while (viewer->advanceToNextFrame() && (numFrames < 0 || (numFrames--) > 0))
+    {
+        // pass any events into EventHandlers assigned to the Viewer
+        viewer->handleEvents();
+
+        viewer->update();
+
+        viewer->recordAndSubmit();
+
+        viewer->present();
+    }
+
+    // clean up done automatically thanks to ref_ptr<>
+    return 0;
+}
