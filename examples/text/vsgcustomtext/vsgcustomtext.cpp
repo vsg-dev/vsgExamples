1ffb610349427acdd96ca04bfa0a6c130e2d243e
Robert Osfield <robert@openscenegraph.com>
2022-05-27 15:51:47 +0100

    Adapted to code to add billboard uniform

--- examples/text/vsgcustomtext/vsgcustomtext.cpp
+++ examples/text/vsgcustomtext/vsgcustomtext.cpp
@@ -5,6 +5,13 @@
 #    include <vsgXchange/all.h>
 #endif
 
+struct BillboardUniform
+{
+    float scale = 2.0f;
+};
+
+using BillboardUniformValue = vsg::Value<BillboardUniform>;
+
 vsg::ref_ptr<vsg::ShaderSet> createMyTextShaderSet(const vsg::Path& vertesShaderFilename,  const vsg::Path& fragmentShaderFilename, vsg::ref_ptr<const vsg::Options> options)
 {
     // based on VulkanSceneGraph/src/text/Text.cpp's vsg::createTextShaderSet(ref_ptr<const Options> options) implementation
@@ -23,23 +30,282 @@ vsg::ref_ptr<vsg::ShaderSet> createMyTextShaderSet(const vsg::Path& vertesShader
 
     auto shaderSet = vsg::ShaderSet::create(vsg::ShaderStages{vertexShader, fragmentShader});
 
-    // used for both CPU and GPU layouts
     shaderSet->addAttributeBinding("inPosition", "", 0, VK_FORMAT_R32G32B32_SFLOAT, vsg::vec3Array::create(1));
     shaderSet->addUniformBinding("textureAtlas", "", 0, 0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_FRAGMENT_BIT, vsg::vec4Array2D::create(1, 1));
+
+    // use only for the billboard code path.
+    shaderSet->addUniformBinding("billboardUniform", "BILLBOARD_AND_SCALE", 0, 1, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1, VK_SHADER_STAGE_VERTEX_BIT, BillboardUniformValue::create());
+
     shaderSet->addPushConstantRange("pc", "", VK_SHADER_STAGE_VERTEX_BIT, 0, 128);
 
-    // only used when using CPU Layout
     shaderSet->addAttributeBinding("inColor", "CPU_LAYOUT", 1, VK_FORMAT_R32G32B32A32_SFLOAT, vsg::vec4Array::create(1));
     shaderSet->addAttributeBinding("inOutlineColor", "CPU_LAYOUT", 2, VK_FORMAT_R32G32B32A32_SFLOAT, vsg::vec4Array::create(1));
     shaderSet->addAttributeBinding("inOutlineWidth", "CPU_LAYOUT", 3, VK_FORMAT_R32_SFLOAT, vsg::floatArray::create(1));
     shaderSet->addAttributeBinding("inTexCoord", "CPU_LAYOUT", 4, VK_FORMAT_R32G32B32_SFLOAT, vsg::vec3Array::create(1));
 
-    // only used when using GPU Layout
-    shaderSet->addUniformBinding("glyphMetrics", "GPU_LAYOUT", 0, 1, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_VERTEX_BIT, vsg::vec4Array2D::create(1, 1));
-    shaderSet->addUniformBinding("textLayout", "GPU_LAYOUT", 1, 0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1, VK_SHADER_STAGE_VERTEX_BIT, vsg::TextLayoutValue::create());
-    shaderSet->addUniformBinding("text", "GPU_LAYOUT", 1, 1, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1, VK_SHADER_STAGE_VERTEX_BIT, vsg::uivec4Array2D::create(1, 1));
+    // assign the custom uniform binding
 
     return shaderSet;
+}
+
+class CustomTechnique : public vsg::Inherit<vsg::CpuLayoutTechnique, CustomTechnique>
+{
+public:
+    void setup(vsg::Text* text, uint32_t minimumAllocation = 0) override;
+
+};
+
+void CustomTechnique::setup(vsg::Text* text, uint32_t minimumAllocation)
+{
+    std::cout<<"CustomTechnique::setup()"<<std::endl;
+
+    auto layout = text->layout;
+
+    vsg::TextQuads quads;
+    layout->layout(text->text, *(text->font), quads);
+
+    vsg::vec4 color = quads.front().colors[0];
+    vsg::vec4 outlineColor = quads.front().outlineColors[0];
+    float outlineWidth = quads.front().outlineWidths[0];
+    bool singleColor = true;
+    bool singleOutlineColor = true;
+    bool singleOutlineWidth = true;
+    for (auto& quad : quads)
+    {
+        for (int i = 0; i < 4; ++i)
+        {
+            if (quad.colors[i] != color) singleColor = false;
+            if (quad.outlineColors[i] != outlineColor) singleOutlineColor = false;
+            if (quad.outlineWidths[i] != outlineWidth) singleOutlineWidth = false;
+        }
+    }
+
+    uint32_t num_quads = std::max(static_cast<uint32_t>(quads.size()), minimumAllocation);
+    uint32_t num_vertices = num_quads * 4;
+    uint32_t num_colors = singleColor ? 1 : num_vertices;
+    uint32_t num_outlineColors = singleOutlineColor ? 1 : num_vertices;
+    uint32_t num_outlineWidths = singleOutlineWidth ? 1 : num_vertices;
+
+    if (!vertices || num_vertices > vertices->size()) vertices = vsg::vec3Array::create(num_vertices);
+    if (!colors || num_colors > colors->size()) colors = vsg::vec4Array::create(num_colors);
+    if (!outlineColors || num_outlineColors > outlineColors->size()) outlineColors = vsg::vec4Array::create(num_outlineColors);
+    if (!outlineWidths || num_outlineWidths > outlineWidths->size()) outlineWidths = vsg::floatArray::create(num_outlineWidths);
+    if (!texcoords || num_vertices > texcoords->size()) texcoords = vsg::vec3Array::create(num_vertices);
+
+    uint32_t vi = 0;
+
+    float leadingEdgeGradient = 0.1f;
+
+    if (singleColor) colors->set(0, color);
+    if (singleOutlineColor) outlineColors->set(0, outlineColor);
+    if (singleOutlineWidth) outlineWidths->set(0, outlineWidth);
+
+    for (auto& quad : quads)
+    {
+        float leadingEdgeTilt = length(quad.vertices[0] - quad.vertices[1]) * leadingEdgeGradient;
+        float topEdgeTilt = leadingEdgeTilt;
+
+        vertices->set(vi, quad.vertices[0]);
+        vertices->set(vi + 1, quad.vertices[1]);
+        vertices->set(vi + 2, quad.vertices[2]);
+        vertices->set(vi + 3, quad.vertices[3]);
+
+        if (!singleColor)
+        {
+            colors->set(vi, quad.colors[0]);
+            colors->set(vi + 1, quad.colors[1]);
+            colors->set(vi + 2, quad.colors[2]);
+            colors->set(vi + 3, quad.colors[3]);
+        }
+
+        if (!singleOutlineColor)
+        {
+            outlineColors->set(vi, quad.outlineColors[0]);
+            outlineColors->set(vi + 1, quad.outlineColors[1]);
+            outlineColors->set(vi + 2, quad.outlineColors[2]);
+            outlineColors->set(vi + 3, quad.outlineColors[3]);
+        }
+
+        if (!singleOutlineWidth)
+        {
+            outlineWidths->set(vi, quad.outlineWidths[0]);
+            outlineWidths->set(vi + 1, quad.outlineWidths[1]);
+            outlineWidths->set(vi + 2, quad.outlineWidths[2]);
+            outlineWidths->set(vi + 3, quad.outlineWidths[3]);
+        }
+
+        texcoords->set(vi, vsg::vec3(quad.texcoords[0].x, quad.texcoords[0].y, leadingEdgeTilt + topEdgeTilt));
+        texcoords->set(vi + 1, vsg::vec3(quad.texcoords[1].x, quad.texcoords[1].y, topEdgeTilt));
+        texcoords->set(vi + 2, vsg::vec3(quad.texcoords[2].x, quad.texcoords[2].y, 0.0f));
+        texcoords->set(vi + 3, vsg::vec3(quad.texcoords[3].x, quad.texcoords[3].y, leadingEdgeTilt));
+
+        vi += 4;
+    }
+
+    uint32_t num_indices = num_quads * 6;
+    if (!indices || num_indices > indices->valueCount())
+    {
+        if (num_vertices > 65536) // check if requires uint or ushort indices
+        {
+            auto ui_indices = vsg::uintArray::create(num_indices);
+            indices = ui_indices;
+
+            auto itr = ui_indices->begin();
+            vi = 0;
+            for (uint32_t i = 0; i < num_quads; ++i)
+            {
+                (*itr++) = vi;
+                (*itr++) = vi + 1;
+                (*itr++) = vi + 2;
+                (*itr++) = vi + 2;
+                (*itr++) = vi + 3;
+                (*itr++) = vi;
+
+                vi += 4;
+            }
+        }
+        else
+        {
+            auto us_indices = vsg::ushortArray::create(num_indices);
+            indices = us_indices;
+
+            auto itr = us_indices->begin();
+            vi = 0;
+            for (uint32_t i = 0; i < num_quads; ++i)
+            {
+                (*itr++) = vi;
+                (*itr++) = vi + 1;
+                (*itr++) = vi + 2;
+                (*itr++) = vi + 2;
+                (*itr++) = vi + 3;
+                (*itr++) = vi;
+
+                vi += 4;
+            }
+        }
+    }
+
+    if (!drawIndexed)
+        drawIndexed = vsg::DrawIndexed::create(static_cast<uint32_t>(quads.size() * 6), 1, 0, 0, 0);
+    else
+        drawIndexed->indexCount = static_cast<uint32_t>(quads.size() * 6);
+
+    // create StateGroup as the root of the scene/command graph to hold the GraphicsProgram, and binding of Descriptors to decorate the whole graph
+    if (!scenegraph)
+    {
+        scenegraph = vsg::StateGroup::create();
+
+        auto shaderSet = createTextShaderSet(text->font->options);
+        auto config = vsg::GraphicsPipelineConfig::create(shaderSet);
+
+        auto& sharedObjects = text->font->sharedObjects;
+        if (!sharedObjects && text->font->options) sharedObjects = text->font->options->sharedObjects;
+        if (!sharedObjects) sharedObjects = vsg::SharedObjects::create();
+
+        vsg::DataList arrays;
+        config->assignArray(arrays, "inPosition", VK_VERTEX_INPUT_RATE_VERTEX, vertices);
+        config->assignArray(arrays, "inColor", singleColor ? VK_VERTEX_INPUT_RATE_INSTANCE : VK_VERTEX_INPUT_RATE_VERTEX, colors);
+        config->assignArray(arrays, "inOutlineColor", singleOutlineColor ? VK_VERTEX_INPUT_RATE_INSTANCE : VK_VERTEX_INPUT_RATE_VERTEX, outlineColors);
+        config->assignArray(arrays, "inOutlineWidth", singleOutlineWidth ? VK_VERTEX_INPUT_RATE_INSTANCE : VK_VERTEX_INPUT_RATE_VERTEX, outlineWidths);
+        config->assignArray(arrays, "inTexCoord", VK_VERTEX_INPUT_RATE_VERTEX, texcoords);
+
+        // set up sampler for atlas.
+        auto sampler = vsg::Sampler::create();
+        sampler->magFilter = VK_FILTER_LINEAR;
+        sampler->minFilter = VK_FILTER_LINEAR;
+        sampler->mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+        sampler->addressModeU = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER;
+        sampler->addressModeV = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER;
+        sampler->addressModeW = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER;
+        sampler->borderColor = VK_BORDER_COLOR_INT_TRANSPARENT_BLACK;
+        sampler->anisotropyEnable = VK_TRUE;
+        sampler->maxAnisotropy = 16.0f;
+        sampler->maxLod = 12.0;
+
+        if (sharedObjects) sharedObjects->share(sampler);
+
+        vsg::Descriptors descriptors;
+        config->assignTexture(descriptors, "textureAtlas", text->font->atlas, sampler);
+
+        // custom part - assigning our billboardUnfirom
+        config->assignUniform(descriptors, "billboardUniform", BillboardUniformValue::create());
+
+        if (sharedObjects) sharedObjects->share(descriptors);
+
+        // disable face culling so text can be seen from both sides
+        config->rasterizationState->cullMode = VK_CULL_MODE_NONE;
+
+        // set alpha blending
+        VkPipelineColorBlendAttachmentState colorBlendAttachment = {};
+        colorBlendAttachment.blendEnable = VK_TRUE;
+        colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT |
+                                              VK_COLOR_COMPONENT_G_BIT |
+                                              VK_COLOR_COMPONENT_B_BIT |
+                                              VK_COLOR_COMPONENT_A_BIT;
+
+        colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;
+        colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
+        colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;
+        colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+        colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
+        colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;
+
+        config->colorBlendState->attachments = {colorBlendAttachment};
+
+        if (sharedObjects)
+            sharedObjects->share(config, [](auto gpc) { gpc->init(); });
+        else
+            config->init();
+
+        scenegraph->add(config->bindGraphicsPipeline);
+
+        auto descriptorSetLayout = vsg::DescriptorSetLayout::create(config->descriptorBindings);
+        if (sharedObjects) sharedObjects->share(descriptorSetLayout);
+        auto descriptorSet = vsg::DescriptorSet::create(descriptorSetLayout, descriptors);
+
+        auto bindDescriptorSet = vsg::BindDescriptorSet::create(VK_PIPELINE_BIND_POINT_GRAPHICS, config->layout, 0, descriptorSet);
+        if (sharedObjects) sharedObjects->share(bindDescriptorSet);
+
+        scenegraph->add(bindDescriptorSet);
+
+#if 0
+        auto scaleUniform = vsg::CustomUniformValue::create(2.0);
+        auto scaleDescriptor = vsg::DescriptorBuffer(scaleUniform, 0);
+
+        Descriptors descriptors{scaleDescriptor};
+        if (sharedObjects) sharedObjects->share(descriptors);
+
+        auto config = vsg::GraphicsPipelineConfig::create(shaderSet);
+
+        auto descriptorBindings = vsg::DescriptorSetLayout::create();
+        auto descriptorSetLayout = vsg::DescriptorSetLayout::create(config->descriptorBindings);
+        if (sharedObjects) sharedObjects->share(descriptorSetLayout);
+
+        auto descriptorSet = vsg::DescriptorSet::create(descriptorSetLayout, descriptors);
+
+        auto bindDescriptorSet = vsg::BindDescriptorSet::create(VK_PIPELINE_BIND_POINT_GRAPHICS, config->layout, 0, descriptorSet);
+        if (sharedObjects) sharedObjects->share(bindDescriptorSet);
+#endif
+
+
+
+        bindVertexBuffers = vsg::BindVertexBuffers::create(0, arrays);
+        bindIndexBuffer = vsg::BindIndexBuffer::create(indices);
+
+        // setup geometry
+        auto drawCommands = vsg::Commands::create();
+        drawCommands->addChild(bindVertexBuffers);
+        drawCommands->addChild(bindIndexBuffer);
+        drawCommands->addChild(drawIndexed);
+
+        scenegraph->addChild(drawCommands);
+    }
+    else
+    {
+        std::cout << "TODO : CpuLayoutTechnique::setup(), does not yet support updates. Consider using GpuLayoutTechnique instead." << std::endl;
+        // bindVertexBuffers->copyDataToBuffers();
+        // bindIndexBuffer->copyDataToBuffers();
+    }
 }
 
 int main(int argc, char** argv)
@@ -64,6 +330,7 @@ int main(int argc, char** argv)
     // set up search paths to SPIRV shaders and textures
     auto options = vsg::Options::create();
     options->paths = vsg::getEnvPaths("VSG_FILE_PATH");
+    auto sharedObjects = options->sharedObjects = vsg::SharedObjects::create();
 #ifdef vsgXchange_all
     // add vsgXchange's support for reading and writing 3rd party file formats
     options->add(vsgXchange::all::create());
@@ -114,7 +381,10 @@ int main(int argc, char** argv)
         text->text = vsg::stringValue::create("Custom vsg::Text shaders.");
         text->font = font;
         text->layout = layout;
+        text->technique = CustomTechnique::create();
         text->setup();
+
+
         scenegraph->addChild(text);
     }
 
