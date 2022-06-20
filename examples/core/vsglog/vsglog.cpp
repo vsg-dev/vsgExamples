54fec41e27734fb8d6cef48305aafee5657d7bfb
Robert Osfield <robert@openscenegraph.com>
2022-06-20 18:14:04 +0100

    Fleshed out base Logger class, with thread safe StdLogger and CustomLogger tests.

--- examples/core/vsglog/vsglog.cpp
+++ examples/core/vsglog/vsglog.cpp
@@ -4,32 +4,109 @@
 
 namespace vsg
 {
+    class Logger : public Object
+    {
+    public:
+        Logger() {}
+
+        virtual void info(const std::string& message) = 0;
+        virtual void warn(const std::string& message) = 0;
+        virtual void critical(const std::string& message) = 0;
+        virtual void error(const std::string& message) = 0;
+    };
+
+    class StdLogger : public Inherit<Logger, StdLogger>
+    {
+    public:
+        StdLogger() {}
+
+        void info(const std::string& message) override
+        {
+            std::scoped_lock<std::mutex> lock(mutex);
+            std::cout<<"cout: "<<message<<std::endl;
+        }
+
+        void warn(const std::string& message) override
+        {
+            std::scoped_lock<std::mutex> lock(mutex);
+            std::cout<<"cout: "<<message<<std::endl;
+        }
+
+        void critical(const std::string& message) override
+        {
+            std::scoped_lock<std::mutex> lock(mutex);
+            std::cerr<<"cerr: "<<message<<std::endl;
+        }
+
+        void error(const std::string& message) override
+        {
+            std::scoped_lock<std::mutex> lock(mutex);
+            std::cerr<<"cerr: "<<message<<std::endl;
+        }
+
+        std::mutex mutex;
+    };
+
+    static ref_ptr<Logger>& log()
+    {
+        static ref_ptr<Logger> s_logger = StdLogger::create();
+        return s_logger;
+    }
+
     void info(const char* str)
     {
-        std::cout<<"info : "<<str<<")"<<std::endl;
+        log()->info(str);
     }
 
     void warn(const char* str)
     {
-        std::cout<<"warn("<<str<<")"<<std::endl;
+        log()->warn(str);
     }
 
     void critical(const char* str)
     {
-        std::cout<<"certical("<<str<<")"<<std::endl;
+        log()->critical(str);
     }
 
     void error(const char* str)
     {
-        std::cout<<"error("<<str<<")"<<std::endl;
-    }
-
-    void debug(const char* str)
-    {
-        std::cout<<"debug("<<str<<")"<< std::endl;
+        log()->error(str);
     }
 
 }
+
+class CustomLogger : public vsg::Inherit<vsg::Logger, CustomLogger>
+{
+public:
+    CustomLogger() {}
+
+    void info(const std::string& message) override
+    {
+        std::scoped_lock<std::mutex> lock(mutex);
+        std::cout<<"custom cout: "<<message<<std::endl;
+    }
+
+    void warn(const std::string& message) override
+    {
+        std::scoped_lock<std::mutex> lock(mutex);
+        std::cout<<"custom cout: "<<message<<std::endl;
+    }
+
+    void critical(const std::string& message) override
+    {
+        std::scoped_lock<std::mutex> lock(mutex);
+        std::cerr<<"custom cerr: "<<message<<std::endl;
+    }
+
+    void error(const std::string& message) override
+    {
+        std::scoped_lock<std::mutex> lock(mutex);
+        std::cerr<<"custom cerr: "<<message<<std::endl;
+    }
+
+    std::mutex mutex;
+};
+
 
 int main(int /*argc*/, char** /*argv*/)
 {
@@ -38,7 +115,13 @@ int main(int /*argc*/, char** /*argv*/)
     vsg::warn("warn string");
     vsg::critical("critical string");
     vsg::error("error string");
-    vsg::debug("debug string");
+
+    vsg::log() = CustomLogger::create();
+
+    vsg::info("info string");
+    vsg::warn("warn string");
+    vsg::critical("critical string");
+    vsg::error("error string");
 
     return 0;
 }
