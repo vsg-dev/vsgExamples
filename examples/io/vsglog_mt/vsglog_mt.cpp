0fc50ca49b99801f066b88f724bd2c8936c3b8b0
Robert Osfield <robert@openscenegraph.com>
2022-06-24 12:52:24 +0100

    Moved ThreadLogger into core VSG.

--- examples/io/vsglog_mt/vsglog_mt.cpp
+++ examples/io/vsglog_mt/vsglog_mt.cpp
@@ -2,64 +2,6 @@
 
 #include <iostream>
 
-class ThreadLogger : public vsg::Inherit<vsg::Logger, ThreadLogger>
-{
-public:
-    ThreadLogger() {}
-
-    void setThreadPrefix(std::thread::id id, const std::string& str)
-    {
-        std::scoped_lock<std::mutex> lock(_mutex);
-        _threadPrefixes[id] = str;
-    }
-
-    std::string debugPrefix = "debug: ";
-    std::string infoPrefix = "info: ";
-    std::string warnPrefix = "Warning: ";
-    std::string errorPrefix = "ERROR: ";
-
-protected:
-
-    std::map<std::thread::id, std::string> _threadPrefixes;
-
-    void print_id(std::ostream& out, std::thread::id id)
-    {
-        auto itr = _threadPrefixes.find(id);
-        if (itr != _threadPrefixes.end())
-        {
-            out<<itr->second;
-        }
-        else
-        {
-            out<<"thread::id = "<<id<<": ";
-        }
-    }
-
-    void debug_implementation(std::string_view message) override
-    {
-        print_id(std::cout, std::this_thread::get_id());
-        std::cout<<debugPrefix<<message<<"\n";
-    }
-
-    void info_implementation(std::string_view message) override
-    {
-        print_id(std::cout, std::this_thread::get_id());
-        std::cout<<infoPrefix<<message<<"\n";
-    }
-
-    void warn_implementation(std::string_view message) override
-    {
-        print_id(std::cout, std::this_thread::get_id());
-        std::cerr<<warnPrefix<<message<<std::endl;
-    }
-
-    void error_implementation(std::string_view message) override
-    {
-        print_id(std::cout, std::this_thread::get_id());
-        std::cerr<<errorPrefix<<message<<std::endl;
-    }
-};
-
 struct MyOperation : public vsg::Inherit<vsg::Operation, MyOperation>
 {
     uint32_t value = 0;
@@ -76,7 +18,7 @@ struct MyOperation : public vsg::Inherit<vsg::Operation, MyOperation>
 int main(int argc, char** argv)
 {
     // assign our custom ThreadLogger
-    auto mt_logger = ThreadLogger::create();
+    auto mt_logger = vsg::ThreadLogger::create();
     vsg::Logger::instance() = mt_logger;
 
     // set thread main thread prefix
@@ -86,17 +28,22 @@ int main(int argc, char** argv)
     auto numThreads = arguments.value<size_t>(16, "-t");
     auto count = arguments.value<size_t>(100, "-n");
     auto level = vsg::Logger::Level(arguments.value(0, "-l"));
+    auto defaultThreadPrefix = arguments.read({"-d", "--default"});
 
     // default to logger level 0 to print all messages, but allow command line to override.
     vsg::Logger::instance()->level = level;
 
     auto operationThreads = vsg::OperationThreads::create(numThreads);
-    uint32_t threadNum = 0;
-    for(auto& thread : operationThreads->threads)
+
+    if (!defaultThreadPrefix)
     {
-        auto prefix = vsg::make_string("thread ", threadNum++, " | ");
-        mt_logger->setThreadPrefix(thread.get_id(), prefix);
-        vsg::info("set thread prefix for thread::id = ", thread.get_id(), " to ", prefix);
+        uint32_t threadNum = 0;
+        for(auto& thread : operationThreads->threads)
+        {
+            auto prefix = vsg::make_string("thread ", threadNum++, " | ");
+            mt_logger->setThreadPrefix(thread.get_id(), prefix);
+            vsg::info("set thread prefix for thread::id = ", thread.get_id(), " to ", prefix);
+        }
     }
 
     vsg::info("Adding ", count, " MyOperations.");
