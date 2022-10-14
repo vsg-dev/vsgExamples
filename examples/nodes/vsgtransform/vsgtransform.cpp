7b49f725c4ca732db524d5fd56a63d784b2e0a03
Robert Osfield <robert@openscenegraph.com>
2022-10-14 15:59:57 +0100

    Added vsgtransform example to illustrate how to set up MatrixTransform

---
+++ examples/nodes/vsgtransform/vsgtransform.cpp
@@ -0,0 +1,148 @@
+#include <vsg/all.h>
+
+#ifdef vsgXchange_FOUND
+#    include <vsgXchange/all.h>
+#endif
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    // set up defaults and read command line arguments to override them
+    auto options = vsg::Options::create();
+    options->paths = vsg::getEnvPaths("VSG_FILE_PATH");
+    options->sharedObjects = vsg::SharedObjects::create();
+
+#ifdef vsgXchange_all
+    // add vsgXchange's support for reading and writing 3rd party file formats
+    options->add(vsgXchange::all::create());
+#endif
+
+    auto windowTraits = vsg::WindowTraits::create();
+    windowTraits->windowTitle = "vsgtransform";
+
+
+    // set up defaults and read command line arguments to override them
+    vsg::CommandLine arguments(&argc, argv);
+    windowTraits->debugLayer = arguments.read({"--debug", "-d"});
+    windowTraits->apiDumpLayer = arguments.read({"--api", "-a"});
+
+    arguments.read("--screen", windowTraits->screenNum);
+    arguments.read("--display", windowTraits->display);
+    auto numFrames = arguments.value(-1, "-f");
+    if (arguments.read({"--fullscreen", "--fs"})) windowTraits->fullscreen = true;
+    if (arguments.read({"--window", "-w"}, windowTraits->width, windowTraits->height)) { windowTraits->fullscreen = false; }
+    double scale = arguments.value<double>(3.0, {"-s", "--scale"});
+
+    // bool useStagingBuffer = arguments.read({"--staging-buffer", "-s"});
+
+    auto outputFilename = arguments.value<std::string>("", "-o");
+
+    if (argc<1)
+    {
+        std::cout<<"Please specify model to load on command line."<<std::endl;
+        return 0;
+    }
+
+    vsg::Path filename = argv[1];
+    auto model = vsg::read_cast<vsg::Node>(filename, options);
+    if (!model)
+    {
+        std::cout<<"Faled to load "<<filename<<std::endl;
+        return 1;
+    }
+
+    // compute the bounds of the scene graph to help position camera
+    auto bounds = vsg::visit<vsg::ComputeBounds>(model).bounds;
+    vsg::dvec3 centre = (bounds.min + bounds.max) * 0.5;
+    double radius = vsg::length(bounds.max - bounds.min) * 0.6;
+
+    auto scene = vsg::Group::create();
+
+    auto tm_1 = vsg::MatrixTransform::create();
+    tm_1->matrix = vsg::translate(-radius*(0.75+scale*0.5), 0.0, 0.0);
+    tm_1->addChild(model);
+    scene->addChild(tm_1);
+
+    auto tm_2 = vsg::MatrixTransform::create();
+    tm_2->matrix = vsg::translate(centre) * vsg::scale(scale, scale, scale) * vsg::translate(-centre);
+    tm_2->addChild(model);
+    scene->addChild(tm_2);
+
+    auto tm_3 = vsg::MatrixTransform::create();
+    tm_3->matrix = vsg::translate(centre + vsg::dvec3(radius*(0.75+scale*0.5), 0.0, 0.0)) * vsg::rotate(vsg::radians(90.0), 1.0, 0.0, 0.0) * vsg::translate(-centre);
+    tm_3->addChild(model);
+    scene->addChild(tm_3);
+
+    // write out scene if required
+    if (!outputFilename.empty())
+    {
+        vsg::write(scene, outputFilename, options);
+        return 0;
+    }
+
+
+    // create the viewer and assign window(s) to it
+    auto viewer = vsg::Viewer::create();
+
+    auto window = vsg::Window::create(windowTraits);
+    if (!window)
+    {
+        std::cout << "Could not create windows." << std::endl;
+        return 1;
+    }
+
+    viewer->addWindow(window);
+
+    vsg::ref_ptr<vsg::LookAt> lookAt;
+
+    // update bounds to new scene extent
+    bounds = vsg::visit<vsg::ComputeBounds>(scene).bounds;
+    centre = (bounds.min + bounds.max) * 0.5;
+    radius = vsg::length(bounds.max - bounds.min) * 0.6;
+
+    // set up the camera
+    lookAt = vsg::LookAt::create(centre + vsg::dvec3(0.0, -radius * 3.5, 0.0), centre, vsg::dvec3(0.0, 0.0, 1.0));
+
+    double nearFarRatio = 0.001;
+    auto perspective = vsg::Perspective::create(30.0, static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height), nearFarRatio * radius, radius * 10.0);
+
+    auto camera = vsg::Camera::create(perspective, lookAt, vsg::ViewportState::create(window->extent2D()));
+
+    // add the camera and scene graph to View
+    auto view = vsg::View::create();
+    view->camera = camera;
+    view->addChild(scene);
+
+    // add close handler to respond the close window button and pressing escape
+    viewer->addEventHandler(vsg::CloseHandler::create(viewer));
+    viewer->addEventHandler(vsg::Trackball::create(camera));
+
+    auto commandGraph = vsg::createCommandGraphForView(window, camera, scene);
+    viewer->assignRecordAndSubmitTaskAndPresentation({commandGraph});
+
+    viewer->compile();
+
+    auto startTime = vsg::clock::now();
+    double numFramesCompleted = 0.0;
+
+    // rendering main loop
+    while (viewer->advanceToNextFrame() && (numFrames < 0 || (numFrames--) > 0))
+    {
+        // pass any events into EventHandlers assigned to the Viewer
+        viewer->handleEvents();
+        viewer->update();
+        viewer->recordAndSubmit();
+        viewer->present();
+
+        numFramesCompleted += 1.0;
+    }
+
+    auto duration = std::chrono::duration<double, std::chrono::seconds::period>(vsg::clock::now() - startTime).count();
+    if (numFramesCompleted > 0.0)
+    {
+        std::cout << "Average frame rate = " << (numFramesCompleted / duration) << std::endl;
+    }
+
+    return 0;
+}
