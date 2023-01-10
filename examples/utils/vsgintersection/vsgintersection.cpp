f9729d61131e851b266e7dfffe7df5daa6c8e6b0
Robert Osfield <robert@openscenegraph.com>
2023-01-10 12:29:54 +0000

    Added EllipsoidModel to Trackball

--- examples/utils/vsgintersection/vsgintersection.cpp
+++ examples/utils/vsgintersection/vsgintersection.cpp
@@ -279,7 +279,7 @@ int main(int argc, char** argv)
     // add close handler to respond the close window button and pressing escape
     viewer->addEventHandler(vsg::CloseHandler::create(viewer));
 
-    viewer->addEventHandler(vsg::Trackball::create(camera));
+    viewer->addEventHandler(vsg::Trackball::create(camera, ellipsoidModel));
 
     auto intersectionHandler = IntersectionHandler::create(builder, camera, scene, ellipsoidModel, radius * 0.1, options);
     intersectionHandler->state = stateInfo;
