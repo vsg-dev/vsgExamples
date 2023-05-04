7b27afb7c2c3951ebfd6d66f035807d39d4eea6a
Robert Osfield <robert@openscenegraph.com>
2023-05-04 15:49:19 +0100

    Added resizing of the project matrices

--- examples/app/vsgmultiviews/vsgmultiviews.cpp
+++ examples/app/vsgmultiviews/vsgmultiviews.cpp
@@ -102,16 +102,26 @@ public:
                 if (auto view = renderGraph->children[i].cast<vsg::View>()) views.emplace_back(i, view);
             }
 
-            if (views.size()>1)
-            {
-                // swap rendering order by swap the view entries in the renderGraph->children
-                std::swap(renderGraph->children[views[0].first], renderGraph->children[views[1].first]);
-                std::swap(views[0].second->camera->viewportState, views[1].second->camera->viewportState);
-            }
+            if (views.size()<2) return;
 
             auto renderPass = renderGraph->getRenderPass();
             if (!renderPass) return;
 
+            auto view0 = views[0].second;
+            auto viewport0 = view0->camera->getViewport();
+            VkExtent2D extent0{ static_cast<uint32_t>(viewport0.width), static_cast<uint32_t>(viewport0.height)};
+
+            auto view1 = views[1].second;
+            auto viewport1 = view1->camera->getViewport();
+            VkExtent2D extent1{ static_cast<uint32_t>(viewport1.width), static_cast<uint32_t>(viewport1.height)};
+
+            // swap rendering order by swap the view entries in the renderGraph->children
+            std::swap(renderGraph->children[views[0].first], renderGraph->children[views[1].first]);
+            std::swap(views[0].second->camera->viewportState, views[1].second->camera->viewportState);
+
+            // change the aspect ratios of the projection matrices to fit the new diemensions.
+            view0->camera->projectionMatrix->changeExtent(extent0, extent1);
+            view1->camera->projectionMatrix->changeExtent(extent1, extent0);
 
             // wait until the device is idle to avoid changing state while it's being used.
             vkDeviceWaitIdle(*(renderPass->device));
