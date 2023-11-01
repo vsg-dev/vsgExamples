5a34849c299cc840e59be5929c35ef80f6ffcde1
Robert Osfield <robert@openscenegraph.com>
2023-11-01 11:59:57 +0000

    Added loading and rendering of models

--- examples/utils/vsgcustomshaderset/vsgcustomshaderset.cpp
+++ examples/utils/vsgcustomshaderset/vsgcustomshaderset.cpp
@@ -23,10 +23,19 @@ int main(int argc, char** argv)
 
     // read any command line options that the ReaderWriters support
     arguments.read(options);
-    if (argc <= 1) return 0;
 
-    auto inputFilename = arguments.value<vsg::Path>("", "-i");
+    auto windowTraits = vsg::WindowTraits::create();
+    windowTraits->windowTitle = "vscustomshaderset";
+    windowTraits->debugLayer = arguments.read({"--debug", "-d"});
+    windowTraits->apiDumpLayer = arguments.read({"--api", "-a"});
+    windowTraits->synchronizationLayer = arguments.read("--sync");
+
+    auto numFrames = arguments.value(-1, "-f");
     auto outputFilename = arguments.value<vsg::Path>("", "-o");
+    auto outputShaderSetFilename = arguments.value<vsg::Path>("", "--os");
+    auto horizonMountainHeight = arguments.value(0.0, "--hmh");
+    auto nearFarRatio = arguments.value<double>(0.001, "--nfr");
+    bool reportAverageFrameRate = arguments.read("--fps");
 
     vsg::ref_ptr<vsg::ShaderSet> shaderSet = pbr_ShaderSet(options);
     options->shaderSets["pbr"] = shaderSet;
@@ -38,10 +47,106 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    if (outputFilename)
+    if (outputShaderSetFilename)
     {
-        vsg::write(shaderSet, outputFilename, options);
+        vsg::write(shaderSet, outputShaderSetFilename, options);
     }
 
+    auto group = vsg::Group::create();
+
+    vsg::Path path;
+
+    // read any vsg files
+    for (int i = 1; i < argc; ++i)
+    {
+        vsg::Path filename = arguments[i];
+        if (auto node = vsg::read_cast<vsg::Node>(filename, options))
+        {
+            group->addChild(node);
+        }
+        else
+        {
+            std::cout << "Unable to load file " << filename << std::endl;
+        }
+    }
+
+    if (group->children.empty())
+    {
+        return 1;
+    }
+
+    vsg::ref_ptr<vsg::Node> vsg_scene;
+    if (group->children.size() == 1)
+        vsg_scene = group->children[0];
+    else
+        vsg_scene = group;
+
+    if (outputFilename)
+    {
+        vsg::write(vsg_scene, outputFilename, options);
+    }
+
+    // create the viewer and assign window(s) to it
+    auto viewer = vsg::Viewer::create();
+    auto window = vsg::Window::create(windowTraits);
+    if (!window)
+    {
+        std::cout << "Could not create window." << std::endl;
+        return 1;
+    }
+
+    viewer->addWindow(window);
+
+    // compute the bounds of the scene graph to help position camera
+    auto bounds = vsg::visit<vsg::ComputeBounds>(vsg_scene).bounds;
+    vsg::dvec3 centre = (bounds.min + bounds.max) * 0.5;
+    double radius = vsg::length(bounds.max - bounds.min) * 0.6;
+
+    // set up the camera
+    auto lookAt = vsg::LookAt::create(centre + vsg::dvec3(0.0, -radius * 3.5, 0.0), centre, vsg::dvec3(0.0, 0.0, 1.0));
+
+    vsg::ref_ptr<vsg::ProjectionMatrix> perspective;
+    auto ellipsoidModel = vsg_scene->getRefObject<vsg::EllipsoidModel>("EllipsoidModel");
+    if (ellipsoidModel)
+    {
+        perspective = vsg::EllipsoidPerspective::create(lookAt, ellipsoidModel, 30.0, static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height), nearFarRatio, horizonMountainHeight);
+    }
+    else
+    {
+        perspective = vsg::Perspective::create(30.0, static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height), nearFarRatio * radius, radius * 4.5);
+    }
+
+    auto camera = vsg::Camera::create(perspective, lookAt, vsg::ViewportState::create(window->extent2D()));
+
+    // add close handler to respond to the close window button and pressing escape
+    viewer->addEventHandler(vsg::CloseHandler::create(viewer));
+    viewer->addEventHandler(vsg::Trackball::create(camera, ellipsoidModel));
+
+    auto commandGraph = vsg::createCommandGraphForView(window, camera, vsg_scene);
+    viewer->assignRecordAndSubmitTaskAndPresentation({commandGraph});
+
+    viewer->compile();
+
+    viewer->start_point() = vsg::clock::now();
+
+    // rendering main loop
+    while (viewer->advanceToNextFrame() && (numFrames < 0 || (numFrames--) > 0))
+    {
+        // pass any events into EventHandlers assigned to the Viewer
+        viewer->handleEvents();
+
+        viewer->update();
+
+        viewer->recordAndSubmit();
+
+        viewer->present();
+    }
+
+    if (reportAverageFrameRate)
+    {
+        auto fs = viewer->getFrameStamp();
+        double fps = static_cast<double>(fs->frameCount) / std::chrono::duration<double, std::chrono::seconds::period>(vsg::clock::now() - viewer->start_point()).count();
+        std::cout<<"Average frame rate = "<<fps<<" fps"<<std::endl;
+    }
     return 0;
 }
