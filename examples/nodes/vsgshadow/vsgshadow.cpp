cf7c261ae6e5fe0c28ae41236c4d57fe2edbe892
Robert Osfield <robert@openscenegraph.com>
2023-12-21 16:39:58 +0000

    Added TracyInstrumentation and GpuAnnoation to vsgshadow

--- examples/nodes/vsgshadow/vsgshadow.cpp
+++ examples/nodes/vsgshadow/vsgshadow.cpp
@@ -227,6 +227,8 @@ int main(int argc, char** argv)
 #ifdef Tracy_FOUND
     else if (arguments.read("--tracy"))
     {
+        windowTraits->deviceExtensionNames.push_back(VK_EXT_CALIBRATED_TIMESTAMPS_EXTENSION_NAME);
+
         auto tracy_instrumentation = vsg::TracyInstrumentation::create();
         arguments.read("--cpu", tracy_instrumentation->settings->cpu_instumentation_level);
         arguments.read("--gpu", tracy_instrumentation->settings->gpu_instumentation_level);
