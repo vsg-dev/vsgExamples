89911b651ee1693526ec46b6ac2a18b7100b3603
Robert Osfield <robert@openscenegraph.com>
2024-09-03 14:32:17 +0100

    Added sleep after end of main loop to help with debugging.

--- examples/app/vsgviewer/vsgviewer.cpp
+++ examples/app/vsgviewer/vsgviewer.cpp
@@ -333,6 +333,8 @@ int main(int argc, char** argv)
             std::cout << "Average frame rate = " << fps << " fps" << std::endl;
         }
 
+        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
         if (auto profiler = instrumentation.cast<vsg::Profiler>())
         {
             instrumentation->finish();
