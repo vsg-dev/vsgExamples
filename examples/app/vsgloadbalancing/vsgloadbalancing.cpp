47ba0e69e8793c3b987b5f3dbab2364bde0411de
Robert Osfield <robert.osfield@gmail.com>
2025-12-04 16:49:43 +0000

    Created the initial base of new vsgloadbalancing example from vsgviewer.

---
+++ examples/app/vsgloadbalancing/vsgloadbalancing.cpp
@@ -0,0 +1,438 @@
+#include <vsg/all.h>
+
+#ifdef vsgXchange_FOUND
+#    include <vsgXchange/all.h>
+#endif
+
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+vsg::ref_ptr<vsg::Node> createTextureQuad(vsg::ref_ptr<vsg::Data> sourceData, vsg::ref_ptr<vsg::Options> options)
+{
+    auto builder = vsg::Builder::create();
+    builder->options = options;
+
+    vsg::StateInfo state;
+    state.image = sourceData;
+    state.lighting = false;
+
+    vsg::GeometryInfo geom;
+    geom.dx.set(static_cast<float>(sourceData->width()), 0.0f, 0.0f);
+    geom.dy.set(0.0f, 0.0f, static_cast<float>(sourceData->height()));
+    geom.dz.set(0.0f, -1.0f, 0.0f);
+
+    return builder->createQuad(geom, state);
+}
+
+void enableGenerateDebugInfo(vsg::ref_ptr<vsg::Options> options)
+{
+    auto shaderHints = vsg::ShaderCompileSettings::create();
+    shaderHints->generateDebugInfo = true;
+
+    auto& text = options->shaderSets["text"] = vsg::createTextShaderSet(options);
+    text->defaultShaderHints = shaderHints;
+
+    auto& flat = options->shaderSets["flat"] = vsg::createFlatShadedShaderSet(options);
+    flat->defaultShaderHints = shaderHints;
+
+    auto& phong = options->shaderSets["phong"] = vsg::createPhongShaderSet(options);
+    phong->defaultShaderHints = shaderHints;
+
+    auto& pbr = options->shaderSets["pbr"] = vsg::createPhysicsBasedRenderingShaderSet(options);
+    pbr->defaultShaderHints = shaderHints;
+}
+
+int main(int argc, char** argv)
+{
+    try
+    {
+        // set up defaults and read command line arguments to override them
+        vsg::CommandLine arguments(&argc, argv);
+
+        // create windowTraits using the any command line arugments to configure settings
+        auto windowTraits = vsg::WindowTraits::create(arguments);
+
+        // if we want to redirect std::cout and std::cerr to the vsg::Logger call vsg::Logger::redirect_stdout()
+        if (arguments.read({"--redirect-std", "-r"})) vsg::Logger::instance()->redirect_std();
+
+        // set up vsg::Options to pass in filepaths, ReaderWriters and other IO related options to use when reading and writing files.
+        auto options = vsg::Options::create();
+        options->sharedObjects = vsg::SharedObjects::create();
+        options->fileCache = vsg::getEnv("VSG_FILE_CACHE");
+        options->paths = vsg::getEnvPaths("VSG_FILE_PATH");
+
+#ifdef vsgXchange_all
+        // add vsgXchange's support for reading and writing 3rd party file formats
+        options->add(vsgXchange::all::create());
+#endif
+
+        options->readOptions(arguments);
+
+        if (uint32_t numOperationThreads = 0; arguments.read("--ot", numOperationThreads)) options->operationThreads = vsg::OperationThreads::create(numOperationThreads);
+
+        bool reportAverageFrameRate = arguments.read("--fps");
+        bool reportMemoryStats = arguments.read("--rms");
+        if (arguments.read({"-t", "--test"}))
+        {
+            windowTraits->swapchainPreferences.presentMode = VK_PRESENT_MODE_IMMEDIATE_KHR;
+            windowTraits->fullscreen = true;
+            reportAverageFrameRate = true;
+        }
+        if (arguments.read({"--st", "--small-test"}))
+        {
+            windowTraits->swapchainPreferences.presentMode = VK_PRESENT_MODE_IMMEDIATE_KHR;
+            windowTraits->width = 192, windowTraits->height = 108;
+            windowTraits->decoration = false;
+            reportAverageFrameRate = true;
+        }
+
+        bool multiThreading = arguments.read("--mt");
+        auto maxTime = arguments.value(std::numeric_limits<double>::max(), "--max-time");
+
+        if (arguments.read("--ThreadLogger")) vsg::Logger::instance() = vsg::ThreadLogger::create();
+        if (int log_level = 0; arguments.read("--log-level", log_level)) vsg::Logger::instance()->level = vsg::Logger::Level(log_level);
+        auto numFrames = arguments.value(-1, "-f");
+        auto pathFilename = arguments.value<vsg::Path>("", "-p");
+        auto loadLevels = arguments.value(0, "--load-levels");
+        auto maxPagedLOD = arguments.value(0, "--maxPagedLOD");
+        auto horizonMountainHeight = arguments.value(0.0, "--hmh");
+        auto nearFarRatio = arguments.value<double>(0.001, "--nfr");
+        if (arguments.read("--rgb")) options->mapRGBtoRGBAHint = false;
+
+        bool depthClamp = arguments.read({"--dc", "--depthClamp"});
+        if (depthClamp)
+        {
+            std::cout << "Enabled depth clamp." << std::endl;
+            auto deviceFeatures = windowTraits->deviceFeatures = vsg::DeviceFeatures::create();
+            deviceFeatures->get().samplerAnisotropy = VK_TRUE;
+            deviceFeatures->get().depthClamp = VK_TRUE;
+        }
+
+        vsg::ref_ptr<vsg::ResourceHints> resourceHints;
+        if (auto resourceHintsFilename = arguments.value<vsg::Path>("", "--rh"))
+        {
+            resourceHints = vsg::read_cast<vsg::ResourceHints>(resourceHintsFilename, options);
+        }
+
+        if (auto outputResourceHintsFilename = arguments.value<vsg::Path>("", "--orh"))
+        {
+            if (!resourceHints) resourceHints = vsg::ResourceHints::create();
+            vsg::write(resourceHints, outputResourceHintsFilename, options);
+            return 0;
+        }
+
+        if (arguments.read({"--shader-debug-info", "--sdi"}))
+        {
+            enableGenerateDebugInfo(options);
+            windowTraits->deviceExtensionNames.push_back(VK_KHR_SHADER_NON_SEMANTIC_INFO_EXTENSION_NAME);
+        }
+
+        if (int log_level = 0; arguments.read("--log-level", log_level)) vsg::Logger::instance()->level = vsg::Logger::Level(log_level);
+        auto logFilename = arguments.value<vsg::Path>("", "--log");
+
+        vsg::ref_ptr<vsg::Instrumentation> instrumentation;
+        if (arguments.read({"--gpu-annotation", "--ga"}) && vsg::isExtensionSupported(VK_EXT_DEBUG_UTILS_EXTENSION_NAME))
+        {
+            windowTraits->debugUtils = true;
+
+            auto gpu_instrumentation = vsg::GpuAnnotation::create();
+            if (arguments.read("--name"))
+                gpu_instrumentation->labelType = vsg::GpuAnnotation::SourceLocation_name;
+            else if (arguments.read("--className"))
+                gpu_instrumentation->labelType = vsg::GpuAnnotation::Object_className;
+            else if (arguments.read("--func"))
+                gpu_instrumentation->labelType = vsg::GpuAnnotation::SourceLocation_function;
+
+            instrumentation = gpu_instrumentation;
+        }
+        else if (arguments.read({"--profiler", "--pr"}))
+        {
+            // set Profiler options
+            auto settings = vsg::Profiler::Settings::create();
+            arguments.read("--cpu", settings->cpu_instrumentation_level);
+            arguments.read("--gpu", settings->gpu_instrumentation_level);
+            arguments.read("--log-size", settings->log_size);
+
+            // create the profiler
+            instrumentation = vsg::Profiler::create(settings);
+        }
+
+        vsg::Affinity affinity;
+        uint32_t cpu = 0;
+        while (arguments.read("-c", cpu))
+        {
+            affinity.cpus.insert(cpu);
+        }
+
+        // should animations be automatically played
+        auto autoPlay = !arguments.read({"--no-auto-play", "--nop"});
+
+        if (arguments.errors()) return arguments.writeErrorMessages(std::cerr);
+
+        if (argc <= 1)
+        {
+            std::cout << "Please specify a 3d model or image file on the command line." << std::endl;
+            return 1;
+        }
+
+        auto group = vsg::Group::create();
+
+        vsg::Path path;
+
+        // read any vsg files
+        for (int i = 1; i < argc; ++i)
+        {
+            vsg::Path filename = arguments[i];
+            path = vsg::filePath(filename);
+
+            auto object = vsg::read(filename, options);
+            if (auto node = object.cast<vsg::Node>())
+            {
+                group->addChild(node);
+            }
+            else if (auto data = object.cast<vsg::Data>())
+            {
+                if (auto textureGeometry = createTextureQuad(data, options))
+                {
+                    group->addChild(textureGeometry);
+                }
+            }
+            else if (object)
+            {
+                std::cout << "Unable to view object of type " << object->className() << std::endl;
+            }
+            else
+            {
+                std::cout << "Unable to load file " << filename << std::endl;
+            }
+        }
+
+        if (group->children.empty())
+        {
+            return 1;
+        }
+
+        vsg::ref_ptr<vsg::Node> vsg_scene;
+        if (group->children.size() == 1)
+            vsg_scene = group->children[0];
+        else
+            vsg_scene = group;
+
+        // create the viewer and assign window(s) to it
+        auto viewer = vsg::Viewer::create();
+        auto window = vsg::Window::create(windowTraits);
+        if (!window)
+        {
+            std::cout << "Could not create window." << std::endl;
+            return 1;
+        }
+
+        viewer->addWindow(window);
+
+        auto ellipsoidModel = vsg_scene->getRefObject<vsg::EllipsoidModel>("EllipsoidModel");
+
+        vsg::ref_ptr<vsg::LookAt> lookAt;
+        vsg::ref_ptr<vsg::ProjectionMatrix> perspective;
+        if (ellipsoidModel)
+        {
+            // compute the bounds of the scene graph to help position camera
+            vsg::ComputeBounds computeBounds;
+            vsg_scene->accept(computeBounds);
+
+            double initialRadius = vsg::length(computeBounds.bounds.max - computeBounds.bounds.min) * 0.5;
+            double modelToEarthRatio = (initialRadius / ellipsoidModel->radiusEquator());
+
+            // if the model is small compared to the radius of the earth position camera in local coordinate frame of the model rather than ECEF.
+            if (modelToEarthRatio < 1.0)
+            {
+                vsg::dvec3 lla = ellipsoidModel->convertECEFToLatLongAltitude((computeBounds.bounds.min + computeBounds.bounds.max) * 0.5);
+
+                auto worldToLocal = ellipsoidModel->computeWorldToLocalTransform(lla);
+                auto localToWorld = ellipsoidModel->computeLocalToWorldTransform(lla);
+
+                // recompute the bounds of the model in the local coordinate frame of the model, rather than ECEF
+                // to give a tigher bound around the dataset.
+                computeBounds.matrixStack.clear();
+                computeBounds.matrixStack.push_back(worldToLocal);
+                computeBounds.bounds.reset();
+                vsg_scene->accept(computeBounds);
+
+                auto bounds = computeBounds.bounds;
+                vsg::dvec3 centre = (bounds.min + bounds.max) * 0.5;
+                double radius = vsg::length(bounds.max - bounds.min) * 0.5;
+
+                lookAt = vsg::LookAt::create(localToWorld * (centre + vsg::dvec3(0.0, 0.0, radius)), localToWorld * centre, vsg::dvec3(0.0, 1.0, 0.0) * worldToLocal);
+            }
+            else
+            {
+                lookAt = vsg::LookAt::create(vsg::dvec3(initialRadius * 2.0, 0.0, 0.0), vsg::dvec3(0.0, 0.0, 0.0), vsg::dvec3(0.0, 0.0, 1.0));
+            }
+
+            perspective = vsg::EllipsoidPerspective::create(lookAt, ellipsoidModel, 30.0, static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height), nearFarRatio, horizonMountainHeight);
+        }
+        else
+        {
+            // compute the bounds of the scene graph to help position camera
+            vsg::ComputeBounds computeBounds;
+            vsg_scene->accept(computeBounds);
+
+            vsg::dvec3 centre = (computeBounds.bounds.min + computeBounds.bounds.max) * 0.5;
+            double radius = vsg::length(computeBounds.bounds.max - computeBounds.bounds.min) * 0.6;
+
+            // set up the camera
+            lookAt = vsg::LookAt::create(centre + vsg::dvec3(0.0, -radius * 3.5, 0.0), centre, vsg::dvec3(0.0, 0.0, 1.0));
+            perspective = vsg::Perspective::create(30.0, static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height), nearFarRatio * radius, radius * 10.5);
+        }
+
+        auto camera = vsg::Camera::create(perspective, lookAt, vsg::ViewportState::create(window->extent2D()));
+
+        // add close handler to respond to the close window button and pressing escape
+        viewer->addEventHandler(vsg::CloseHandler::create(viewer));
+
+        auto cameraAnimation = vsg::CameraAnimationHandler::create(camera, pathFilename, options);
+        viewer->addEventHandler(cameraAnimation);
+        if (autoPlay && cameraAnimation->animation)
+        {
+            cameraAnimation->play();
+
+            if (reportAverageFrameRate && maxTime == std::numeric_limits<double>::max())
+            {
+                maxTime = cameraAnimation->animation->maxTime();
+            }
+        }
+
+        viewer->addEventHandler(vsg::Trackball::create(camera, ellipsoidModel));
+
+        // if required preload specific number of PagedLOD levels.
+        if (loadLevels > 0)
+        {
+            vsg::LoadPagedLOD loadPagedLOD(camera, loadLevels);
+
+            auto startTime = vsg::clock::now();
+
+            vsg_scene->accept(loadPagedLOD);
+
+            auto time = std::chrono::duration<float, std::chrono::milliseconds::period>(vsg::clock::now() - startTime).count();
+            std::cout << "No. of tiles loaded " << loadPagedLOD.numTiles << " in " << time << "ms." << std::endl;
+        }
+
+        auto commandGraph = vsg::createCommandGraphForView(window, camera, vsg_scene);
+        viewer->assignRecordAndSubmitTaskAndPresentation({commandGraph});
+
+        if (instrumentation) viewer->assignInstrumentation(instrumentation);
+
+        if (multiThreading)
+        {
+            viewer->setupThreading();
+
+            if (affinity)
+            {
+                auto cpu_itr = affinity.cpus.begin();
+
+                // set affinity of main thread
+                if (cpu_itr != affinity.cpus.end())
+                {
+                    std::cout << "vsg::setAffinity() " << *cpu_itr << std::endl;
+                    vsg::setAffinity(vsg::Affinity(*cpu_itr++));
+                }
+
+                for (auto& thread : viewer->threads)
+                {
+                    if (thread.joinable() && cpu_itr != affinity.cpus.end())
+                    {
+                        std::cout << "vsg::setAffinity(" << thread.get_id() << ") " << *cpu_itr << std::endl;
+                        vsg::setAffinity(thread, vsg::Affinity(*cpu_itr++));
+                    }
+                }
+            }
+        }
+        else if (affinity)
+        {
+            std::cout << "vsg::setAffinity(";
+            for (auto cpu_num : affinity.cpus)
+            {
+                std::cout << " " << cpu_num;
+            }
+            std::cout << " )" << std::endl;
+
+            vsg::setAffinity(affinity);
+        }
+
+        viewer->compile(resourceHints);
+
+        if (maxPagedLOD > 0)
+        {
+            // set targetMaxNumPagedLODWithHighResSubgraphs after Viewer::compile() as it will assign any DatabasePager if required.
+            for (auto& task : viewer->recordAndSubmitTasks)
+            {
+                if (task->databasePager) task->databasePager->targetMaxNumPagedLODWithHighResSubgraphs = maxPagedLOD;
+            }
+        }
+
+        if (autoPlay)
+        {
+            // find any animation groups in the loaded scene graph and play the first animation in each of the animation groups.
+            auto animationGroups = vsg::visit<vsg::FindAnimations>(vsg_scene).animationGroups;
+            for (auto ag : animationGroups)
+            {
+                if (!ag->animations.empty()) viewer->animationManager->play(ag->animations.front());
+            }
+        }
+
+        viewer->start_point() = vsg::clock::now();
+
+        // rendering main loop
+        while (viewer->advanceToNextFrame() && (numFrames < 0 || (numFrames--) > 0) && (viewer->getFrameStamp()->simulationTime < maxTime))
+        {
+            // pass any events into EventHandlers assigned to the Viewer
+            viewer->handleEvents();
+
+            viewer->update();
+
+            viewer->recordAndSubmit();
+
+            viewer->present();
+        }
+
+        if (reportAverageFrameRate)
+        {
+            auto fs = viewer->getFrameStamp();
+            double fps = static_cast<double>(fs->frameCount) / std::chrono::duration<double, std::chrono::seconds::period>(vsg::clock::now() - viewer->start_point()).count();
+            std::cout << "Average frame rate = " << fps << " fps" << std::endl;
+        }
+
+        if (reportMemoryStats)
+        {
+            if (options->sharedObjects)
+            {
+                vsg::LogOutput output;
+                options->sharedObjects->report(output);
+            }
+        }
+
+        if (auto profiler = instrumentation.cast<vsg::Profiler>())
+        {
+            instrumentation->finish();
+            if (logFilename)
+            {
+                std::ofstream fout(logFilename);
+                profiler->log->report(fout);
+            }
+            else
+            {
+                profiler->log->report(std::cout);
+            }
+        }
+    }
+    catch (const vsg::Exception& ve)
+    {
+        for (int i = 0; i < argc; ++i) std::cerr << argv[i] << " ";
+        std::cerr << "\n[Exception] - " << ve.message << " result = " << ve.result << std::endl;
+        return 1;
+    }
+
+    // clean up done automatically thanks to ref_ptr<>
+    return 0;
+}
